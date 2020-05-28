a907d7ce8313d6634ca4962f4048534cf3470f78
Prithvi Prabhu <prithvi@h2o.ai>
2020-05-28 09:36:02 -0700

    Fix naming inconsistencies h2oai/q#678

--- ui/src/form.tsx
+++ ui/src/form.tsx
@@ -1006,7 +1006,7 @@ const
       )
     return { render }
   }),
-  XButton = bond(({ args, button: m, submit }: { args: Rec, button: Button, submit: () => void }) => {
+  XButton = bond(({ args, model: m, submit }: { args: Rec, model: Button, submit: () => void }) => {
     args[m.name] = false
     const
       render = () => {
@@ -1095,7 +1095,7 @@ const
           <div data-test='expander' className={className}>
             <Fluent.Separator alignContent="start"><Fluent.ActionButton title={actionTitle} iconProps={expanderIcon} onClick={onClick}>{m.label}</Fluent.ActionButton></Fluent.Separator>
             <div>
-              <Fields items={m.items} args={args} submit={submit} />
+              <XComponents items={m.items} args={args} submit={submit} />
             </div>
           </div>
         )
@@ -1133,37 +1133,37 @@ const
       return <div>{children}</div>
     }
   },
-  Field = ({ component: c, args, submit }: { component: Component, args: Rec, submit: () => void }) => {
-    if (c.text) return <XToolTip content={c.text.tooltip} expand={false}><XText model={c.text} /></XToolTip>
-    if (c.label) return <XToolTip content={c.label.tooltip} expand={false}><XLabel model={c.label} /></XToolTip>
-    if (c.link) return <XToolTip content={c.link.tooltip} expand={false}><XLink model={c.link} /></XToolTip>
-    if (c.separator) return <XSeparator model={c.separator} />
-    if (c.progress) return <XToolTip content={c.progress.tooltip}><XProgress model={c.progress} /></XToolTip>
-    if (c.message_bar) return <XMessageBar model={c.message_bar} />
-    if (c.textbox) return <XToolTip content={c.textbox.tooltip}><XTextbox args={args} model={c.textbox} /></XToolTip>
-    if (c.checkbox) return <XToolTip content={c.checkbox.tooltip} expand={false}><XCheckbox args={args} model={c.checkbox} submit={submit} /></XToolTip>
-    if (c.toggle) return <XToolTip content={c.toggle.tooltip} expand={false}><XToggle args={args} model={c.toggle} submit={submit} /></XToolTip>
-    if (c.choice_group) return <XToolTip content={c.choice_group.tooltip}><XChoiceGroup args={args} model={c.choice_group} submit={submit} /></XToolTip>
-    if (c.dropdown) return <XToolTip content={c.dropdown.tooltip}><XDropdown args={args} model={c.dropdown} submit={submit} /></XToolTip>
-    if (c.checklist) return <XToolTip content={c.checklist.tooltip}><XChecklist args={args} model={c.checklist} /></XToolTip>
-    if (c.combobox) return <XToolTip content={c.combobox.tooltip}><XCombobox args={args} model={c.combobox} /></XToolTip>
-    if (c.slider) return <XToolTip content={c.slider.tooltip}><XSlider args={args} model={c.slider} submit={submit} /></XToolTip>
-    if (c.spinbox) return <XToolTip content={c.spinbox.tooltip} expand={false}><XSpinbox args={args} model={c.spinbox} /></XToolTip>
-    if (c.date_picker) return <XToolTip content={c.date_picker.tooltip}><XDatePicker args={args} model={c.date_picker} /></XToolTip>
-    if (c.color_picker) return <XToolTip content={c.color_picker.tooltip}><XColorPicker args={args} model={c.color_picker} /></XToolTip>
-    if (c.file_upload) return <XToolTip content={c.file_upload.tooltip}><XFileUpload args={args} model={c.file_upload} submit={submit} /></XToolTip>
-    if (c.table) return <XToolTip content={c.table.tooltip}><XTable args={args} model={c.table} submit={submit} /></XToolTip>
-    if (c.buttons) return <XButtons args={args} model={c.buttons} submit={submit} />
-    if (c.tabs) return <XTabs args={args} model={c.tabs} submit={submit} />
-    if (c.button) return <XToolTip content={c.button.tooltip} showIcon={false} expand={false}><XStandAloneButton args={args} model={c.button} submit={submit} /></XToolTip>
-    if (c.expander) return <XExpander args={args} model={c.expander} submit={submit} />
-    if (c.nav) return <XNav args={args} model={c.nav} submit={submit} />
+  XComponent = ({ model: m, args, submit }: { model: Component, args: Rec, submit: () => void }) => {
+    if (m.text) return <XToolTip content={m.text.tooltip} expand={false}><XText model={m.text} /></XToolTip>
+    if (m.label) return <XToolTip content={m.label.tooltip} expand={false}><XLabel model={m.label} /></XToolTip>
+    if (m.link) return <XToolTip content={m.link.tooltip} expand={false}><XLink model={m.link} /></XToolTip>
+    if (m.separator) return <XSeparator model={m.separator} />
+    if (m.progress) return <XToolTip content={m.progress.tooltip}><XProgress model={m.progress} /></XToolTip>
+    if (m.message_bar) return <XMessageBar model={m.message_bar} />
+    if (m.textbox) return <XToolTip content={m.textbox.tooltip}><XTextbox args={args} model={m.textbox} /></XToolTip>
+    if (m.checkbox) return <XToolTip content={m.checkbox.tooltip} expand={false}><XCheckbox args={args} model={m.checkbox} submit={submit} /></XToolTip>
+    if (m.toggle) return <XToolTip content={m.toggle.tooltip} expand={false}><XToggle args={args} model={m.toggle} submit={submit} /></XToolTip>
+    if (m.choice_group) return <XToolTip content={m.choice_group.tooltip}><XChoiceGroup args={args} model={m.choice_group} submit={submit} /></XToolTip>
+    if (m.dropdown) return <XToolTip content={m.dropdown.tooltip}><XDropdown args={args} model={m.dropdown} submit={submit} /></XToolTip>
+    if (m.checklist) return <XToolTip content={m.checklist.tooltip}><XChecklist args={args} model={m.checklist} /></XToolTip>
+    if (m.combobox) return <XToolTip content={m.combobox.tooltip}><XCombobox args={args} model={m.combobox} /></XToolTip>
+    if (m.slider) return <XToolTip content={m.slider.tooltip}><XSlider args={args} model={m.slider} submit={submit} /></XToolTip>
+    if (m.spinbox) return <XToolTip content={m.spinbox.tooltip} expand={false}><XSpinbox args={args} model={m.spinbox} /></XToolTip>
+    if (m.date_picker) return <XToolTip content={m.date_picker.tooltip}><XDatePicker args={args} model={m.date_picker} /></XToolTip>
+    if (m.color_picker) return <XToolTip content={m.color_picker.tooltip}><XColorPicker args={args} model={m.color_picker} /></XToolTip>
+    if (m.file_upload) return <XToolTip content={m.file_upload.tooltip}><XFileUpload args={args} model={m.file_upload} submit={submit} /></XToolTip>
+    if (m.table) return <XToolTip content={m.table.tooltip}><XTable args={args} model={m.table} submit={submit} /></XToolTip>
+    if (m.buttons) return <XButtons args={args} model={m.buttons} submit={submit} />
+    if (m.tabs) return <XTabs args={args} model={m.tabs} submit={submit} />
+    if (m.button) return <XToolTip content={m.button.tooltip} showIcon={false} expand={false}><XStandAloneButton args={args} model={m.button} submit={submit} /></XToolTip>
+    if (m.expander) return <XExpander args={args} model={m.expander} submit={submit} />
+    if (m.nav) return <XNav args={args} model={m.nav} submit={submit} />
     return <Fluent.MessageBar messageBarType={Fluent.MessageBarType.severeWarning}>This component could not be rendered.</Fluent.MessageBar>
   },
-  Fields = ({ args, items, submit }: { args: Rec, items: Component[], submit: () => void }) => {
-    const fields = items.map((f, i) => <Field key={i} component={f} args={args} submit={submit} />)
+  XComponents = ({ args, items, submit }: { args: Rec, items: Component[], submit: () => void }) => {
+    const components = items.map((m, i) => <XComponent key={i} model={m} args={args} submit={submit} />)
     // TODO gap 10px between fields
-    return <>{fields}</>
+    return <>{components}</>
   },
   View = bond(({ state, changed }: Card<State>) => {
     const
@@ -1180,7 +1180,7 @@ const
 
         return (
           <div className={css.card}>
-            <Fields items={items} args={args} submit={submit} />
+            <XComponents items={items} args={args} submit={submit} />
           </div>)
       }
     return { render, changed }
