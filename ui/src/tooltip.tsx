99de878fa55d0d628f4c6dedca03ce7b92a063fe
Prithvi Prabhu <prithvi@h2o.ai>
2020-05-28 19:03:38 -0700

    Clean up tooltip implementation h2oai/q#678

--- ui/src/tooltip.tsx
+++ ui/src/tooltip.tsx
@@ -2,6 +2,7 @@ import * as Fluent from '@fluentui/react';
 import React from 'react';
 import { stylesheet } from 'typestyle';
 import { Markdown } from './markdown';
+import { B, S } from './telesync';
 
 const
   css = stylesheet({
@@ -25,32 +26,37 @@ const
   })
 
 export const
-  XToolTip = ({ children, content, showIcon, expand }: { children: React.ReactChild, content: string, showIcon?: boolean, expand?: boolean }) => {
+  XToolTip = ({ children, content, showIcon, expand }: {
+    children: React.ReactChild,
+    content?: S,
+    showIcon?: B,
+    expand?: B
+  }) => {
+    if (!content) return <>{children}</>
+
     showIcon = showIcon === undefined ? true : showIcon
     expand = expand === undefined ? true : expand
-    if (content && content.length > 0) {
-      const tooltipProps: Fluent.ITooltipProps = {
+
+    const
+      tooltipProps: Fluent.ITooltipProps = {
         onRenderContent: () => (<div><Markdown source={content} /></div>)
       }
-      if (showIcon) {
-        return (
-          <div className={css.container}>
-            <div className={expand ? css.element : ''}>{children}</div>
-            <Fluent.TooltipHost tooltipProps={tooltipProps}>
-              <Fluent.FontIcon className={css.icon} iconName='Info' />
-            </Fluent.TooltipHost>
-          </div>
-        )
-      } else {
-        return (
-          <div className={css.container}>
-            <Fluent.TooltipHost tooltipProps={tooltipProps}>
-              {children}
-            </Fluent.TooltipHost>
-          </div>
-        )
-      }
+    if (showIcon) {
+      return (
+        <div className={css.container}>
+          <div className={expand ? css.element : ''}>{children}</div>
+          <Fluent.TooltipHost tooltipProps={tooltipProps}>
+            <Fluent.FontIcon className={css.icon} iconName='Info' />
+          </Fluent.TooltipHost>
+        </div>
+      )
     } else {
-      return <div>{children}</div>
+      return (
+        <div className={css.container}>
+          <Fluent.TooltipHost tooltipProps={tooltipProps}>
+            {children}
+          </Fluent.TooltipHost>
+        </div>
+      )
     }
   }
