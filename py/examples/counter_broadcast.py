0ebfec1daaad51f21e4971f3b619e1950094bb29
Prithvi Prabhu <prithvi@h2o.ai>
2020-06-25 23:55:31 -0700

    Fix counter example h2oai/q#678

--- py/examples/counter_broadcast.py
+++ py/examples/counter_broadcast.py
@@ -11,13 +11,13 @@ async def main(q: Q):
     items = pack([ui.button(name='increment', label=f'Count={count}')])
 
     if count > 0:
-        form = q.page['a_form']
+        form = q.page['example']
         form.items = items
     else:
-        q.page['a_form'] = ui.form_card(box='1 1 12 10', items=items)
+        q.page['example'] = ui.form_card(box='1 1 12 10', items=items)
 
     await q.page.push()
 
 
 if __name__ == '__main__':
-    listen('/test_app', main)
+    listen('/demo', main)
