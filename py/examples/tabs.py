d4f05cfddc12fc2de9372f9b4be5a18263089227
Prithvi Prabhu <prithvi@h2o.ai>
2020-07-03 13:43:48 -0700

    Improve tabs example h2oai/q#1080

--- py/examples/tabs.py
+++ py/examples/tabs.py
@@ -4,9 +4,9 @@
 from telesync import Q, listen, ui
 
 tabs = [
-    ui.tab(name='eggs', label='Eggs', icon='Calendar'),
+    ui.tab(name='email', label='Mail', icon='Mail'),
+    ui.tab(name='events', label='Events', icon='Calendar'),
     ui.tab(name='spam', label='Spam'),
-    ui.tab(name='ham', label='Ham'),
 ]
 
 
@@ -14,12 +14,20 @@ async def main(q: Q):
     if q.args.menu:
         q.page['example'].items = [
             ui.tabs(name='menu', value=q.args.menu, items=tabs),
+            get_tab_content(q.args.menu),
         ]
     else:
         q.page['example'] = ui.form_card(box='1 1 4 10', items=[
-            ui.tabs(name='menu', value='eggs', items=tabs),
+            ui.tabs(name='menu', value='email', items=tabs),
+            get_tab_content('email'),
         ])
     await q.page.push()
+
+
+def get_tab_content(category: str):
+    # Return a checklist of dummy items.
+    items = [f'{category.title()} {i}' for i in range(1, 11)]
+    return ui.checklist(name='items', choices=[ui.choice(name=item, label=item) for item in items])
 
 
 if __name__ == '__main__':
