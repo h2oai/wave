3d1022d57ed864df4b2331fa0f1ddd3dbea9429f
Prithvi Prabhu <prithvi@h2o.ai>
2020-07-03 21:58:07 -0700

    Add examples for tab card h2oai/q#1121

---
+++ py/examples/tab_link.py
@@ -0,0 +1,20 @@
+# Tab / Links
+# Use tab cards to display tabs on a page.
+# This examples render tabs styled as links.
+# ---
+from telesync import site, ui
+
+page = site['/demo']
+
+page['tabs'] = ui.tab_card(
+    box='1 1 4 1',
+    items=[
+        ui.tab(name='#menu/spam', label='Spam'),
+        ui.tab(name='#menu/ham', label='Ham'),
+        ui.tab(name='#menu/eggs', label='Eggs'),
+        ui.tab(name='#about', label='About'),
+    ],
+    link=True,
+)
+
+page.sync()
