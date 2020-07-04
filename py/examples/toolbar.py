5563717163d302e2ca0b054aaf9baea3e7d9a32a
Prithvi Prabhu <prithvi@h2o.ai>
2020-07-03 18:24:45 -0700

    Add example for toolbar card h2oai/q#1121

---
+++ py/examples/toolbar.py
@@ -0,0 +1,35 @@
+# Navigation Bar
+# No description available.
+# ---
+from telesync import Q, listen, ui, site
+
+
+async def main(q: Q):
+    q.page['nav'] = ui.toolbar_card(
+        box='1 1 4 1',
+        items=[
+            ui.command(
+                name='new', label='New', icon='Add', items=[
+                    ui.command(name='email', label='Email Message', icon='Mail'),
+                    ui.command(name='calendar', label='Calendar Event', icon='Calendar'),
+                ]
+            ),
+            ui.command(name='upload', label='Upload', icon='Upload'),
+            ui.command(name='share', label='Share', icon='Share'),
+            ui.command(name='download', label='Download', icon='Download'),
+        ],
+        secondary_items=[
+            ui.command(name='tile', caption='Grid View', icon='Tiles'),
+            ui.command(name='info', caption='Info', icon='Info'),
+        ],
+        overflow_items=[
+            ui.command(name='move', label='Move to...', icon='MoveToFolder'),
+            ui.command(name='copy', label='Copy to...', icon='Copy'),
+            ui.command(name='rename', label='Rename', icon='Edit'),
+        ],
+    )
+    await q.page.push()
+
+
+if __name__ == '__main__':
+    listen('/demo', main)
