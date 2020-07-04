ec5b24e811a82c179d28dc8ddd00501ae691215c
Prithvi Prabhu <prithvi@h2o.ai>
2020-07-03 23:19:00 -0700

    Add sidebar nav example  h2oai/q#1123

---
+++ py/examples/nav.py
@@ -0,0 +1,23 @@
+# Nav
+# Use nav cards to display sidebar navigation.
+# ---
+from telesync import site, ui
+
+page = site['/demo']
+
+page['tabs'] = ui.nav_card(
+    box='1 1 2 5',
+    items=[
+        ui.nav_group('Menu', items=[
+            ui.nav_item(name='#menu/spam', label='Spam'),
+            ui.nav_item(name='#menu/ham', label='Ham'),
+            ui.nav_item(name='#menu/eggs', label='Eggs'),
+        ]),
+        ui.nav_group('Help', items=[
+            ui.nav_item(name='#about', label='About'),
+            ui.nav_item(name='#support', label='Support'),
+        ])
+    ],
+)
+
+page.sync()
