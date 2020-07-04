eb3c6e073a544dec79d8d9a312208d04435f47e0
Prithvi Prabhu <prithvi@h2o.ai>
2020-07-03 20:55:54 -0700

    Add example for routing using toolbar h2oai/q#1121

---
+++ py/examples/toolbar_routing.py
@@ -0,0 +1,42 @@
+# Hash Routing
+# This example demonstrates how you can observe and handle changes to the browser's
+# [location hash](https://developer.mozilla.org/en-US/docs/Web/API/Location/hash)
+#
+# The location hash can be accessed using `q.args['#']`.
+#
+# ---
+from telesync import Q, listen, ui, site
+
+
+async def main(q: Q):
+    hash = q.args['#']
+    if hash:
+        blurb = q.page['blurb']
+        if hash == 'menu/spam':
+            blurb.content = "Sorry, we're out of spam!"
+        elif hash == 'menu/ham':
+            blurb.content = "Sorry, we're out of ham!"
+        elif hash == 'menu/eggs':
+            blurb.content = "Sorry, we're out of eggs!"
+        elif hash == 'about':
+            blurb.content = 'Everything here is gluten-free!'
+    else:
+        q.page['nav'] = ui.toolbar_card(
+            box='1 1 4 1',
+            items=[
+                ui.command(name='#menu/spam', label='Spam'),
+                ui.command(name='#menu/ham', label='Ham'),
+                ui.command(name='#menu/eggs', label='Eggs'),
+                ui.command(name='#about', label='About'),
+            ],
+        )
+        q.page['blurb'] = ui.markdown_card(
+            box='1 2 4 2',
+            title='Store',
+            content='Welcome to our store!',
+        )
+    await q.page.push()
+
+
+if __name__ == '__main__':
+    listen('/demo', main)
