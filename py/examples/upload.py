d9e5e8224a39179effbbc7d08985a52d185c6edd
Prithvi Prabhu <prithvi@h2o.ai>
2020-07-06 12:05:48 -0700

    Add upload/download example h2oai/q#1075

---
+++ py/examples/upload.py
@@ -0,0 +1,27 @@
+# File uploads
+# No description available.
+# ---
+import os.path
+from telesync import site, ui
+
+
+def write_csv(filename, rows):
+    with open(filename, 'w') as f:
+        f.write('\n'.join([','.join([str(x) for x in row]) for row in rows]))
+
+
+# Create a couple of fake CSV files
+write_csv('squares.csv', [[x, x * x] for x in range(1, 11)])
+write_csv('cubes.csv', [[x, x * x * x] for x in range(1, 11)])
+
+# Upload CSVs
+squares_path, cubes_path = site.upload(['squares.csv', 'cubes.csv'])
+
+# Display links to these CSVs
+page = site['/demo']
+page['hello'] = ui.markdown_card(
+    box='1 1 2 2',
+    title='Download CSVs',
+    content=f'[Squares]({squares_path}) [Cubes]({cubes_path})',
+)
+page.sync()
