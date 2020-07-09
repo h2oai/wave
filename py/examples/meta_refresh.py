3fceb241df8773899a4530b3c3d37ad519b3d772
Prithvi Prabhu <prithvi@h2o.ai>
2020-07-08 22:36:26 -0700

    Add example for meta/refresh attribute. Closes h2oai/q#1078

---
+++ py/examples/meta_refresh.py
@@ -0,0 +1,17 @@
+# Meta / Refresh
+# Turn off live updates for static pages to conserve server resources.
+# ---
+from telesync import site, ui
+
+page = site['/demo']
+
+# Set refresh rate to zero ("no updates")
+page['meta'] = ui.meta_card(box='', refresh=0)
+
+page['example'] = ui.markdown_card(
+    box='1 1 2 2',
+    title='No updates for you',
+    content='This page stops receiving updates once loaded.',
+)
+
+page.sync()
