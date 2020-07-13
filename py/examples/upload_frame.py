52937a102bd46d3c13c409ded43f7d428c81e183
Prithvi Prabhu <prithvi@h2o.ai>
2020-07-13 11:32:08 -0700

    Add example for displaying iframe content > 2MB. Closes h2oai/q#1122

---
+++ py/examples/upload_frame.py
@@ -0,0 +1,43 @@
+# Uploads / Frame
+# To display content > 2MB in frame cards, first upload the content and then use it in a frame card.
+# ---
+import os
+import uuid
+from telesync import site, ui
+
+html_content = '''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>X</title>
+</head>
+<body>
+    <h2>Hello World!</h2>
+    <p>And now for something completely different...</p>
+</body>
+</html>
+'''
+
+# Create a random filename for our HTML file
+# e.g. 6ad24c6f-54ed-41e0-8458-a2efd5e05952.html
+html_filename = f'{str(uuid.uuid4())}.html'
+
+# Save HTML content to file
+with open(html_filename, 'w') as f:
+    f.write(html_content)
+
+# Upload HTML file
+html_path, = site.upload([html_filename])
+
+# Clean up
+os.remove(html_filename)
+
+# Add a frame card to the page and point it to the HTML file we just uploaded.
+# The path will look something like /_f/adb747ae-999f-460b-8050-3d636594d0cf/6ad24c6f-54ed-41e0-8458-a2efd5e05952.html
+page = site['/demo']
+page['example'] = ui.frame_card(
+    box='1 1 2 3',
+    title='An uploaded frame',
+    path=html_path,
+)
+page.save()
