972f06a8e90c01b2088711f545329e12ccb45bb1
Prithvi Prabhu <prithvi@h2o.ai>
2020-07-13 19:15:49 -0700

    Add wizard example h2oai/q#1081

---
+++ py/examples/wizard.py
@@ -0,0 +1,44 @@
+# Wizard
+# Create a multi-step wizard using form cards.
+# ---
+from telesync import Q, ui, listen
+
+
+async def main(q: Q):
+    if not q.client.initialized:  # First visit, create an empty form card for our wizard
+        q.page['wizard'] = ui.form_card(box='1 1 2 4', items=[])
+        q.client.initialized = True
+
+    wizard = q.page['wizard']  # Get a reference to the wizard form
+    if q.args.step1:
+        wizard.items = [
+            ui.text_xl('Wizard - Step 1'),
+            ui.text("What is your name?"),
+            ui.textbox(name='nickname', label='My name is...', value='Gandalf'),
+            ui.buttons([ui.button(name='step2', label='Next', primary=True)]),
+        ]
+    elif q.args.step2:
+        q.client.nickname = q.args.nickname
+        wizard.items = [
+            ui.text_xl('Wizard - Step 2'),
+            ui.text(f"Hi {q.args.nickname}! How do you feel right now?"),
+            ui.textbox(name='feeling', label='I feel...', value='magical'),
+            ui.buttons([ui.button(name='step3', label='Next', primary=True)]),
+        ]
+    elif q.args.step3:
+        wizard.items = [
+            ui.text_xl('Wizard - Done'),
+            ui.text(f"What a coincidence, {q.client.nickname}! I feel {q.args.feeling} too!"),
+            ui.buttons([ui.button(name='step1', label='Try Again', primary=True)]),
+        ]
+    else:
+        wizard.items = [
+            ui.text_xl('Wizard Example'),
+            ui.text("Let's have a conversation, shall we?"),
+            ui.buttons([ui.button(name='step1', label='Of course!', primary=True)]),
+        ]
+
+    await q.page.save()
+
+
+listen('/demo', main)
