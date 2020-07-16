e3b3f64d9c3213b556bb5b8337a8cf0e4d3bfe19
Prithvi Prabhu <prithvi@h2o.ai>
2020-07-16 14:41:13 -0700

    Bugfix matplotlib example h2oai/q#1157

--- py/examples/plot_matplotlib.py
+++ py/examples/plot_matplotlib.py
@@ -13,32 +13,35 @@ np.random.seed(19680801)
 
 async def main(q: Q):
     if not q.client.initialized:  # First visit
+        q.client.initialized = True
+        q.client.points = 25
+        q.client.alpha = 50
+
         q.page['controls'] = ui.form_card(
             box='1 1 2 3',
             items=[
                 ui.text_xl("Lets make some plots"),
-                ui.slider(name='points', label='Points', min=5, max=50, step=1, value=25, trigger=True),
-                ui.slider(name='alpha', label='Alpha', min=5, max=100, step=1, value=50, trigger=True),
+                ui.slider(name='points', label='Points', min=5, max=50, step=1, value=q.client.points, trigger=True),
+                ui.slider(name='alpha', label='Alpha', min=5, max=100, step=1, value=q.client.alpha, trigger=True),
             ]
         )
         q.page['plot'] = ui.markdown_card(box='3 1 2 3', title='Your plot!', content='')
-        q.client.initialized = True
 
     if q.args.points:
-        # Get arguments
-        points = q.args.points
-        alpha = q.args.alpha / 100.0
-    else:
-        points = 25
-        alpha = 0.5
+        q.client.points = q.args.points
+
+    if q.args.alpha:
+        q.client.alpha = q.args.alpha
+
+    n = q.client.points
 
     # Render plot
     plt.figure(figsize=(2, 2))
     plt.scatter(
-        np.random.rand(points), np.random.rand(points),
-        s=(30 * np.random.rand(points)) ** 2,
-        c=np.random.rand(points),
-        alpha=alpha
+        np.random.rand(n), np.random.rand(n),
+        s=(30 * np.random.rand(n)) ** 2,
+        c=np.random.rand(n),
+        alpha=q.client.alpha / 100.0
     )
     image_filename = f'{str(uuid.uuid4())}.png'
     plt.savefig(image_filename)
