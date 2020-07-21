ccfc7c347eb8fa5213f823925ff56d7ff7810be9
Prithvi Prabhu <prithvi@h2o.ai>
2020-07-21 11:32:26 -0700

    Add path data generator to graphics module h2oai/q#1162

---
+++ py/examples/graphics_path.py
@@ -0,0 +1,16 @@
+# Graphics / Path
+# Use the graphics API to draw a red square.
+# Source: https://codepen.io/dudleystorey/pen/HLBki
+# ---
+from telesync import site, ui, graphics as g
+
+# Use relative coords to draw a square, sort of like Python's turtle package.
+red_square = g.p().m(25, 25).h(50).v(50).h(-50).z().path(fill='red')
+
+page = site['/demo']
+page['example'] = ui.graphics_card(
+    box='1 1 2 3', view_box='0 0 100 100', width='100%', height='100%',
+    data=g.data(foo=red_square),
+)
+
+page.save()
--- py/telesync/graphics.py
+++ py/telesync/graphics.py
@@ -3,6 +3,9 @@ from typing import List, Union
 from .core import data as _data, Data, Ref
 
 
+# TODO add formal parameters for shape functions, including presentation attributes:
+# https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/Presentation
+
 def data(**kwargs) -> Union[str, Data]:
     """
     Create a data placeholder for a graphics card.
@@ -140,3 +143,334 @@ def text(**kwargs):
     :return: Data for the graphical element.
     """
     return _el('t', kwargs)
+
+
+class Path:
+    """
+    A convenience class for drawing SVG paths.
+    """
+
+    def __init__(self):
+        self.__d = []
+
+    def _d(self, command: str, *args) -> 'Path':
+        self.__d.append(command)
+        for arg in args:
+            self.__d.append(str(arg))
+        return self
+
+    def d(self) -> str:
+        """
+        Serialize this path's commands into SVG path data.
+
+        :return: The ``d`` attribute for a SVG path.
+        """
+        return ' '.join(self.__d)
+
+    def path(self, **kwargs):
+        """
+        A SVG path element representing the commands in this ``Path`` instance.
+        Same as calling ``telesync.graphics.path(d=path.d())``
+
+        :param kwargs: Additional attributes for the SVG path element.
+        :return: A SVG path element.
+        """
+        return path(d=self.d(), **kwargs)
+
+    def M(self, x: float, y: float) -> 'Path':
+        """
+        Start a new sub-path at the given (x,y) coordinates.
+        In absolute coordinates.
+
+        See https://www.w3.org/TR/SVG/paths.html#PathDataMovetoCommands
+
+        :param x: x-coordinate
+        :param y: y-coordinate
+        :return: The current ``Path`` instance.
+        """
+        return self._d('M', x, y)
+
+    def m(self, x: float, y: float) -> 'Path':
+        """
+        Start a new sub-path at the given (x,y) coordinates.
+        In relative coordinates.
+
+        See https://www.w3.org/TR/SVG/paths.html#PathDataMovetoCommands
+
+        :param x: x-coordinate
+        :param y: y-coordinate
+        :return: The current ``Path`` instance.
+        """
+        return self._d('m', x, y)
+
+    def Z(self) -> 'Path':
+        """
+        Close the current subpath by connecting it back to the current subpath's initial point.
+
+        See https://www.w3.org/TR/SVG/paths.html#PathDataClosePathCommand
+
+        :return: The current ``Path`` instance.
+        """
+        return self._d('Z')
+
+    def z(self) -> 'Path':
+        """
+        Close the current subpath by connecting it back to the current subpath's initial point.
+
+        See https://www.w3.org/TR/SVG/paths.html#PathDataClosePathCommand
+
+        :return: The current ``Path`` instance.
+        """
+        return self._d('z')
+
+    def L(self, x: float, y: float) -> 'Path':
+        """
+        Draw a line from the current point to the given (x,y) coordinate which becomes the new current point.
+        In absolute coordinates.
+
+        See https://www.w3.org/TR/SVG/paths.html#PathDataLinetoCommands
+
+        :param x: x-coordinate
+        :param y: y-coordinate
+        :return: The current ``Path`` instance.
+        """
+        return self._d('L', x, y)
+
+    def l(self, x: float, y: float) -> 'Path':
+        """
+        Draw a line from the current point to the given (x,y) coordinate which becomes the new current point.
+        In relative coordinates.
+
+        See https://www.w3.org/TR/SVG/paths.html#PathDataLinetoCommands
+
+        :param x: x-coordinate
+        :param y: y-coordinate
+        :return: The current ``Path`` instance.
+        """
+        return self._d('l', x, y)
+
+    def H(self, x: float) -> 'Path':
+        """
+        Draws a horizontal line from the current point.
+        In absolute coordinates.
+
+        See https://www.w3.org/TR/SVG/paths.html#PathDataLinetoCommands
+
+        :param x: x-coordinate
+        :return: The current ``Path`` instance.
+        """
+        return self._d('H', x)
+
+    def h(self, x: float) -> 'Path':
+        """
+        Draws a horizontal line from the current point.
+        In relative coordinates.
+
+        See https://www.w3.org/TR/SVG/paths.html#PathDataLinetoCommands
+
+        :param x: x-coordinate
+        :return: The current ``Path`` instance.
+        """
+        return self._d('h', x)
+
+    def V(self, y: float) -> 'Path':
+        """
+        Draws a vertical line from the current point.
+        In absolute coordinates.
+
+        See https://www.w3.org/TR/SVG/paths.html#PathDataLinetoCommands
+
+        :param y: y-coordinate
+        :return: The current ``Path`` instance.
+        """
+        return self._d('V', y)
+
+    def v(self, y: float) -> 'Path':
+        """
+        Draws a vertical line from the current point.
+        In relative coordinates.
+
+        See https://www.w3.org/TR/SVG/paths.html#PathDataLinetoCommands
+
+        :param y: y-coordinate
+        :return: The current ``Path`` instance.
+        """
+        return self._d('v', y)
+
+    def C(self, x1: float, y1: float, x2: float, y2: float, x: float, y: float) -> 'Path':
+        """
+        Draws a cubic Bézier curve from the current point to (x,y) using (x1,y1) as the control point at the beginning
+        of the curve and (x2,y2) as the control point at the end of the curve.
+        In absolute coordinates.
+
+        See https://www.w3.org/TR/SVG/paths.html#PathDataCubicBezierCommands
+
+        :param x1: x-coordinate of first control point
+        :param y1: x-coordinate of first control point
+        :param x2: y-coordinate of second control point
+        :param y2: y-coordinate of second control point
+        :param x: x-coordinate
+        :param y: y-coordinate
+        :return: The current ``Path`` instance.
+        """
+        return self._d('C', x1, y1, x2, y2, x, y)
+
+    def c(self, x1: float, y1: float, x2: float, y2: float, x: float, y: float) -> 'Path':
+        """
+        Draws a cubic Bézier curve from the current point to (x,y) using (x1,y1) as the control point at the beginning
+        of the curve and (x2,y2) as the control point at the end of the curve.
+        In relative coordinates.
+
+        See https://www.w3.org/TR/SVG/paths.html#PathDataCubicBezierCommands
+
+        :param x1: x-coordinate of first control point
+        :param y1: x-coordinate of first control point
+        :param x2: y-coordinate of second control point
+        :param y2: y-coordinate of second control point
+        :param x: x-coordinate
+        :param y: y-coordinate
+        :return: The current ``Path`` instance.
+        """
+        return self._d('c', x1, y1, x2, y2, x, y)
+
+    def S(self, x2: float, y2: float, x: float, y: float) -> 'Path':
+        """
+        Draws a cubic Bézier curve from the current point to (x,y). The first control point is assumed to be the
+        reflection of the second control point on the previous command relative to the current point.
+        (x2,y2) is the second control point (i.e., the control point at the end of the curve).
+        In absolute coordinates.
+
+        See https://www.w3.org/TR/SVG/paths.html#PathDataCubicBezierCommands
+
+        :param x2: y-coordinate of second control point
+        :param y2: y-coordinate of second control point
+        :param x: x-coordinate
+        :param y: y-coordinate
+        :return: The current ``Path`` instance.
+        """
+        return self._d('S', x2, y2, x, y)
+
+    def s(self, x2: float, y2: float, x: float, y: float) -> 'Path':
+        """
+        Draws a cubic Bézier curve from the current point to (x,y). The first control point is assumed to be the
+        reflection of the second control point on the previous command relative to the current point.
+        (x2,y2) is the second control point (i.e., the control point at the end of the curve).
+        In relative coordinates.
+
+        See https://www.w3.org/TR/SVG/paths.html#PathDataCubicBezierCommands
+
+        :param x2: y-coordinate of second control point
+        :param y2: y-coordinate of second control point
+        :param x: x-coordinate
+        :param y: y-coordinate
+        :return: The current ``Path`` instance.
+        """
+        return self._d('s', x2, y2, x, y)
+
+    def Q(self, x1: float, y1: float, x: float, y: float) -> 'Path':
+        """
+        Draws a quadratic Bézier curve from the current point to (x,y) using (x1,y1) as the control point.
+        In absolute coordinates.
+
+        See https://www.w3.org/TR/SVG/paths.html#PathDataQuadraticBezierCommands
+
+        :param x1: x-coordinate of first control point
+        :param y1: x-coordinate of first control point
+        :param x: x-coordinate
+        :param y: y-coordinate
+        :return: The current ``Path`` instance.
+        """
+        return self._d('Q', x1, y1, x, y)
+
+    def q(self, x1: float, y1: float, x: float, y: float) -> 'Path':
+        """
+        Draws a quadratic Bézier curve from the current point to (x,y) using (x1,y1) as the control point.
+        In relative coordinates.
+
+        See https://www.w3.org/TR/SVG/paths.html#PathDataQuadraticBezierCommands
+
+        :param x1: x-coordinate of first control point
+        :param y1: x-coordinate of first control point
+        :param x: x-coordinate
+        :param y: y-coordinate
+        :return: The current ``Path`` instance.
+        """
+        return self._d('q', x1, y1, x, y)
+
+    def T(self, x: float, y: float) -> 'Path':
+        """
+        Draws a quadratic Bézier curve from the current point to (x,y). The control point is assumed to be the
+        reflection of the control point on the previous command relative to the current point.
+        In absolute coordinates.
+
+        See https://www.w3.org/TR/SVG/paths.html#PathDataQuadraticBezierCommands
+
+        :param x: x-coordinate
+        :param y: y-coordinate
+        :return: The current ``Path`` instance.
+        """
+        return self._d('T', x, y)
+
+    def t(self, x: float, y: float) -> 'Path':
+        """
+        Draws a quadratic Bézier curve from the current point to (x,y). The control point is assumed to be the
+        reflection of the control point on the previous command relative to the current point.
+        In relative coordinates.
+
+        See https://www.w3.org/TR/SVG/paths.html#PathDataQuadraticBezierCommands
+
+        :param x: x-coordinate
+        :param y: y-coordinate
+        :return: The current ``Path`` instance.
+        """
+        return self._d('t', x, y)
+
+    def A(self, rx: float, ry: float, x_axis_rotation: float, large_arc: bool, sweep: bool, x: float,
+          y: float) -> 'Path':
+        """
+        Draws an elliptical arc from the current point to (x, y). The size and orientation of the ellipse are defined
+        by two radii (rx, ry) and an ``x_axis_rotation``, which indicates how the ellipse as a whole is rotated,
+        in degrees, relative to the current coordinate system. The center (cx, cy) of the ellipse is calculated
+        automatically to satisfy the constraints imposed by the other parameters. ``large_arc`` and ``sweep_flag``
+        contribute to the automatic calculations and help determine how the arc is drawn.
+        In absolute coordinates.
+
+        See https://www.w3.org/TR/SVG/paths.html#PathDataEllipticalArcCommands
+
+        :param rx: x-radius
+        :param ry: y-radius
+        :param x_axis_rotation: Rotation in degrees.
+        :param large_arc: Determines if the arc should be greater than or less than 180 degrees.
+        :param sweep: Determines if the arc should begin moving at positive angles or negative ones.
+        :param x: x-coordinate
+        :param y: y-coordinate
+        :return: The current ``Path`` instance.
+        """
+        return self._d('A', rx, ry, x_axis_rotation, 1 if large_arc else 0, 1 if sweep else 0, x, y)
+
+    def a(self, rx: float, ry: float, x_axis_rotation: float, large_arc: bool, sweep: bool, x: float,
+          y: float) -> 'Path':
+        """
+        Draws an elliptical arc from the current point to (x, y). The size and orientation of the ellipse are defined
+        by two radii (rx, ry) and an ``x_axis_rotation``, which indicates how the ellipse as a whole is rotated,
+        in degrees, relative to the current coordinate system. The center (cx, cy) of the ellipse is calculated
+        automatically to satisfy the constraints imposed by the other parameters. ``large_arc`` and ``sweep_flag``
+        contribute to the automatic calculations and help determine how the arc is drawn.
+        In relative coordinates.
+
+        See https://www.w3.org/TR/SVG/paths.html#PathDataEllipticalArcCommands
+
+        :param rx: x-radius
+        :param ry: y-radius
+        :param x_axis_rotation: Rotation in degrees.
+        :param large_arc: Determines if the arc should be greater than or less than 180 degrees.
+        :param sweep: Determines if the arc should begin moving at positive angles or negative ones.
+        :param x: x-coordinate
+        :param y: y-coordinate
+        :return: The current ``Path`` instance.
+        """
+        return self._d('a', rx, ry, x_axis_rotation, 1 if large_arc else 0, 1 if sweep else 0, x, y)
+
+
+def p() -> Path():
+    return Path()
