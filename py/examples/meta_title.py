218e4453e5d77a449198d20c3fd9bf8049b7ef1c
Prithvi Prabhu <prithvi@h2o.ai>
2020-07-31 15:49:17 -0700

    Add example for setting browser window title h2oai/q#1191

---
+++ py/examples/meta_title.py
@@ -0,0 +1,16 @@
+# Meta / Title
+# Set the browser window title for a page
+# ---
+from h2o_q import site, ui
+
+page = site['/demo']
+
+page['meta'] = ui.meta_card(box='', title='And now for something completely different!')
+
+page['example'] = ui.markdown_card(
+    box='1 1 2 2',
+    title='',
+    content='<a href="/demo" target="_blank">Open this page in a new window</a> to view its title.',
+)
+
+page.save()
