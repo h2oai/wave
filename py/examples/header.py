ea69eda908a79d67b738a0d1d5a504c021724dff
Prithvi Prabhu <prithvi@h2o.ai>
2020-07-31 23:25:33 -0700

    Add examples for header card h2oai/q#1194

---
+++ py/examples/header.py
@@ -0,0 +1,32 @@
+# Header
+# Use a header card to display a page header.
+# ---
+from h2o_q import site, ui
+
+page = site['/demo']
+page['header1'] = ui.header_card(
+    box='1 1 3 1',
+    title='The Amazing Gonkulator',
+    subtitle='And now for something completely different!',
+)
+page['header2'] = ui.header_card(
+    box='1 2 3 1',
+    title='The Amazing Gonkulator',
+    subtitle='And now for something completely different!',
+    icon='Design',
+)
+page['header3'] = ui.header_card(
+    box='1 3 3 1',
+    title='The Amazing Gonkulator',
+    subtitle='And now for something completely different!',
+    icon='Cycling',
+    icon_color='$violet',
+)
+page['header4'] = ui.header_card(
+    box='1 4 3 1',
+    title='The Amazing Gonkulator',
+    subtitle='And now for something completely different!',
+    icon='ExploreData',
+    icon_color='$red',
+)
+page.save()
