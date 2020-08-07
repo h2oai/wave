26a02de9eb24ff2712812ec118e18489155ed7ef
Prithvi Prabhu <prithvi@h2o.ai>
2020-08-06 21:44:19 -0700

    Add examples for frame cards h2oai/q#1157

---
+++ py/examples/frame.py
@@ -0,0 +1,23 @@
+# Frame
+# Use a frame card to display HTML content.
+# ---
+from h2o_q import site, ui
+
+html = '''
+<!DOCTYPE html>
+<html>
+<body>
+  <h1>Hello World!</h1>
+</body>
+</html>
+'''
+
+page = site['/demo']
+
+page['example'] = ui.frame_card(
+    box='1 1 2 2',
+    title='Example',
+    content=html,
+)
+
+page.save()
