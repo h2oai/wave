6d6245bb4b7546844c8011bc85276f09c0d92986
Prithvi Prabhu <prithvi@h2o.ai>
2020-08-06 21:43:20 -0700

    Add examples for inline frames h2oai/q#1148

---
+++ py/examples/form_frame.py
@@ -0,0 +1,24 @@
+# Form / Frame
+# Use a frame component in a form card to display HTML content inline.
+# ---
+from h2o_q import site, ui
+
+html = '''
+<!DOCTYPE html>
+<html>
+<body>
+  <h1>Hello World!</h1>
+</body>
+</html>
+'''
+
+page = site['/demo']
+
+page['example'] = ui.form_card(
+    box='1 1 2 2',
+    items=[
+        ui.frame(content=html, height='100px')
+    ]
+)
+
+page.save()
