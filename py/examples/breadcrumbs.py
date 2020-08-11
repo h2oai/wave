d8eca1fc569bb3ca186ba5a94b2e2ada7f0e67e3
Martin Turoci <martin.turoci@h2o.ai>
2020-08-11 17:41:28 +0200

    docs: Replace own breadcrumbs docs with official Fluent's h2oai/q#1214

--- py/examples/breadcrumbs.py
+++ py/examples/breadcrumbs.py
@@ -1,5 +1,10 @@
 # Breadcrumbs
-# Use breadcrumbs cards to show actual position within navigation.
+# Breadcrumbs should be used as a navigational aid in your app or site.
+# They indicate the current page’s location within a hierarchy and help
+# the user understand where they are in relation to the rest of that hierarchy.
+# They also afford one-click access to higher levels of that hierarchy. 
+# Breadcrumbs are typically placed, in horizontal form, under the masthead 
+# or navigation of an experience, above the primary content area.
 # ---
 from h2o_q import site, ui
 
--- ui/src/breadcrumbs.tsx
+++ ui/src/breadcrumbs.tsx
@@ -10,7 +10,15 @@ interface BreadcrumbItem {
   /** The label to display. */
   label: S
 }
-/** Create a card containing navigation breadcrumbs. */
+/**
+ * Create a card containing breadcrumbs.
+ * Breadcrumbs should be used as a navigational aid in your app or site.
+ * They indicate the current page’s location within a hierarchy and help
+ * the user understand where they are in relation to the rest of that hierarchy.
+ * They also afford one-click access to higher levels of that hierarchy. 
+ * Breadcrumbs are typically placed, in horizontal form, under the masthead 
+ * or navigation of an experience, above the primary content area.
+ */
 interface State {
   /** Items to render. */
   items: BreadcrumbItem[]
