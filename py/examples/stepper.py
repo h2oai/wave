82f49068842ac8042c034ed7633c279bfe96cc35
Martin Turoci <martin.turoci@h2o.ai>
2020-08-27 15:50:48 +0200

    fix: Refactor #61

--- py/examples/stepper.py
+++ py/examples/stepper.py
@@ -8,7 +8,7 @@ async def main(q: Q):
     q.page['basic-stepper'] = ui.stepper_card(
         box='1 1 4 1',
         name='stepper',
-        steps=[
+        items=[
           ui.step(label='Step 1'),
           ui.step(label='Step 2'),
           ui.step(label='Step 3'),
@@ -17,7 +17,7 @@ async def main(q: Q):
     q.page['icon-stepper'] = ui.stepper_card(
         box='1 2 4 1',
         name='stepper',
-        steps=[
+        items=[
           ui.step(label='Step 1', icon='MailLowImportance'),
           ui.step(label='Step 2', icon='TaskManagerMirrored'),
           ui.step(label='Step 3', icon='Cafe'),
@@ -26,7 +26,7 @@ async def main(q: Q):
     q.page['almost-done-stepper'] = ui.stepper_card(
         box='1 3 4 1',
         name='stepper',
-        steps=[
+        items=[
           ui.step(label='Step 1', done=True),
           ui.step(label='Step 2', done=True),
           ui.step(label='Step 3'),
