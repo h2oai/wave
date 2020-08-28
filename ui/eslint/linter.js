b2dadb071123a3f1d0cd1d3c2fc9103a07fb59cb
Martin Turoci <martin.turoci@h2o.ai>
2020-08-28 09:20:02 +0200

    style: Custom Eslint rule for data-test attributes on component wrappers #55

--- ui/eslint/linter.js
+++ ui/eslint/linter.js
@@ -13,12 +13,35 @@ module.exports = {
               const isBoxed = init.callee.name === 'box'
               const varName = id.name
               if (isBoxed && !varName.endsWith('B')) {
-                context.report(node, `${varName} should be suffixed with B since it is a boxed.`)
+                context.report(node, `${varName} should be suffixed with B since it is boxed.`)
               }
             }
           })
         }
       })
-    }
+    },
+    "data-test-on-component": {
+      meta: {
+        type: 'suggestion',
+      },
+      create: context => ({
+        VariableDeclaration: node => {
+          if (!node.declarations) return
+
+          node.declarations.forEach(({ init, id }) => {
+            if (init && id.name === 'render' && init.body.body) {
+              const { argument } = init.body.body.find(({ type }) => type === 'ReturnStatement')
+              const found =
+                argument.type === 'JSXElement' &&
+                argument.openingElement.attributes &&
+                argument.openingElement.attributes.length &&
+                argument.openingElement.attributes.find((({ name }) => name.name === 'data-test'))
+
+              if (!found) context.report(node, `Wrapper component in render must have a data-test attribute.`)
+            }
+          })
+        }
+      })
+    },
   }
 }
