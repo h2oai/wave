09c898b68298e09401b88a90a6a506a14e8e9d36
= <1964678+ashrith@users.noreply.github.com>
2020-09-09 11:39:29 -0400

    added stat_small and new hello_world

---
+++ r/R/examples/stat_small.R
@@ -0,0 +1,17 @@
+source("../core.R") 
+
+value_list = c("$85.00","$100","$343.43","$1.00","$10","$30","$54")
+test_page <- page("/page_demo")
+
+test_page <- page.add(test_page,"/page_demo","test",ui_small_stat_card(box="1 1 1 1",title="test",value="$55.05"))
+
+page.save(test_page,"/page_demo")
+
+
+while(TRUE)
+{
+        Sys.sleep(3)
+        test_page[[1]]$value$value <- sample(value_list,1)
+        page.save(test_page,"/page_demo")
+}
+
