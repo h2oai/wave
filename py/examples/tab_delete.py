8f0e07e92d98e1c6c07a6e12054162d5eca67f41
Gabor Fodor <fodgabor.math@gmail.com>
2020-09-18 12:55:07 +0200

    feat: Add example for tabs and card delete

---
+++ py/examples/tab_delete.py
@@ -0,0 +1,43 @@
+# Tabs
+# Navigate between two or more tabs.
+# Delete the cards when switching between tabs.
+# ---
+from h2o_q import Q, listen, ui
+
+TABS = 'abcde'
+
+
+async def display_tab(q):
+    q.page[f'example_{q.client.tab}'] = ui.markup_card(
+        box='1 2 4 3',
+        title=q.client.tab.upper(),
+        content='\n'.join([''.join([q.client.tab] * 10) for _ in range(50)])
+    )
+    await q.page.save()
+
+
+async def remove_cards(q: Q):
+    for tab in TABS:
+        del q.page[f'example_{tab}']
+    await q.page.save()
+
+
+async def main(q: Q):
+    if not q.client.initialized:
+        q.client.tab = 'a'
+        q.page['tabs'] = ui.tab_card(  # Initialize once
+            box='1 1 4 1',
+            items=[ui.tab(name=f'#{t}', label=t.upper()) for t in TABS]
+        )
+        q.client.initialized = True
+
+    if q.args['#']:
+        q.client.tab = str(q.args['#'])
+
+    await remove_cards(q)
+    await display_tab(q)
+    await q.page.save()
+
+
+if __name__ == '__main__':
+    listen('/demo', main)
