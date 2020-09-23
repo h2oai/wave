e934e8f1d00c950137a1acd2b000f8389deeedae
Prithvi Prabhu <prithvi@h2o.ai>
2020-09-23 12:05:33 -0700

    Add table example using backend search #23

---
+++ py/examples/table_filter_backend.py
@@ -0,0 +1,54 @@
+# Table / Filter / Backend
+# Filter table using Python
+# ---
+import pandas as pd
+from faker import Faker
+from h2o_q import Q, listen, ui
+
+fake = Faker()
+
+N = 50  # number of rows
+
+# Make a synthetic data frame
+addresses = pd.DataFrame(dict(
+    ID=[i + 1 for i in range(N)],
+    Name=[fake.name() for _ in range(N)],
+    Language=[fake.language_name() for _ in range(N)],
+    Job=[fake.job() for _ in range(N)],
+    Address=[fake.address() for _ in range(N)],
+    City=[fake.city() for _ in range(N)],
+))
+
+column_names = ['ID', 'Name', 'Language', 'Job', 'Address', 'City']
+
+
+def df_to_rows(df: pd.DataFrame):
+    return [ui.table_row(row['ID'], [row[name] for name in column_names]) for i, row in df.iterrows()]
+
+
+def search_df(df: pd.DataFrame, term: str):
+    str_cols = df.select_dtypes(include=[object])
+    return df[str_cols.apply(lambda column: column.str.contains(term, case=False, na=False)).any(axis=1)]
+
+
+async def main(q: Q):
+    if not q.client.initialized:
+        q.page['form'] = ui.form_card(box='1 1 -1 11', items=[
+            ui.textbox(name='search', label='Search', placeholder='Enter a keyword...', trigger=True),
+            ui.table(
+                name='issues',
+                columns=[ui.table_column(name=name, label=name) for name in column_names],
+                rows=df_to_rows(addresses)
+            )
+        ])
+        q.client.initialized = True
+    else:
+        table = q.page['form'].items[1].table
+        term: str = q.args.search
+        term = term.strip() if term else ''
+        table.rows = df_to_rows(search_df(addresses, term) if len(term) else addresses)
+
+    await q.page.save()
+
+
+listen('/demo', main)
