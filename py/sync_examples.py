9bbd64421f1591f4a8b770f523c6d89cc1a916fd
Prithvi Prabhu <prithvi@h2o.ai>
2020-10-05 20:01:21 -0700

    docs: Add table of contents for examples #23

--- py/sync_examples.py
+++ py/sync_examples.py
@@ -13,13 +13,12 @@ class Example:
         self.slug = self.name.replace('_', '-')
         self.filename = filename
         self.title = title
+        self.subtitle = description.split('\n')[0].strip()
         self.description = description
         self.code = code
 
     def to_md(self):
-        return f"""
----
-id: {self.slug}        
+        return f"""---
 title: {self.title}
 ---
 
@@ -30,7 +29,7 @@ title: {self.title}
 ```py
 {self.code}
 ```
-        """.strip()
+        """
 
 
 def read_lines(p: str) -> List[str]:
@@ -60,6 +59,14 @@ def load_example(filename: str) -> Example:
     title, description = strip_comment(header[0]), [strip_comment(x) for x in header[1:]]
     return Example(filename, title, '\n'.join(description), code)
 
+def build_index(examples: List[Example]):
+    return f'''---
+title: Contents
+slug: /examples
+---
+
+''' + '\n'.join([f'- [{e.title}](examples/{e.slug}): {e.subtitle}' for e in examples])
+
 
 def main():
     filenames = [line.strip() for line in read_lines(os.path.join(example_dir, 'tour.conf')) if
@@ -74,8 +81,10 @@ def main():
         if not os.path.exists(os.path.join(thumbnail_dir, f'{e.slug}.png')):
             print(f'*** ALERT: no thumbnail found for example "{e.slug}"')
 
-    example_items = [dict(slug=e.slug, title=e.title) for e in examples]
+    example_items = [dict(slug=e.slug) for e in examples]
+    example_items.insert(0, dict(slug='index'))
     write_file(os.path.join(website_dir, 'examples.js'), f'module.exports={json.dumps(example_items)}')
+    write_file(os.path.join(example_md_dir, 'index.md'), build_index(examples))
 
 
 if __name__ == '__main__':
