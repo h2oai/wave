9326a4279d52bfd5c5c201d9b93b118c61b38356
Martin Turoci <martin.turoci@h2o.ai>
2020-10-15 14:43:08 +0200

    chore: add datepicker trigger tour example #108

--- py/examples/date_picker_trigger.py
+++ py/examples/date_picker_trigger.py
@@ -1,4 +1,4 @@
-# Form / Date Picker
+# Form / Date Picker / Trigger
 # Use date picker with trigger flag to sync user input after selection.
 # ---
 from h2o_q import Q, listen, ui
@@ -8,11 +8,12 @@ async def main(q: Q):
     if not q.client.initialized:
         q.page['example'] = ui.form_card(box='1 1 4 10', items=[
             ui.text(f'date_trigger={q.args.date_trigger}'),
-            ui.date_picker(name='date_trigger', label='Date picker with trigger', trigger=True),
+            ui.date_picker(name='date_trigger', label='Date picker with trigger', trigger=True)
         ])
         q.client.initialized = True
     else:
-        q.page['example'].items[0] = ui.text(f'date_trigger={q.args.date_trigger}')
+        q.page['example'].items[0].text.content = f'date_trigger={q.args.date_trigger}'
+        q.page['example'].items[1].date_picker.value = q.args.date_trigger
     await q.page.save()
 
 
