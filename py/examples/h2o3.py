c086015c599b2d5f571f09b4d69b407bc545e3a2
geomodular <peter.szabo@h2o.ai>
2020-11-05 15:05:50 +0100

    feat: update example to reflect a new design

--- py/examples/h2o3.py
+++ py/examples/h2o3.py
@@ -1,22 +1,21 @@
-import h2o
-from h2o_wave import site, data, ui, build_model, get_model
+from h2o_wave import site, data, ui
+from h2o_wave.ml import build_model
 import datatable as dt
 
 page = site['/predictions']
 
 target = 'Fuel_Price'
-train_set = 'walmart_train.csv'
-test_set = 'walmart_test.csv'
-
-model = build_model(train_set, target=target)
-prediction_set = model.predict(test_set)
+train_set = '/Users/geomodular/Datasets/walmart_train.csv'
+test_set = '/Users/geomodular/Datasets/walmart_test.csv'
 
 df_train = dt.fread(train_set)
 df_test = dt.fread(test_set)
-df_predictions = dt.fread(prediction_set)
+
+model = build_model(train_set, target=target)
+prediction = model.predict(df_test.to_tuples())
 
 n_train = 50
-n_test = 10
+n_test = df_test.nrows
 n_total = n_train + n_test
 v = page.add('example', ui.plot_card(
     box='1 1 4 5',
@@ -27,7 +26,7 @@ v = page.add('example', ui.plot_card(
 
 # We are taking the last `n_train` values from the train set.
 values_train = [(df_train[-i, 'Date'], df_train[-i, 'Fuel_Price']) for i in reversed(range(1, n_train + 1))]
-values_test = [(df_test[i, 'Date'], df_predictions[i, 0]) for i in range(n_test)]
+values_test = [(df_test[i, 'Date'], prediction[i][0]) for i in range(n_test)]
 
 v.data = values_train + values_test
 
