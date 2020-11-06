5520eb4aeae3f440b7a0f0bf4244adcdd0ad6836
Prithvi Prabhu <prithvi@h2o.ai>
2020-11-06 13:50:20 -0800

    Add comments to example

--- py/examples/layout_grids.py
+++ py/examples/layout_grids.py
@@ -1,5 +1,5 @@
-# Layout / Size
-# How to adjust the size of cards on a page.
+# Layout / Grids
+# How to create a responsive layout.
 # ---
 
 from h2o_wave import site, ui
@@ -7,26 +7,42 @@ from h2o_wave import site, ui
 page = site['/demo']
 page.drop()
 
+# The meta card's 'grids' attribute defines how to lay out cards for different viewport sizes.
+# We define three layout grids here.
 page['meta'] = ui.meta_card(box='', grids=[
     ui.grid(
+        # If the viewport width >= 0:
         breakpoint=0,
+        # Create one column, sized automatically.
         columns=['auto'],
+        # Create 5 rows: 100px for the header, and 4 others for content, sized automatically.
         rows=['100px'] + ['auto'] * 4,
     ),
     ui.grid(
+        # If the viewport width >= 768:
         breakpoint=768,
+        # Create 2 columns: 250px for the sidebar, and another for the content, sized automatically.
         columns=['250px', 'auto'],
+        # Create 4 rows: 100px for the header, and 3 others for content, sized automatically.
         rows=['100px'] + ['auto'] * 3,
     ),
     ui.grid(
+        # If the viewport width >= 1200:
         breakpoint=1200,
+        # Create 3 columns: 300px for the sidebar, and 2 others taking up 1/2 and 1/2 of the remaining width.
         columns=['300px', '1fr', '1fr'],
+        # Create 3 rows: 100px for the header, and 2 others taking up 1/3rd and 2/3rd of the remaining height.
         rows=['100px', '1fr', '2fr'],
+        # Fix width to 1200px.
         width='1200px',
     ),
 ])
 
 page['header'] = ui.markdown_card(
+    # The box attribute for a card specifies where to place the card for each of our three viewport sizes above.
+    # If the viewport width >= 0: use 1 1 1 1.
+    # If the viewport width >= 768: use 1 1 2 1.
+    # If the viewport width >= 1200: use 1 1 3 1.
     box='1 1 1 1 / 1 1 2 1 / 1 1 3 1',
     title='Header',
     content='',
