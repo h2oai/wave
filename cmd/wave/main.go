99891cc19e5c3cd2b11264101ac7db1edfc07370
Michal Raska <michal.raska@h2o.ai>
2020-11-09 13:41:13 +0100

    feat: use env as default for OpenID config

--- cmd/wave/main.go
+++ cmd/wave/main.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
-	"strconv"
 	"strings"
 )
 
@@ -31,22 +30,40 @@ func main() {
 		version bool
 	)
 
-	boolFlagWithDefault(&version, "version", false, "print version and exit")
-	stringFlagWithDefault(&conf.Listen, "listen", ":55555", "listen on this address")
-	stringFlagWithDefault(&conf.WebDir, "web-dir", "./www", "directory to serve web assets from")
-	stringFlagWithDefault(&conf.DataDir, "data-dir", "./data", "directory to store site data")
-	stringFlagWithDefault(&conf.AccessKeyID, "access-key-id", "access_key_id", "default access key ID")
-	stringFlagWithDefault(&conf.AccessKeySecret, "access-key-secret", "access_key_secret", "default access key secret")
-	stringFlagWithDefault(&conf.Init, "init", "", "initialize site content from AOF log")
-	stringFlagWithDefault(&conf.Compact, "compact", "", "compact AOF log")
-	stringFlagWithDefault(&conf.CertFile, "tls-cert-file", "", "path to certificate file (TLS only)")
-	stringFlagWithDefault(&conf.KeyFile, "tls-key-file", "", "path to private key file (TLS only)")
-	boolFlagWithDefault(&conf.Debug, "debug", false, "enable debug mode (profiling, inspection, etc.)")
-	stringFlagWithDefault(&conf.OIDCClientID, "oidc-client-id", "", "OIDC client ID")
-	stringFlagWithDefault(&conf.OIDCClientSecret, "oidc-client-secret", "", "OIDC client secret")
-	stringFlagWithDefault(&conf.OIDCProviderURL, "oidc-provider-url", "", "OIDC provider URL")
-	stringFlagWithDefault(&conf.OIDCRedirectURL, "oidc-redirect-url", "", "OIDC redirect URL")
-	stringFlagWithDefault(&conf.OIDCEndSessionURL, "oidc-end-session-url", "", "OIDC end session URL")
+	flag.BoolVar(&version, "version", false, "print version and exit")
+	flag.StringVar(&conf.Listen, "listen", ":55555", "listen on this address")
+	flag.StringVar(&conf.WebDir, "web-dir", "./www", "directory to serve web assets from")
+	flag.StringVar(&conf.DataDir, "data-dir", "./data", "directory to store site data")
+	flag.StringVar(&conf.AccessKeyID, "access-key-id", "access_key_id", "default access key ID")
+	flag.StringVar(&conf.AccessKeySecret, "access-key-secret", "access_key_secret", "default access key secret")
+	flag.StringVar(&conf.Init, "init", "", "initialize site content from AOF log")
+	flag.StringVar(&conf.Compact, "compact", "", "compact AOF log")
+	flag.StringVar(&conf.CertFile, "tls-cert-file", "", "path to certificate file (TLS only)")
+	flag.StringVar(&conf.KeyFile, "tls-key-file", "", "path to private key file (TLS only)")
+	flag.BoolVar(&conf.Debug, "debug", false, "enable debug mode (profiling, inspection, etc.)")
+
+	const (
+		oidcClientID      = "oidc-client-id"
+		oidcClientSecret  = "oidc-client-secret"
+		oidcProviderURL   = "oidc-provider-url"
+		oidcRedirectURL   = "oidc-redirect-url"
+		oidcEndSessionURL = "oidc-end-session-url"
+	)
+
+	conf.OIDCClientID = os.Getenv(envVarName(oidcClientID))
+	flag.StringVar(&conf.OIDCClientID, oidcClientID, conf.OIDCClientID, "OIDC client ID")
+
+	conf.OIDCClientSecret = os.Getenv(envVarName(oidcClientSecret))
+	flag.StringVar(&conf.OIDCClientSecret, oidcClientSecret, conf.OIDCClientSecret, "OIDC client secret")
+
+	conf.OIDCProviderURL = os.Getenv(envVarName(oidcProviderURL))
+	flag.StringVar(&conf.OIDCProviderURL, oidcProviderURL, conf.OIDCProviderURL, "OIDC provider URL")
+
+	conf.OIDCRedirectURL = os.Getenv(envVarName(oidcRedirectURL))
+	flag.StringVar(&conf.OIDCRedirectURL, oidcRedirectURL, conf.OIDCRedirectURL, "OIDC redirect URL")
+
+	conf.OIDCEndSessionURL = os.Getenv(envVarName(oidcEndSessionURL))
+	flag.StringVar(&conf.OIDCEndSessionURL, oidcEndSessionURL, conf.OIDCEndSessionURL, "OIDC end session URL")
 
 	flag.Parse()
 
@@ -64,39 +81,7 @@ func main() {
 	wave.Run(conf)
 }
 
-func stringFlagWithDefault(p *string, name string, defaultValue string, usage string) {
-	flag.StringVar(p, name, lookupStringEnv(envVarName(name), defaultValue), usage)
-}
-
-func boolFlagWithDefault(p *bool, name string, defaultValue bool, usage string) {
-	envVal, err := lookupBoolEnv(envVarName(name), defaultValue)
-	if err != nil {
-		panic(err)
-	}
-	flag.BoolVar(p, name, envVal, usage)
-}
-
 func envVarName(n string) string {
 	envVar := strings.ToUpper(strings.ReplaceAll(n, "-", "_"))
 	return fmt.Sprintf("%s_%s", envVarNamePrefix, envVar)
-}
-
-func lookupStringEnv(name string, defaultValue string) string {
-	val, found := os.LookupEnv(name)
-	if !found {
-		return defaultValue
-	}
-	return val
-}
-
-func lookupBoolEnv(name string, defaultValue bool) (bool, error) {
-	rawVal, found := os.LookupEnv(name)
-	if !found {
-		return defaultValue, nil
-	}
-	val, err := strconv.ParseBool(rawVal)
-	if err != nil {
-		return false, fmt.Errorf("failed parsing %s: %v", name, err)
-	}
-	return val, nil
 }
