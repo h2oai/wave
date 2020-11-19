8a3099ad8f65cc294592175b33a3bfa2afda2626
geomodular <peter.szabo@h2o.ai>
2020-11-19 14:49:49 +0100

    feat: simplify example

--- py/examples/h2o3.py
+++ py/examples/h2o3.py
@@ -4,30 +4,25 @@ import datatable as dt
 
 page = site['/predictions']
 
-target = 'Fuel_Price'
-train_set = 'walmart_train.csv'
-test_set = 'walmart_test.csv'
+train_set = '/Users/geomodular/Datasets/walmart_small_train.csv'
+test_set = '/Users/geomodular/Datasets/walmart_small_test.csv'
+
+model = build_model(train_set, target='Weekly_Sales')
+prediction = model.predict(test_set)
 
 df_train = dt.fread(train_set)
 df_test = dt.fread(test_set)
 
-model = build_model(train_set, target=target)
-prediction = model.predict(test_set)
+# We are taking the last 50 values from the train set.
+values_train = df_train[-50:, ['Date', 'Weekly_Sales']].to_tuples()
+# And continue using next values from test set.
+values_test = [(df_test[i, 'Date'], prediction[i][0]) for i in range(df_test.nrows)]
 
-n_train = 50
-n_test = df_test.nrows
-n_total = n_train + n_test
 v = page.add('example', ui.plot_card(
     box='1 1 4 5',
     title='Line',
-    data=data('date price', n_total),
-    plot=ui.plot([ui.mark(type='line', x_scale='time', x='=date', y='=price', y_min=0)])
+    data=data('date sales', rows=values_train + values_test),
+    plot=ui.plot([ui.mark(type='line', x_scale='time', x='=date', y='=sales', y_min=0)])
 ))
-
-# We are taking the last `n_train` values from the train set.
-values_train = [(df_train[-i, 'Date'], df_train[-i, 'Fuel_Price']) for i in reversed(range(1, n_train + 1))]
-values_test = [(df_test[i, 'Date'], prediction[i][0]) for i in range(n_test)]
-
-v.data = values_train + values_test
 
 page.save()
