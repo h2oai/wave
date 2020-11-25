f072942861beafb123c46a3a860a6ce5349ced95
Prithvi Prabhu <prithvi@h2o.ai>
2020-11-25 13:02:24 -0800

    Improve dialog example

--- py/examples/meta_dialog.py
+++ py/examples/meta_dialog.py
@@ -9,27 +9,29 @@ async def serve(q: Q):
     if not q.client.initialized:
         q.page['meta'] = ui.meta_card(box='')
         q.page['example'] = ui.form_card(box='1 1 -1 -1', items=[
-            ui.button(name='show_dialog', label='Show dialog')
+            ui.button(name='show_dialog', label='Order donuts', primary=True)
         ])
         q.client.initialized = True
-    if q.args.show_dialog:
-        q.page['meta'].dialog = ui.dialog(title='Dialog Title', items=[
-            ui.text('Dialog content'),
-            ui.buttons([ui.button(name='next', label='Next', primary=True)])
-        ])
-    if q.args.next:
-        q.page['meta'].dialog.items = [
-            ui.text('Final step'),
-            ui.buttons([
-                ui.button(name='cancel', label='Cancel'),
-                ui.button(name='submit', label='Submit', primary=True),
+    else:
+        if q.args.show_dialog:
+            q.page['meta'].dialog = ui.dialog(title='Order Donuts', items=[
+                ui.text('Donuts cost $1.99. Proceed?'),
+                ui.buttons([ui.button(name='next_step', label='Next', primary=True)])
             ])
-        ]
+        elif q.args.next_step:
+            q.page['meta'].dialog.items = [
+                ui.text('You will be charged $1.99. Proceed?'),
+                ui.buttons([
+                    ui.button(name='cancel', label='Back to safety'),
+                    ui.button(name='submit', label='Place order', primary=True),
+                ])
+            ]
+        elif q.args.submit:
+            q.page['example'].items = [ui.message_bar('success', 'Order placed!')]
+            q.page['meta'].dialog = None
 
-    if q.args.submit:
-        q.page['example'].items = [ui.text('Dialog submitted')]
-
-    if q.args.cancel or q.args.submit:
-        q.page['meta'].dialog = None
+        elif q.args.cancel:
+            q.page['example'].items = [ui.message_bar('info', 'Order canceled!')]
+            q.page['meta'].dialog = None
 
     await q.page.save()
