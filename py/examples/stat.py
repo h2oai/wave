a00a53db4ede25261e07e2ff622727c962d96e38
Dulaj Atapattu <dulaj.atapattu@h2o.ai>
2020-12-11 15:45:16 +0530

    Remove foo bar like variables

--- py/examples/stat.py
+++ py/examples/stat.py
@@ -14,16 +14,16 @@ mock_description = 'Your description goes here'
 
 fakePercent = FakePercent()
 fakeSeries = FakeCategoricalSeries()
-val, pc = fakePercent.next()
+val, percentage = fakePercent.next()
 cat_s, val_s, pc_s = fakeSeries.next()
 
 # Stat cards
 page.add('large_stat_card', ui.large_stat_card(
     box='1 1 2 2',
     title="Large Stat Card",
-    value='=${{intl qux minimum_fraction_digits=2 maximum_fraction_digits=2}}',
-    aux_value='={{intl quux style="percent" minimum_fraction_digits=1 maximum_fraction_digits=1}}',
-    data=dict(qux=val, quux=pc),
+    value='={{intl value minimum_fraction_digits=2 maximum_fraction_digits=2}}',
+    aux_value='={{intl aux_value style="percent" minimum_fraction_digits=1 maximum_fraction_digits=1}}',
+    data=dict(value=val, aux_value=percentage),
     caption=mock_description,
 ))
 
@@ -37,45 +37,45 @@ page.add('small_stat_card', ui.small_stat_card(
 page.add('tall_gauge_stat_card', ui.tall_gauge_stat_card(
     box='1 3 1 2',
     title='Tall Gauge',
-    value='=${{intl foo minimum_fraction_digits=2 maximum_fraction_digits=2}}',
-    aux_value='={{intl bar style="percent" minimum_fraction_digits=2 maximum_fraction_digits=2}}',
+    value='=${{intl value minimum_fraction_digits=2 maximum_fraction_digits=2}}',
+    aux_value='={{intl aux_value style="percent" minimum_fraction_digits=2 maximum_fraction_digits=2}}',
     plot_color='$red',
-    progress=pc,
-    data=dict(foo=val, bar=pc),
+    progress=percentage,
+    data=dict(value=val, aux_value=percentage),
 ))
 
 page.add('wide_gauge_stat_card', ui.wide_gauge_stat_card(
     box='2 3 2 1',
     title='Wide Gauge',
-    value='=${{intl foo minimum_fraction_digits=2 maximum_fraction_digits=2}}',
-    aux_value='={{intl bar style="percent" minimum_fraction_digits=2 maximum_fraction_digits=2}}',
+    value='=${{intl value minimum_fraction_digits=2 maximum_fraction_digits=2}}',
+    aux_value='={{intl aux_value style="percent" minimum_fraction_digits=2 maximum_fraction_digits=2}}',
     plot_color='$red',
-    progress=pc,
-    data=dict(foo=val, bar=pc),
+    progress=percentage,
+    data=dict(value=val, aux_value=percentage),
 ))
 
 # Bar cards
 page.add('large_bar_stat_card', ui.large_bar_stat_card(
     box='1 5 2 2',
     title='Large Bar',
-    value='=${{intl foo minimum_fraction_digits=2 maximum_fraction_digits=2}}',
+    value='=${{intl value minimum_fraction_digits=2 maximum_fraction_digits=2}}',
     value_caption='This Month',
-    aux_value='={{intl bar style="percent" minimum_fraction_digits=2 maximum_fraction_digits=2}}',
+    aux_value='={{intl aux_value style="percent" minimum_fraction_digits=2 maximum_fraction_digits=2}}',
     aux_value_caption='Previous Month',
     plot_color='$red',
-    progress=pc,
-    data=dict(foo=val, bar=pc),
+    progress=percentage,
+    data=dict(value=val, aux_value=percentage),
     caption=mock_description,
 ))
 
 page.add('wide_bar_stat_card', ui.wide_bar_stat_card(
     box='3 5 2 1',
     title='Wide Bar',
-    value='=${{intl foo minimum_fraction_digits=2 maximum_fraction_digits=2}}',
-    aux_value='={{intl bar style="percent" minimum_fraction_digits=2 maximum_fraction_digits=2}}',
+    value='=${{intl value minimum_fraction_digits=2 maximum_fraction_digits=2}}',
+    aux_value='={{intl aux_value style="percent" minimum_fraction_digits=2 maximum_fraction_digits=2}}',
     plot_color='$red',
-    progress=pc,
-    data=dict(foo=val, bar=pc),
+    progress=percentage,
+    data=dict(value=val, aux_value=percentage),
 ))
 
 # Time series data cards
@@ -85,13 +85,13 @@ for i in range(len(curves)):
     page.add(f'small_series_stat_card_area_{i}', ui.small_series_stat_card(
         box=f'{i + 1} 7 1 1',
         title="Series",
-        value='=${{intl qux minimum_fraction_digits=2 maximum_fraction_digits=2}}',
-        data=dict(qux=val_s, quux=pc_s),
-        plot_category='foo',
+        value='=${{intl value minimum_fraction_digits=2 maximum_fraction_digits=2}}',
+        data=dict(value=val_s, aux_value=pc_s),
+        plot_category='category',
         plot_type='area',
-        plot_value='qux',
+        plot_value='value',
         plot_color=colors[i],
-        plot_data=data('foo qux', -15),
+        plot_data=data('category value', -15),
         plot_zero_value=0,
         plot_curve=curves[i],
     ))
@@ -99,13 +99,13 @@ for i in range(len(curves)):
 page.add('small_series_stat_card_interval', ui.small_series_stat_card(
     box='1 8 1 1',
     title='Series',
-    value='=${{intl qux minimum_fraction_digits=2 maximum_fraction_digits=2}}',
-    data=dict(qux=val_s, quux=pc_s),
-    plot_category='foo',
+    value='=${{intl value minimum_fraction_digits=2 maximum_fraction_digits=2}}',
+    data=dict(value=val_s, aux_value=pc_s),
+    plot_category='category',
     plot_type='interval',
-    plot_value='qux',
+    plot_value='value',
     plot_color='$red',
-    plot_data=data('foo qux', -20),
+    plot_data=data('category value', -20),
     plot_zero_value=0,
 ))
 
@@ -113,14 +113,14 @@ for i in range(len(curves)):
     page.add(f'wide_series_stat_card_area_{i}', ui.wide_series_stat_card(
         box=f'{1 + 2*i} 9 2 1',
         title='Wide Series Area',
-        value='=${{intl qux minimum_fraction_digits=2 maximum_fraction_digits=2}}',
-        aux_value='={{intl quux style="percent" minimum_fraction_digits=1 maximum_fraction_digits=1}}',
-        data=dict(qux=val_s, quux=pc_s / 100),
-        plot_category='foo',
+        value='=${{intl value minimum_fraction_digits=2 maximum_fraction_digits=2}}',
+        aux_value='={{intl aux_value style="percent" minimum_fraction_digits=1 maximum_fraction_digits=1}}',
+        data=dict(value=val_s, aux_value=pc_s / 100),
+        plot_category='category',
         plot_type='area',
-        plot_value='qux',
+        plot_value='value',
         plot_color=colors[i],
-        plot_data=data('foo qux', -15),
+        plot_data=data('category value', -15),
         plot_zero_value=0,
         plot_curve=curves[i],
     ))
@@ -128,14 +128,14 @@ for i in range(len(curves)):
 page.add('wide_series_stat_card_interval', ui.wide_series_stat_card(
     box='1 10 2 1',
     title='Wide Series Interval',
-    value='=${{intl qux minimum_fraction_digits=2 maximum_fraction_digits=2}}',
-    aux_value='={{intl quux style="percent" minimum_fraction_digits=1 maximum_fraction_digits=1}}',
-    data=dict(qux=val, quux=pc / 100),
-    plot_category='foo',
+    value='=${{intl value minimum_fraction_digits=2 maximum_fraction_digits=2}}',
+    aux_value='={{intl aux_value style="percent" minimum_fraction_digits=1 maximum_fraction_digits=1}}',
+    data=dict(value=val, aux_value=percentage / 100),
+    plot_category='category',
     plot_type='interval',
-    plot_value='qux',
+    plot_value='value',
     plot_color='$red',
-    plot_data=data('foo qux', -15),
+    plot_data=data('category value', -15),
     plot_zero_value=0,
 ))
 
@@ -144,14 +144,14 @@ for i in range(len(curves)):
     page.add(f'tall_series_stat_card_area_{i}', ui.tall_series_stat_card(
         box=f'{i + 1} 11 1 2',
         title='Tall Series',
-        value='=${{intl qux minimum_fraction_digits=2 maximum_fraction_digits=2}}',
-        aux_value='={{intl quux style="percent" minimum_fraction_digits=1 maximum_fraction_digits=1}}',
-        data=dict(qux=val_s, quux=pc_s / 100),
+        value='=${{intl value minimum_fraction_digits=2 maximum_fraction_digits=2}}',
+        aux_value='={{intl aux_value style="percent" minimum_fraction_digits=1 maximum_fraction_digits=1}}',
+        data=dict(value=val_s, aux_value=pc_s / 100),
         plot_type='area',
-        plot_category='foo',
-        plot_value='qux',
+        plot_category='category',
+        plot_value='value',
         plot_color=colors[i],
-        plot_data=data('foo qux', -15),
+        plot_data=data('category value', -15),
         plot_zero_value=0,
         plot_curve=curves[i],
     ))
@@ -159,14 +159,14 @@ for i in range(len(curves)):
 page.add('tall_series_stat_card', ui.tall_series_stat_card(
     box='1 13 1 2',
     title='Tall Series',
-    value='=${{intl qux minimum_fraction_digits=2 maximum_fraction_digits=2}}',
-    aux_value='={{intl quux style="percent" minimum_fraction_digits=1 maximum_fraction_digits=1}}',
-    data=dict(qux=val_s, quux=pc_s / 100),
-    plot_category='foo',
+    value='=${{intl value minimum_fraction_digits=2 maximum_fraction_digits=2}}',
+    aux_value='={{intl aux_value style="percent" minimum_fraction_digits=1 maximum_fraction_digits=1}}',
+    data=dict(value=val_s, aux_value=pc_s / 100),
+    plot_category='category',
     plot_type='interval',
-    plot_value='qux',
+    plot_value='value',
     plot_color='$red',
-    plot_data=data('foo qux', -20),
+    plot_data=data('category value', -20),
     plot_zero_value=0,
 ))
 
@@ -179,67 +179,67 @@ while True:
     c = page['small_stat_card']
     c.value = f'${val:.2f}'
 
-    val, pc = fakePercent.next()
+    val, percentage = fakePercent.next()
     c = page['wide_gauge_stat_card']
-    c.data.foo = val
-    c.data.bar = pc
-    c.progress = pc
+    c.data.value = val
+    c.data.aux_value = percentage
+    c.progress = percentage
 
-    val, pc = fakePercent.next()
+    val, percentage = fakePercent.next()
     c = page['tall_gauge_stat_card']
-    c.data.foo = val
-    c.data.bar = pc
-    c.progress = pc
+    c.data.value = val
+    c.data.aux_value = percentage
+    c.progress = percentage
 
-    val, pc = fakePercent.next()
+    val, percentage = fakePercent.next()
     c = page['wide_bar_stat_card']
-    c.data.foo = val
-    c.data.bar = pc
-    c.progress = pc
+    c.data.value = val
+    c.data.aux_value = percentage
+    c.progress = percentage
 
-    val, pc = fakePercent.next()
+    val, percentage = fakePercent.next()
     c = page['large_bar_stat_card']
-    c.data.foo = val
-    c.data.bar = pc
-    c.progress = pc
+    c.data.value = val
+    c.data.aux_value = percentage
+    c.progress = percentage
 
     for i in range(len(curves)):
         cat_s, val_s, pc_s = fakeSeries.next()
         c = page[f'small_series_stat_card_area_{i}']
-        c.data.qux = val_s
-        c.data.quux = pc_s
+        c.data.value = val_s
+        c.data.aux_value = pc_s
         c.plot_data[-1] = [cat_s, val_s]
 
     cat_s, val_s, pc_s = fakeSeries.next()
     c = page['small_series_stat_card_interval']
-    c.data.qux = val_s
-    c.data.quux = pc_s
+    c.data.value = val_s
+    c.data.aux_value = pc_s
     c.plot_data[-1] = [cat_s, val_s]
 
     for i in range(len(curves)):
         cat_s, val_s, pc_s = fakeSeries.next()
         c = page[f'wide_series_stat_card_area_{i}']
-        c.data.qux = val_s
-        c.data.quux = pc_s / 100
+        c.data.value = val_s
+        c.data.aux_value = pc_s / 100
         c.plot_data[-1] = [cat_s, val_s]
 
     cat_s, val_s, pc_s = fakeSeries.next()
     c = page['wide_series_stat_card_interval']
-    c.data.qux = val_s
-    c.data.quux = pc_s / 100
+    c.data.value = val_s
+    c.data.aux_value = pc_s / 100
     c.plot_data[-1] = [cat_s, val_s]
 
     for i in range(len(curves)):
         cat_s, val_s, pc_s = fakeSeries.next()
         c = page[f'tall_series_stat_card_area_{i}']
-        c.data.qux = val_s
-        c.data.quux = pc_s / 100
+        c.data.value = val_s
+        c.data.aux_value = pc_s / 100
         c.plot_data[-1] = [cat_s, val_s]
 
     cat_s, val_s, pc_s = fakeSeries.next()
     c = page['tall_series_stat_card']
-    c.data.qux = val_s
-    c.data.quux = pc_s / 100
+    c.data.value = val_s
+    c.data.aux_value = pc_s / 100
     c.plot_data[-1] = [cat_s, val_s]
 
     page.save()
