74ab042e0ff18a93786c9bd735df41812fad03fa
Martin Turoci <martin.turoci@h2o.ai>
2020-12-14 09:25:29 +0100

    feat: add commit lint to native git hooks

--- githooks/commit-msg.js
+++ githooks/commit-msg.js
@@ -4,7 +4,16 @@
 const
   fs = require('fs'),
   { execSync } = require('child_process'),
-  message = fs.readFileSync(process.argv[2], 'utf8').trim()
+  message = fs.readFileSync(process.argv[2], 'utf8').trim(),
+  splitBranchName = execSync('git branch').toString().split('\n').find(b => b.trim().charAt(0) === '*').trim().substring(2).split('-'),
+  lastIndex = splitBranchName.length - 1
 
-const splitBranchName = execSync('git branch').toString().split('\n').find(b => b.trim().charAt(0) === '*').trim().substring(2).split('-')
-if (!isNaN(+splitBranchName[splitBranchName.length - 1])) fs.writeFileSync(process.argv[2], `${message} #${splitBranchName[1]}`)
+// Append issue number if branch name ends with -#ISSUE_NUMBER.
+if (!isNaN(+splitBranchName[lastIndex])) fs.writeFileSync(process.argv[2], `${message} #${splitBranchName[lastIndex]}`)
+
+// Lint commit msg.
+try {
+  execSync(`node ui/node_modules/@commitlint/cli/cli.js --config ui/.commitlintrc.js -e`, { stdio: 'inherit' })
+} catch (error) {
+  process.exit(1)
+}
