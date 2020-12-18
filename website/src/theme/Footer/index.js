0cf58e93d64d413524637672ca6896172a064553
Martin Turoci <martin.turoci@h2o.ai>
2020-12-18 08:42:36 +0100

    fix(docs): server side generate fail due to client only object used

--- website/src/theme/Footer/index.js
+++ website/src/theme/Footer/index.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import OriginalFooter from '@theme-original/Footer';
+import React from 'react'
+import OriginalFooter from '@theme-original/Footer'
+import BrowserOnly from '@docusaurus/BrowserOnly'
 
-export default function Footer(props) {
+const Notice = () => {
   const
-    [isNoticeHidden, setIsNoticeHidden] = React.useState(!!localStorage.getItem('noticeRead')),
+    [isNoticeHidden, setIsNoticeHidden] = React.useState(!!window.localStorage.getItem('noticeRead')),
     onHideNotice = () => {
-      localStorage.setItem('noticeRead', true)
+      window.localStorage.setItem('noticeRead', true)
       setIsNoticeHidden(true)
     }
   return (
+    <section className='notice' style={{ opacity: isNoticeHidden ? 0 : 1 }}>
+      <div>
+        <p>By using this website you agree to our use of cookies. </p>
+        <a href='https://www.h2o.ai/privacy/' target='_blank'>Read H2O.ai’s privacy policy.</a>
+      </div>
+      <span className='notice__close' onClick={onHideNotice}>X</span>
+    </section>
+  )
+}
+
+export default function Footer(props) {
+  return (
     <>
-      <section className='notice' style={{ opacity: isNoticeHidden ? 0 : 1 }}>
-        <div>
-          <p>By using this website you agree to our use of cookies. </p>
-          <a href='https://www.h2o.ai/privacy/' target='_blank'>Read H2O.ai’s privacy policy.</a>
-        </div>
-        <span className='notice__close' onClick={onHideNotice}>X</span>
-      </section>
+      <BrowserOnly>{Notice}</BrowserOnly>
       <OriginalFooter {...props} />
     </>
   )
