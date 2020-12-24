14dffc6308a9cc154a9306b9e84e97f6a08a218c
VijithaEkanayake <ekvijitha@gmail.com>
2020-12-24 11:59:16 +0530

    fix(examples): Remove usage of pandas in stat card examples

--- py/examples/stat_wide_series_interval.py
+++ py/examples/stat_wide_series_interval.py
@@ -1,11 +1,10 @@
 # Stat / Series / Wide / Interval
 # Create a wide stat card displaying a primary value, an auxiliary value and a series plot.
 # ---
-import pandas as pd
 import time
 from h2o_wave import site, ui, data
 
-donut_prices = pd.DataFrame([
+daily_donut_prices = [
     ('2010-01-02', 3.95, 8.5, 5.25, 4.75, 2.86),
     ('2010-01-03', 7.83, 6.25, 8.50, 2.79, 4.93),
     ('2010-01-04', 3.33, 4.75, 6.5, 3.27, 3.16),
@@ -15,53 +14,47 @@ donut_prices = pd.DataFrame([
     ('2010-01-08', 7.20, 7.125, 4.70, 5.0, 3.97),
     ('2010-01-09', 8.5, 6.33, 5.125, 6.04, 4.29),
     ('2010-01-10', 5.25, 5.95, 5.75, 6.5, 6.41),
-    ('2010-01-11', 24.0, 20.70, 7.70, 7.33, 6.68), ],
-    columns=['date', 'Cream', 'Cake', 'Cruller', 'Frosted', 'Blueberry'])
-
-donut_prices.set_index('date', inplace=True)
+    ('2010-01-11', 24.0, 20.70, 7.70, 7.33, 6.68), ]
 
 
-def next_time_series_value(column, point):
-    return (donut_prices[column][point]).item(), (donut_prices[column][point] / 100).item()
-
-
-def next_data_point():
+def next_time_series_value():
     i = 0
     while True:
-        yield (i, i % donut_prices.index.size)
+        yield ((daily_donut_prices[i % len(daily_donut_prices)])[1:], i)
         i += 1
 
 
 page = site['/demo']
 
+donuts = ['Cream', 'Cake', 'Cruller', 'Frosted', 'Blueberry']
 colors = '$red $pink $blue $azure $cyan $teal $mint $green $lime $yellow $amber $orange $tangerine'.split()
-columns = donut_prices.columns
-next_data_point = next_data_point()
+next_price = next_time_series_value()
+donut_values, sample_count = next(next_price)
 cards = []
-for index, column in enumerate(columns):
-    val, pc = next_time_series_value(column, index)
-    stat_card = page.add(f'example{index}', ui.wide_series_stat_card(
-        box=f'1 {index + 1} 2 1',
-        title=column,
+
+for i in range(len(donut_values)):
+    stat_card = page.add(f'example{i}', ui.wide_series_stat_card(
+        box=f'1 {i + 1} 2 1',
+        title=donuts[i],
         value='=${{intl qux minimum_fraction_digits=2 maximum_fraction_digits=2}}',
         aux_value='={{intl quux style="percent" minimum_fraction_digits=1 maximum_fraction_digits=1}}',
-        data=dict(qux=val, quux=pc),
+        data=dict(qux=donut_values[i], quux=donut_values[i] / 100),
         plot_category='foo',
-        plot_type='area',
+        plot_type='interval',
         plot_value='qux',
-        plot_color=colors[index],
+        plot_color=colors[i],
         plot_data=data('foo qux', -15),
         plot_zero_value=0,
-        plot_curve=columns[index],
+        plot_curve=donuts[i],
     ))
-    cards.append((column, stat_card))
+    cards.append((donuts[i], stat_card))
 page.save()
 
 while True:
     time.sleep(1)
-    sample_count, row = next(next_data_point)
-    for column, stat_card in (cards):
-        val, pc = next_time_series_value(column, row)
+    donut_values, sample_count = next(next_price)
+    for index, (column, stat_card) in enumerate(cards):
+        val, pc = donut_values[index], donut_values[index] / 100
         stat_card.data.qux = val
         stat_card.data.quux = pc
         stat_card.plot_data[-1] = [sample_count, val]
