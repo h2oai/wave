44c092f873fba0b9ed6d02749749478407522b8a
Achraf Merzouki <achraf.merzouki@h2o.ai>
2020-12-29 16:18:36 -0500

    make tour.py examples popen use current system encoding settings
    make tour.py examples popen use current system encoding settings
    
    
    a

--- py/examples/tour.py
+++ py/examples/tour.py
@@ -35,11 +35,13 @@ class Example:
     async def start(self):
         # The environment passed into Popen must include SYSTEMROOT, otherwise Popen will fail when called
         # inside python during initialization if %PATH% is configured, but without %SYSTEMROOT%.
-        env = {'SYSTEMROOT': os.environ['SYSTEMROOT']} if sys.platform.lower().startswith('win') else {}
+        env = os.environ.copy()
+        env.update({'SYSTEMROOT': os.environ['SYSTEMROOT']} if sys.platform.lower().startswith('win') else {})
         if self.is_app:
+            env.update(dict(H2O_WAVE_EXTERNAL_ADDRESS=f'http://{_app_host}:{_app_port}'))
             self.process = subprocess.Popen(
                 [sys.executable, '-m', 'uvicorn', '--port', _app_port, f'examples.{self.name}:main'],
-                env=dict(H2O_WAVE_EXTERNAL_ADDRESS=f'http://{_app_host}:{_app_port}', **env))
+                env=env)
         else:
             self.process = subprocess.Popen([sys.executable, os.path.join(example_dir, self.filename)], env=env)
 
