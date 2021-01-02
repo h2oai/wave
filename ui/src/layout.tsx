32c0167ed4c7e11ec32843e5da7587464a1a8efe
Prithvi Prabhu <prithvi@h2o.ai>
2021-01-01 17:48:39 -0800

    fix: Apply left/right margins to transparent cards by default. #317 Content appears flush with window border otherwise.

--- ui/src/layout.tsx
+++ ui/src/layout.tsx
@@ -184,7 +184,7 @@ const
       $nest: {
         '>*:first-child': {
           position: 'absolute',
-          left: 0, top: 0, right: 0, bottom: 0,
+          left: 15, top: 0, right: 15, bottom: 0,
         }
       }
     },
