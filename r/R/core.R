2000fc0dc5867815a3ffecd83956e013fd7024a3
= <1964678+ashrith@users.noreply.github.com>
2021-01-12 11:07:44 -0800

    updated core to be able to create a library

--- r/R/core.R
+++ r/R/core.R
@@ -1,4 +1,4 @@
-#install.packages(c("jsonlite","httr","stringr","future"),repo = "https://lib.ugent.be/CRAN/")
+install.packages(c("jsonlite","httr","stringr","future"),repo = "https://lib.ugent.be/CRAN/")
 library(jsonlite)
 library(httr)
 library(stringr) 
@@ -26,35 +26,6 @@ plan(list(multisession,multisession))
 
 .default_internal_address = 'ws://localhost:10101'
 
-#' Configure the environmental variables
-#' This is a (hidden) internal function
-#' 
-#' The environmental variables configured here are:
-#' internal_address
-#' external_address
-#' hub_address
-#' hub_access_key_id
-#' hub_access_key_secret
-#' shutdown_timeout
-#' They are stored in a list \code{.config}
-#' @return
-#' @export
-#'
-#' @examples
-.Config <- function(){
-        .config <<- list()
-        .config$internal_address <<- .get.env.var(c("INTERNAL_ADDRESS",.default_internal_address))
-        .config$external_address <<- .get.env.var(c("EXTERNAL_ADDRESS",.config$internal_address))
-        .config$hub_address <<- .get.env.var(c("ADDRESS",'http://localhost:10101'))
-        .config$hub_access_key_id <<- .get.env.var(c('ACCESS_KEY_ID', 'access_key_id'))
-        .config$hub_access_key_secret <<- .get.env.var(c('ACCESS_KEY_SECRET','access_key_secret'))
-        .config$shutdown_timeout <<- as.integer(.get.env.var(c('SHUTDOWN_TIMEOUT','3')))
-}
-
-.Config()
-#.onLoad <- function(){
-#   .Config()
-#}
         
 
 #' Function to check if the object is a primitive.
@@ -91,8 +62,8 @@ plan(list(multisession,multisession))
 }
 
 #' Data Dump
-#' A function to check the kind of data that needs to be packed and sent. 
-#' Different configurations of the data will be enclosed with different named lists. 
+#'A function to check the kind of data that needs to be packed and sent. 
+#'Different configurations of the data will be enclosed with different named lists. 
 #' @param fields 
 #' @param size 
 #' @param data 
