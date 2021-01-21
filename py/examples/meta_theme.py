e84f2de09f0b6924029b77a6218645e45248c567
Martin Turoci <martin.turoci@h2o.ai>
2021-01-21 11:35:28 +0100

    feat: improve theme example #51

--- py/examples/meta_theme.py
+++ py/examples/meta_theme.py
@@ -1,16 +1,32 @@
 # Meta / Theme
 # Change the base color theme of the app.
 # ---
-from h2o_wave import site, ui
+from h2o_wave import Q, ui, main, app, cypress, Cypress
 
-page = site['/demo']
 
-page['meta'] = ui.meta_card(box='', theme='neon')
+@app('/demo')
+async def serve(q: Q):
+    if not q.client.initialized:
+        q.page['meta'] = ui.meta_card(box='')
+        q.page['controls'] = ui.form_card(box='1 1 2 2', items=[
+            ui.text_xl('This is normal theme'),
+            ui.button(name='change', label='Change to see neon theme', primary=True)
+        ])
+        q.client.current_theme = 'light'
+        q.client.initialized = True
 
-page['example'] = ui.markdown_card(
-    box='1 1 2 2',
-    title='Look at those colors',
-    content='This is a neon theme!',
-)
+    meta = q.page['meta']
+    controls = q.page['controls']
 
-page.save()
+    if q.args.change and q.client.current_theme != 'neon':
+        meta.theme = 'neon'
+        q.client.current_theme = 'neon'
+        controls.items[0].text_xl.content = 'This is Neon theme!'
+        controls.items[1].button.label = 'Back to normal'
+    elif q.args.change:
+        meta.theme = 'light'
+        q.client.current_theme = 'light'
+        controls.items[0].text_xl.content = 'This is normal theme'
+        controls.items[1].button.label = 'Change to see neon theme'
+
+    await q.page.save()
