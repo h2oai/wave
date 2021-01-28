0a2fdfd9c9be58e726195d02a3240ad0efc115da
Martin Turoci <martin.turoci@h2o.ai>
2021-01-28 16:12:06 +0100

    chore: update base images in linux container to test consistence for local testing

--- ui/test/visual_regression/api.spec.ts
+++ ui/test/visual_regression/api.spec.ts
@@ -1,8 +1,8 @@
 import { it, expect } from "@playwright/test"
 
 it("compares page screenshot", async ({ page, browserName }) => {
-  await new Promise((res) => setTimeout(() => res('Resolved'), 1000))
   await page.goto('http://localhost:10101/tour', { waitUntil: 'networkidle' })
+  await new Promise((res) => setTimeout(() => res('Resolved'), 1000))
   const screenshot = await page.screenshot()
   expect(screenshot).toMatchSnapshot(`test-${browserName}.png`, { threshold: 0.2 })
 })
