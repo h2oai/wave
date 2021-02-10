df0ec5c6e4d2015cc6f1c903cd768d3e232f997c
= <1964678+ashrith@users.noreply.github.com>
2021-02-10 10:59:30 -0800

    updated stat_large.R core.R

--- r/R/examples/stat_large.R
+++ r/R/examples/stat_large.R
@@ -1,4 +1,4 @@
-library(h2owave)
+#library(h2owave)
 
 
 captions <- list("Our Constitution begins with those three simple words."
@@ -11,16 +11,16 @@ card_title <- sample(row.names(mtcars),1)
 card_value <- as.character(sample(mtcars[['mpg']],1))
 card_pc <- as.integer(runif(1,1,100))
 
-test_page <- page("/page_demo")
-test_page <- page.add(test_page,"/page_demo","test",ui_large_stat_card(box="1 1 2 2"
+if("page.name" %in% names(page)) page$drop()
+page <- Site("/demo")
+page$add.card("test",ui_large_stat_card(box="1 1 2 2"
                                                                        ,title=card_title
                                                                        ,value='=${{intl foo minimum_fraction_digits=2 maximum_fraction_digits=2}}'
                                                                        ,aux_value='={{intl bar style="percent" minimum_fraction_digits=2 maximum_fraction_digits=2}}'
                                                                        ,data=list(foo=card_value,bar=card_pc/100)
                                                                        ,caption=sample(captions,1)[[1]]
                                                                        ))
-
-page.save(test_page,"/page_demo")
+page$save()
 
 while(TRUE)
 {
@@ -28,8 +28,8 @@ while(TRUE)
   card_pc <- as.integer(runif(1,1,100))
   card_value <- as.character(sample(mtcars[['mpg']],1))
   caption <- sample(captions,1)[[1]]
-  test_page[[1]]$value$data$foo <- card_value
-  test_page[[1]]$value$data$bar <- card_pc/100
-  test_page[[1]]$value$caption <- caption
-  page.save(test_page,"/page_demo")
+  page$page[[1]]$value$data$foo <- card_value
+  page$page[[1]]$value$data$bar <- card_pc/100
+  page$page[[1]]$value$caption <- caption
+  page$save()
 }
