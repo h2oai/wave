0957e28082891fb9d45472dc9ddb2d9dcb2326bb
= <1964678+ashrith@users.noreply.github.com>
2021-03-25 09:02:29 -0400

    updated makefile and removed datagen - we dont need this for the examples anymore

--- r/R/requirements.R
+++
@@ -1,8 +0,0 @@
-`%notin%` <- Negate(`%in%`)
-for(i in readLines("requirements.txt"))
-{
-        if(i %notin% rownames(installed.packages()))
-        {
-            install.packages(i,repos="https://cloud.r-project.org")
-        }
-}
