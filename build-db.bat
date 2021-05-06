1d5323a8be985dba9dc6972030c1c61eebc6dcad
Prithvi Prabhu <prithvi@h2o.ai>
2021-05-06 14:18:37 -0700

    ci: D'oh github actions has mingw #694

--- build-db.bat
+++
@@ -1,5 +0,0 @@
-for /f %%i in ('date /t') do set @bdate=%%i
-for /f %%i in ('time /t') do set @btime=%%i
-go build -ldflags "-X main.Version=%VERSION% -X main.BuildDate=%@bdate%-%@btime%" -o wavedb.exe cmd\wavedb\main.go
-7z a -tzip wavedb-%VERSION%-windows-amd64.zip wavedb.exe
-
