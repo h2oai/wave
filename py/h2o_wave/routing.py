63b42ad1b044e34385f7ed41d1b6196f55b51fd9
Prithvi Prabhu <prithvi@h2o.ai>
2021-05-06 16:32:47 -0700

    feat: Add ability to match multiple conditions while routing #787

--- py/h2o_wave/routing.py
+++ py/h2o_wave/routing.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import Optional, Callable
 import asyncio
 import logging
 from starlette.routing import compile_path
@@ -20,16 +21,27 @@ from .server import Q
 
 logger = logging.getLogger(__name__)
 
-_arg_handlers = {}
+_arg_handlers = {}  # dictionary of arg name => [(predicate, handler)]
 _path_handlers = []
 
 
-def on(arg: str = None):
+def _add_handler(arg: str, func: Callable, predicate: Optional[Callable]):
+    if arg not in _arg_handlers:
+        _arg_handlers[arg] = handlers = []
+    else:
+        handlers = _arg_handlers[arg]
+    handlers.append((predicate, func,))
+
+
+def on(arg: str = None, predicate: Optional[Callable] = None):
     """
-    Indicate that a function is a query handler that should be invoked when `q.args` contains an argument that matches a specific name or pattern.
+    Indicate that a function is a query handler that should be invoked when `q.args` contains an argument that matches a specific name or pattern or value.
 
     Examples:
-    A function annotated with `@on('foo')` is invoked whenever `q.args['foo']` is found.
+    A function annotated with `@on('foo')` is invoked whenever `q.args['foo']` is found and the value is truthy.
+    A function annotated with `@on('foo', lambda x: x is False)` is invoked whenever `q.args['foo']` is False.
+    A function annotated with `@on('foo', lambda x: isinstance(x, bool)` is invoked whenever `q.args['foo']` is True or False.
+    A function annotated with `@on('foo', lambda x: 42 <= x <= 420)` is invoked whenever `q.args['foo']` between 42 and 420.
     A function annotated with `@on('#foo')` is invoked whenever `q.args['#']` equals 'foo'.
     A function annotated with `@on('#foo/bar')` is invoked whenever `q.args['#']` equals 'foo/bar'.
     A function annotated with `@on('#foo/&lcub;fruit&rcub;')` is invoked whenever `q.args['#']` matches 'foo/apple', 'foo/orange', etc. The parameter 'fruit' is passed to the function (in this case, 'apple', 'orange', etc.)
@@ -43,20 +55,24 @@ def on(arg: str = None):
 
     Args:
         arg: The name of the `q.arg` argument (in case of plain arguments) or a pattern (in case of hash arguments, or `q.args['#']`). If not provided, the `q.arg` argument is assumed to be the same as the name of the function.
+        predicate: A function (or lambda) to test the value of the argument. If provided, the query handler is invoked if the function returns a truthy value.
     """
 
     def wrap(func):
         func_name = func.__name__
         if not asyncio.iscoroutinefunction(func):
             raise ValueError(f'@on function {func_name} must be async')
+        if predicate:
+            if not callable(predicate):
+                raise ValueError('@on predicate must be callable')
         if isinstance(arg, str) and len(arg):
             if arg.startswith('#'):
                 rx, _, conv = compile_path(arg[1:])
                 _path_handlers.append((rx, conv, func))
             else:
-                _arg_handlers[arg] = func
+                _add_handler(arg, func, predicate)
         else:
-            _arg_handlers[func_name] = func
+            _add_handler(func_name, func, predicate)
         logger.debug(f'Registered event handler {func_name}')
         return func
 
@@ -88,10 +104,17 @@ async def handle_on(q: Q) -> bool:
                     else:
                         await func(q)
                     return True
-        elif arg_value is not None:
-            func = _arg_handlers.get(arg)
-            if func:
-                await func(q)
-                return True
-
+        else:
+            entries = _arg_handlers.get(arg)
+            if entries:
+                for entry in entries:
+                    predicate, func = entry
+                    if predicate:
+                        if predicate(arg_value):
+                            await func(q)
+                            return True
+                    else:
+                        if arg_value:
+                            await func(q)
+                            return True
     return False
