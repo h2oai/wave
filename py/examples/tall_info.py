3c34c489dfc54a6373fef3b93dd732d1a8d21ac6
Prithvi Prabhu <prithvi@h2o.ai>
2021-05-10 17:20:17 -0700

    Smaller box; smaller caption; remove image_height

--- py/examples/tall_info.py
+++ py/examples/tall_info.py
@@ -6,17 +6,11 @@ from h2o_wave import site, ui
 page = site['/demo']
 
 page.add('example', ui.tall_info_card(
-    box='1 1 3 6',
+    box='1 1 2 3',
     title='Info Card',
-    caption='''
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia aliquam maxime quos facere
-    necessitatibus tempore eum odio, qui illum. Repellat modi dolor facilis odio ex possimus
-    ratione voluptate pariatur cupiditate, quidem quaerat sapiente exercitationem in omnis nulla
-    maiores consequatur dolores illo inventore quae obcaecati culpa totam corporis! Repudiandae, nostrum repellendus.
-    ''',
+    caption='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia aliquam maxime quos facere.',
     category='Category',
-    image='https://images.pexels.com/photos/3225517/pexels-photo-3225517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', # noqa
-    image_height='200px'
+    image='https://images.pexels.com/photos/3225517/pexels-photo-3225517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
 ))
 
 page.save()
