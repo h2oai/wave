8057d7bb0f478bbf29ab2d59b998706d9c6b3bbd
Prithvi Prabhu <prithvi@h2o.ai>
2021-05-19 10:10:22 -0700

    docs: Improve closable dialog example #790

--- py/examples/meta_dialog_closable.py
+++ py/examples/meta_dialog_closable.py
@@ -1,5 +1,5 @@
 # Meta / Dialog / Closable
-# Display a #dialog. #meta #closable
+# Display a #dialog having a close button, and detect when it's closed. #meta
 # ---
 from h2o_wave import main, app, Q, ui
 
@@ -7,16 +7,34 @@ from h2o_wave import main, app, Q, ui
 @app('/demo')
 async def serve(q: Q):
     if not q.client.initialized:
+        # Create an empty meta_card to hold the dialog
         q.page['meta'] = ui.meta_card(box='')
+        # Display a button to launch dialog dialog
         q.page['example'] = ui.form_card(box='1 1 2 1', items=[
-            ui.button(name='show_dialog', label='Open dialog', primary=True)
+            ui.button(name='launch_dialog', label='Launch dialog', primary=True)
         ])
         q.client.initialized = True
-    if q.args.show_dialog:
-        q.page['meta'].dialog = ui.dialog(title='Order Donuts', name='dialog', closable=True, items=[
-            ui.text('This dialog can be only closed via X icon.'),
-        ], events=['dismissed'])
-    if q.events.dialog and q.events.dialog.dismissed:
-        q.page['meta'].dialog = None
+
+    # Was the launch_dialog button clicked?
+    if q.args.launch_dialog:
+        # Create a dialog with a close button
+        q.page['meta'].dialog = ui.dialog(
+            title='Hello!',
+            name='my_dialog',
+            items=[
+                ui.text('Click the X button to close this dialog.'),
+            ],
+            # Enable a close button (displayed at the top-right of the dialog)
+            closable=True,
+            # Get notified when the dialog is dismissed.
+            events=['dismissed'],
+        )
+
+    # Did we get events from the dialog?
+    if q.events.my_dialog:
+        # Was the dialog dismissed?
+        if q.events.my_dialog.dismissed:
+            # Delete the dialog
+            q.page['meta'].dialog = None
 
     await q.page.save()
