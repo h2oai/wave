7de256cf6a0f34d678463684aba830c330dc933a
Prithvi Prabhu <prithvi@h2o.ai>
2021-05-24 17:54:15 -0700

    chore: Release v0.16.0

--- py/h2o_wave/__init__.py
+++ py/h2o_wave/__init__.py
@@ -36,7 +36,7 @@ from .db import connect, WaveDBConnection, WaveDB, WaveDBError
 from .types import *
 from .test import cypress, Cypress
 
-__version__ = '0.15.0'
+__version__ = '0.16.0'
 __author__ = 'Prithvi Prabhu'
 
 __pdoc__ = {
