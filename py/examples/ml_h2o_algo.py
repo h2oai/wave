8bedae5824e3c8b2a9dfbf47ccd0b6f3266841a5
Rohan Rao <rohanrao88@gmail.com>
2021-06-11 00:12:58 +0530

    added WaveML H2O-3 Algo example

--- py/examples/ml_h2o_algo.py
+++ py/examples/ml_h2o_algo.py
@@ -1,5 +1,5 @@
-# WaveML / H2O-3 / Categorical
-# Configure categorical columns for Wave Models built using H2O-3 AutoML.
+# WaveML / H2O-3 / Algo
+# Configure a specific algo for Wave Models built using H2O-3 AutoML.
 # ---
 from h2o_wave import main, app, Q, ui, copy_expando
 from h2o_wave_ml import build_model, ModelType
@@ -17,15 +17,15 @@ async def serve(q: Q):
             train_df=q.client.train_df,
             target_column='target',
             model_type=ModelType.H2O3,
-            categorical_columns=q.client.categorical_columns,
-            _h2o3_max_runtime_secs=5,
-            _h2o3_nfolds=2
+            _h2o3_max_runtime_secs=30,
+            _h2o3_nfolds=2,
+            _h2o3_include_algos=[q.client.algo]
         )
         model_id = q.client.wave_model.model.model_id
         accuracy = round(100 - q.client.wave_model.model.mean_per_class_error() * 100, 2)
 
         # show training details and prediction option
-        q.page['example'].items[1].dropdown.values = q.client.categorical_columns
+        q.page['example'].items[1].choice_group.value = q.client.algo
         q.page['example'].items[2].buttons.items[1].button.disabled = False
         q.page['example'].items[3].message_bar.type = 'success'
         q.page['example'].items[3].message_bar.text = 'Training successfully completed!'
@@ -45,8 +45,8 @@ async def serve(q: Q):
         data = load_wine(as_frame=True)['frame']
         q.client.train_df, q.client.test_df = train_test_split(data, train_size=0.8)
 
-        # columns
-        column_choices = [ui.choice(x, x) for x in q.client.train_df.columns if x != 'target']
+        # algos
+        algo_choices = [ui.choice(x, x) for x in ['DRF', 'GLM', 'XGBoost', 'GBM', 'DeepLearning']]
 
         # display ui
         q.page['example'] = ui.form_card(
@@ -54,8 +54,7 @@ async def serve(q: Q):
             items=[
                 ui.text(content='''The sample dataset used is the
                     <a href="https://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_wine.html" target="_blank">wine dataset</a>.'''),
-                ui.dropdown(name='categorical_columns', label='Select categorical columns',
-                            choices=column_choices, values=[]),
+                ui.choice_group(name='algo', label='Select Algo', choices=algo_choices, value='DRF'),
                 ui.buttons(items=[
                     ui.button(name='train', label='Train', primary=True),
                     ui.button(name='predict', label='Predict', primary=True, disabled=True),
