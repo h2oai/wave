33948cc4c173d682e430c5531f3da7f19b11d382
Rohan Rao <rohanrao88@gmail.com>
2021-07-08 21:37:37 +0530

    refactor to more readable

--- py/examples/ml_dai.py
+++ py/examples/ml_dai.py
@@ -3,7 +3,7 @@
 # ---
 import os
 
-from h2o_wave import main, app, Q, ui
+from h2o_wave import main, app, Q, copy_expando, ui
 from h2o_wave_ml import build_model, ModelType
 from h2o_wave_ml.utils import list_dai_instances
 
@@ -13,10 +13,16 @@ from sklearn.model_selection import train_test_split
 STEAM_URL = os.environ.get('STEAM_URL')
 MLOPS_URL = os.environ.get('MLOPS_URL')
 
+DATASET_TEXT = '''The sample dataset used is the
+    <a href="https://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_wine.html" target="_blank">wine dataset</a>.'''
+STEAM_TEXT = f'''No Driverless AI instances available. You may create one in 
+    <a href="{STEAM_URL}/#/driverless/instances" target="_blank">AI Engines</a> and refresh the page.'''
+
 
 @app('/demo')
 async def serve(q: Q):
     if 'H2O_CLOUD_ENVIRONMENT' not in os.environ:
+        # show appropriate message if app is not running on cloud
         q.page['example'] = ui.form_card(
             box='1 1 -1 -1',
             items=[
@@ -29,21 +35,32 @@ async def serve(q: Q):
         )
     elif q.args.train:
         # get DAI instance name
+        copy_expando(q.args, q.client)
+
         for dai_instance in q.client.dai_instances:
-            if dai_instance['id'] == int(q.args.dai_instance_id):
+            if dai_instance['id'] == int(q.client.dai_instance_id):
                 dai_instance_name = dai_instance['name']
 
-        model_details = f'''**Driverless AI Experiment:**
-            <a href="{STEAM_URL}/oidc-login-start?forward=/proxy/driverless/{q.args.dai_instance_id}/openid/callback" target="_blank">{dai_instance_name}</a>'''
+        # set DAI model details
+        q.client.model_details = f'''**Driverless AI Experiment:**
+            <a href="{STEAM_URL}/oidc-login-start?forward=/proxy/driverless/{q.client.dai_instance_id}/openid/callback" target="_blank">{dai_instance_name}</a>'''
 
         # show training progress and details
-        q.page['example'].items[1].dropdown.value = q.args.dai_instance_id
-        q.page['example'].items[3].buttons.visible = False
-        q.page['example'].items[4].message_bar.type = 'info'
-        q.page['example'].items[4].message_bar.text = 'Training in progress...'
-        q.page['example'].items[5].progress.visible = True
-        q.page['example'].items[6].text.content = model_details
-        q.page['example'].items[7].text.content = ''
+        q.page['example'].items = [
+            ui.text(content=DATASET_TEXT),
+            ui.dropdown(
+                name='dai_instance_id',
+                label='Select Driverless AI instance',
+                choices=q.client.choices_dai_instances,
+                value=q.client.dai_instance_id,
+                required=True),
+            ui.buttons(items=[
+                ui.button(name='train', label='Train', primary=True, disabled=True),
+                ui.button(name='predict', label='Predict', primary=True, disabled=True)
+            ]),
+            ui.progress(label='Training in progress...', caption='This can take a few minutes...'),
+            ui.text(content=q.client.model_details)
+        ]
         await q.page.save()
 
         # train WaveML Model using Driverless AI
@@ -59,25 +76,48 @@ async def serve(q: Q):
             _dai_interpretability=10
         )
 
+        # update DAI model details
         project_id = q.client.wave_model.project_id
-        model_details += f'''<br />**MLOps Deployment:**
+        q.client.model_details += f'''<br />**MLOps Deployment:**
             <a href="{MLOPS_URL}/projects/{project_id}" target="_blank">{project_id}'''
 
         # show prediction option
-        q.page['example'].items[3].buttons.visible = True
-        q.page['example'].items[3].buttons.items[1].button.disabled = False
-        q.page['example'].items[4].message_bar.type = 'success'
-        q.page['example'].items[4].message_bar.text = 'Training successfully completed!'
-        q.page['example'].items[5].progress.visible = False
-        q.page['example'].items[6].text.content = model_details
+        q.page['example'].items = [
+            ui.text(content=DATASET_TEXT),
+            ui.dropdown(
+                name='dai_instance_id',
+                label='Select Driverless AI instance',
+                choices=q.client.choices_dai_instances,
+                value=q.client.dai_instance_id,
+                required=True),
+            ui.buttons(items=[
+                ui.button(name='train', label='Train', primary=True),
+                ui.button(name='predict', label='Predict', primary=True)
+            ]),
+            ui.message_bar(type='success', text='Training successfully completed!'),
+            ui.text(content=q.client.model_details)
+        ]
     elif q.args.predict:
         # predict on test data
         preds = q.client.wave_model.predict(test_df=q.client.test_df)
 
         # show predictions
-        q.page['example'].items[4].message_bar.text = 'Prediction successfully completed!'
-        q.page['example'].items[7].text.content = f'''**Example predictions:** <br />
-            {preds[0]} <br /> {preds[1]} <br /> {preds[2]}'''
+        q.page['example'].items = [
+            ui.text(content=DATASET_TEXT),
+            ui.dropdown(
+                name='dai_instance_id',
+                label='Select Driverless AI instance',
+                choices=q.client.choices_dai_instances,
+                value=q.client.dai_instance_id,
+                required=True),
+            ui.buttons(items=[
+                ui.button(name='train', label='Train', primary=True),
+                ui.button(name='predict', label='Predict', primary=True)
+            ]),
+            ui.message_bar(type='success', text='Prediction successfully completed!'),
+            ui.text(content=q.client.model_details),
+            ui.text(content=f'**Example predictions:** <br />{preds[0]} <br /> {preds[1]} <br /> {preds[2]}')
+        ]
     else:
         # prepare sample train and test dataframes
         data = load_wine(as_frame=True)['frame']
@@ -85,7 +125,7 @@ async def serve(q: Q):
 
         # DAI instances
         q.client.dai_instances = list_dai_instances(refresh_token=q.auth.refresh_token)
-        choices_dai_instances = [
+        q.client.choices_dai_instances = [
             ui.choice(
                 name=str(x['id']),
                 label=f'{x["name"]} ({x["status"].capitalize()})',
@@ -96,36 +136,28 @@ async def serve(q: Q):
         running_dai_instances = [x['id'] for x in q.client.dai_instances if x['status'] == 'running']
         if len(running_dai_instances) == 0:
             disable_training = True
-            dai_instance_id = ''
+            q.client.dai_instance_id = ''
         else:
             disable_training = False
-            dai_instance_id = str(running_dai_instances[0])
+            q.client.dai_instance_id = str(running_dai_instances[0])
 
         # display ui
         q.page['example'] = ui.form_card(
             box='1 1 -1 -1',
             items=[
-                ui.text(content='''The sample dataset used is the
-                    <a href="https://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_wine.html" target="_blank">wine dataset</a>.'''),
+                ui.text(content=DATASET_TEXT),
                 ui.dropdown(
                     name='dai_instance_id',
                     label='Select Driverless AI instance',
-                    choices=choices_dai_instances,
-                    value=dai_instance_id,
+                    choices=q.client.choices_dai_instances,
+                    value=q.client.dai_instance_id,
                     required=True),
-                ui.text(
-                    content=f'''No Driverless AI instances available. You may create one in 
-                        <a href="{STEAM_URL}/#/driverless/instances" target="_blank">AI Engines</a> and 
-                        refresh the page.''',
-                    visible=disable_training),
+                ui.text(content=STEAM_TEXT, visible=disable_training),
                 ui.buttons(items=[
                     ui.button(name='train', label='Train', primary=True, disabled=disable_training),
                     ui.button(name='predict', label='Predict', primary=True, disabled=True),
                 ]),
-                ui.message_bar(type='warning', text='Training will take a few seconds'),
-                ui.progress(label='This can take a few minutes...', visible=False),
-                ui.text(content=''),
-                ui.text(content='')
+                ui.message_bar(type='warning', text='Training will take a few seconds')
             ]
         )
 
