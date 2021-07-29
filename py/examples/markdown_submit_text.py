7f07ebfef210a9c5f8513113377ae3ae0e3b48b1
Martin Turoci <martin.turoci@h2o.ai>
2021-07-29 14:15:12 +0200

    docs: improve md text submit example #293

--- py/examples/markdown_submit_text.py
+++ py/examples/markdown_submit_text.py
@@ -1,13 +1,21 @@
 # Markdown / Submit / Text
 # Use "?" to prefix the desired q.args.key that you want to have submitted after clicking a phrase.
 # ---
-from h2o_wave import site, ui, main
 
-page = site['/demo']
+from h2o_wave import main, app, Q, ui
 
-page['example'] = ui.markdown_card(
-    box='1 1 3 -1',
-    title='I was made using markdown!',
-    content='The quick brown [fox](?fox) jumps over the lazy [dog](?dog).'
-)
-page.save()
+
+@app('/demo')
+async def serve(q: Q):
+    if not q.client.initialized:
+        q.page['clicked'] = ui.form_card(box='1 1 3 2', items=[
+            ui.text(content='The quick brown [fox](?fox) jumps over the lazy [dog](?dog).'),
+            ui.text(content='Clicked: nothing'),
+        ])
+        q.client.initialized = True
+
+    if q.args.fox:
+        q.page['clicked'].items[1].text.content = 'Clicked: fox'
+    if q.args.dog:
+        q.page['clicked'].items[1].text.content = 'Clicked: dog'
+    await q.page.save()
