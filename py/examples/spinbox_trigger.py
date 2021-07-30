a6edd9ba496b9990b3f2a4c1cf86732ed404a1bb
Martin Turoci <martin.turoci@h2o.ai>
2021-07-30 13:07:01 +0200

    chore: improve example by removing index access items pattern #595

--- py/examples/spinbox_trigger.py
+++ py/examples/spinbox_trigger.py
@@ -2,19 +2,23 @@
 # Enable the `trigger` attribute in order to handle live changes to a spinbox.
 # #form #spinbox #trigger
 # ---
+from typing import Optional
 from h2o_wave import main, app, Q, ui
+
+
+def get_form_items(value: Optional[float]):
+    return [
+        ui.text(f'spinbox_trigger={value}'),
+        ui.spinbox(name='spinbox_trigger', label='Pick a number', trigger=True, value=value),
+    ]
 
 
 @app('/demo')
 async def serve(q: Q):
     if not q.client.initialized:
-        q.page['example'] = ui.form_card(box='1 1 4 10', items=[
-            ui.text(f'spinbox_trigger={q.args.spinbox_trigger}'),
-            ui.spinbox(name='spinbox_trigger', label='Pick a number', trigger=True),
-        ])
+        q.page['example'] = ui.form_card(box='1 1 4 10', items=get_form_items(None))
         q.client.initialized = True
-    else:
-        q.page['example'].items[0].text.content = f'Selected number: {q.args.spinbox_trigger}'
-        q.page['example'].items[1].spinbox.value = q.args.spinbox_trigger
+    if q.args.spinbox_trigger:
+        q.page['example'].items = get_form_items(q.args.spinbox_trigger)
 
     await q.page.save()
