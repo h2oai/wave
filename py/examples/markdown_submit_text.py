70882349a0deaf7d90c32f7c78e8ec013d09e58d
Martin Turoci <martin.turoci@h2o.ai>
2021-07-30 09:08:28 +0200

    chore: refactor py example to not use indeces #293

--- py/examples/markdown_submit_text.py
+++ py/examples/markdown_submit_text.py
@@ -5,17 +5,21 @@
 from h2o_wave import main, app, Q, ui
 
 
+def get_form_items(clicked: str):
+    return [
+        ui.text(content='The quick brown [fox](?fox) jumps over the lazy [dog](?dog)'),
+        ui.text(content=f'Clicked: {clicked}'),
+    ]
+
+
 @app('/demo')
 async def serve(q: Q):
     if not q.client.initialized:
-        q.page['clicked'] = ui.form_card(box='1 1 3 2', items=[
-            ui.text(content='The quick brown [fox](?fox) jumps over the lazy [dog](?dog).'),
-            ui.text(content='Clicked: nothing'),
-        ])
+        q.page['example'] = ui.form_card(box='1 1 3 2', items=get_form_items('Nothing'))
         q.client.initialized = True
 
     if q.args.fox:
-        q.page['clicked'].items[1].text.content = 'Clicked: fox'
+        q.page['example'].items = get_form_items('fox')
     if q.args.dog:
-        q.page['clicked'].items[1].text.content = 'Clicked: dog'
+        q.page['example'].items = get_form_items('dog')
     await q.page.save()
