1bac8515fbf2b2ca760c9c8eb2251e5a0cd9ef06
mturoci <64769322+mturoci@users.noreply.github.com>
2021-08-13 18:12:25 +0200

    docs: Improve routing examples. #953 (#957)
    * docs: Improve routing examples. #953
    
    * docs: Improve breadcrumbs example. #953

--- py/examples/breadcrumbs.py
+++ py/examples/breadcrumbs.py
@@ -11,19 +11,33 @@ from h2o_wave import main, app, Q, ui
 
 @app('/demo')
 async def serve(q: Q):
-    if '#' in q.args:
-        hash_ = q.args['#']
-        q.page['breadcrumbs'] = ui.form_card(box='1 1 2 2', items=[
-            ui.text(f'#={hash_}'),
-            ui.button(name='show_breadcrumbs', label='Back', primary=True),
-        ])
-    else:
-        q.page['breadcrumbs'] = ui.breadcrumbs_card(
-            box='1 1 4 1',
-            items=[
-                ui.breadcrumb(name='#menu1', label='Menu 1'),
-                ui.breadcrumb(name='#menu2', label='Menu 2'),
-                ui.breadcrumb(name='#menu3', label='Menu 3'),
-            ],
-        )
+    blurb_items = [ui.button(name='#submenu', label='Go to submenu', link=True)]
+    blurb_title = 'Welcome to Menu'
+    breadcrumbs = [ui.breadcrumb(name='#menu', label='Menu')]
+
+    if q.args['#'] == 'menu':
+        blurb_items = [ui.button(name='#submenu', label='Go to submenu', link=True)]
+        blurb_title = 'Welcome to Menu!'
+        breadcrumbs = [
+            ui.breadcrumb(name='#menu', label='Menu'),
+        ]
+    elif q.args['#'] == 'submenu':
+        blurb_items = [ui.button(name='#subsubmenu', label='Go to subsubmenu', link=True)]
+        blurb_title = 'Welcome to Submenu!'
+        breadcrumbs = [
+            ui.breadcrumb(name='#menu', label='Menu'),
+            ui.breadcrumb(name='#submenu', label='Submenu'),
+        ]
+    elif q.args['#'] == 'subsubmenu':
+        blurb_items = [ui.text('You cannot go deeper, click on Breadcrumbs above to navigate back')]
+        blurb_title = 'Welcome to Subsubmenu!'
+        breadcrumbs = [
+            ui.breadcrumb(name='#menu', label='Menu'),
+            ui.breadcrumb(name='#submenu', label='Submenu'),
+            ui.breadcrumb(name='#subsubmenu', label='Subsubmenu'),
+        ]
+
+    q.page['blurb'] = ui.form_card(box='1 2 3 2', title=blurb_title, items=blurb_items)
+    q.page['breadcrumbs'] = ui.breadcrumbs_card(box='1 1 3 1', items=breadcrumbs)
+
     await q.page.save()
