51a0728a2fa288aec194eaac0bae80abf2eb9682
lo5 <prithvi.prabhu@gmail.com>
2021-11-04 19:54:02 -0700

    Revert changes to index.tsx

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -29,10 +29,7 @@ loadTheme({
 // Initialize Fluent icons
 initializeIcons()
 
-ReactDOM.render(
-  <Router />,
-  document.getElementById('root')
-)
+ReactDOM.render(<Router />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
