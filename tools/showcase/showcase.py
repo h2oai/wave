635520edd58e19096443c3aa3ef3401751865051
Martin Turoci <martin.turoci@h2o.ai>
2021-11-25 10:21:16 +0100

    feat: add multi browser support #699

--- tools/showcase/showcase.py
+++ tools/showcase/showcase.py
@@ -9,7 +9,7 @@ import math
 import sys
 import shutil
 from json import JSONEncoder
-from typing import Any, List
+from typing import List
 from pathlib import Path
 from playwright.sync_api import sync_playwright
 from multiprocessing import Pool
@@ -53,54 +53,86 @@ page.save()
     '''
 
 
-def make_snippet_screenshot(code: List[str], screenshot_name: str, page: Any, group: str, pool_idx: int):
+def generate_diff_view():
+    if any(os.scandir(diff_folder_path)):
+        with open('template-index.html', 'r') as f:
+            content = f.read()
+        with open('index.html', 'w+') as f:
+            for file in Path(diff_folder_path).rglob('*.png'):
+                src = '/'.join(file.parts)
+                content += f'''
+<h2>{str(file).replace('test/diff', '')}</h2>
+<div class='row'>
+  <img src='{src.replace('diff', 'base', 1)}' />
+  <img src='{src.replace('diff', 'compare', 1)}' />
+  <img src='{src}' />
+</div>'''
+            content += '\n</body>\n</html>'
+            f.write(content)
+
+
+def make_snippet_screenshot(code: List[str], img_name: str, page, grp: str, pool_idx: int, is_test: bool, browser: str):
     code_str = ''.join(code)
     match = re.findall('(q.page\\[)(\'|\")([\\w-]+)', code_str)
     if not match:
-        raise ValueError(f'Could not extract card name for {screenshot_name}.')
-    card_name = match[0][2] if match[0][2] != 'meta' else match[1][2]
+        raise ValueError(f'Could not extract card name for {img_name}.')
     example_file = f'showcase{pool_idx}.py'
     with open(os.path.join(example_file_path, example_file), 'w+') as f:
         f.write(get_template_code(code_str, pool_idx))
     with subprocess.Popen(['venv/bin/python', f'showcase/{example_file}'], cwd=os.path.join('..', '..', 'py'), stderr=subprocess.PIPE) as p: # noqa
         _, err = p.communicate()
         if err:
-            raise ValueError(f'Could not generate {group} {screenshot_name}\n{err.decode()}')
+            raise ValueError(f'Could not generate {img_name}\n{err.decode()}')
         page.goto(f'http://localhost:10101/{pool_idx}', wait_until='networkidle')
 
         if 'frame_card' in code_str:
             time.sleep(1)  # Wait for iframe content to be loaded.
 
-        path = os.path.join(docs_path, 'docs', 'showcase', group, 'assets', screenshot_name)
-        if group:
-            os.makedirs(os.path.dirname(path), exist_ok=True)
+        path = os.path.join(docs_path, 'docs', 'showcase', grp, 'assets', img_name)
+        if is_test:
+            base_path = os.path.join('test', 'base', browser, grp, img_name)
+            is_base = not Path(base_path).exists()
+            path = base_path if is_base else os.path.join('test', 'compare', browser, grp, img_name)
+
+        os.makedirs(os.path.dirname(path), exist_ok=True)
         sub_selector = ' > *' if 'ui.form_card' in code_str else ''
         selector = f'[data-test="{card_name}"]{sub_selector}'
         page.wait_for_selector(selector)
+        print(f'Generating {img_name}')
         page.query_selector(selector).screenshot(path=path)
+        if is_test and not is_base:
+            img_1 = Image.open(base_path).convert('RGB')
+            img_2 = Image.open(os.path.join('test', 'compare', browser, grp, img_name)).convert('RGB')
+            img_diff = ImageChops.difference(img_1, img_2)
+            if img_diff.getbbox():
+                print(f'\033[91mMISMATCH FOUND FOR: {img_name}\033[0m')
+                diff_path = os.path.join(diff_folder_path, browser, grp, img_name)
+                os.makedirs(os.path.dirname(diff_path), exist_ok=True)
+                img_diff.save(diff_path)
 
 
-def generate_screenshots(files: List[DocFile], pool_idx: int):
+def generate_screenshots(files: List[DocFile], pool_idx: int, is_test: bool):
     with sync_playwright() as playwright:
-        browser = playwright.chromium.launch()
-        page = browser.new_page()
-        for file in files:
-            with open(os.path.join(docs_path, file.path), 'r') as f:
-                is_code = False
-                code = []
-                file_idx = 0
-                for line in f.readlines():
-                    if is_code and not line.startswith('```'):
-                        code.append(line)
-                    if line.startswith('```'):
-                        if is_code:
-                            screenshot_name = f"{file.name}-{file_idx}.png"
-                            print(f'Generating screenshot for {screenshot_name}')
-                            make_snippet_screenshot(code, screenshot_name, page, file.group, pool_idx)
-                            file_idx = file_idx + 1
-                            code = []
-                        is_code = not is_code
-        browser.close()
+        browsers = ['chromium', 'webkit', 'firefox'] if is_test else ['chromium']
+        for b in browsers:
+            browser = getattr(playwright, b).launch()
+            page = browser.new_page()
+            for file in files:
+                with open(os.path.join(docs_path, file.path), 'r') as f:
+                    is_code = False
+                    code = []
+                    file_idx = 0
+                    for line in f.readlines():
+                        if is_code and not line.startswith('```'):
+                            code.append(line)
+                        if line.startswith('```'):
+                            if is_code:
+                                screenshot_name = f"{file.name}-{file_idx}.png"
+                                make_snippet_screenshot(code, screenshot_name, page, file.group, pool_idx, is_test, b)
+                                file_idx = file_idx + 1
+                                code = []
+                            is_code = not is_code
+            browser.close()
 
 
 def append_images(files: List[DocFile]):
@@ -143,8 +175,12 @@ def main():
     qd_server = None
     arg_files = sys.argv[1:]
     files = []
-    if not arg_files:
-        # Remove old contents first.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', type=str, help='an integer for the accumulator', default=None)
+    parser.add_argument('--test', action='store_true', help='Run visual tests')
+    args = parser.parse_args()
+
+    if not args.file:
         for f in glob.glob(os.path.join(docs_path, 'docs', 'showcase', '*.md')):
             os.remove(f)
         for f in glob.glob(os.path.join(docs_path, 'docs', 'showcase', 'assets', '*.png')):
@@ -176,7 +212,6 @@ def main():
     except Exception as e:
         print(f'Error: {str(e)}')
     finally:
-        # Cleanup.
         if os.path.exists(example_file_path):
             shutil.rmtree(example_file_path, ignore_errors=True)
         if qd_server:
