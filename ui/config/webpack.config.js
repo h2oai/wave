9d83399632b1831fb8db57912aa6eb05dd9d94a0
Martin Turoci <martin.turoci@h2o.ai>
2022-01-05 17:53:43 +0100

    chore: Add visual progress to ui build #1169

--- ui/config/webpack.config.js
+++ ui/config/webpack.config.js
@@ -641,6 +641,7 @@ module.exports = function (webpackEnv) {
         // The formatter is invoked directly in WebpackDevServerUtils during development
         formatter: isEnvProduction ? typescriptFormatter : undefined,
       }),
+      new webpack.ProgressPlugin(),
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell webpack to provide empty mocks for them so importing them works.
