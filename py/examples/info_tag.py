f22b2afa0c00b041e992bd9c6c6d4212c617cecb
Alexandre Alencar <alencardev@gmail.com>
2022-01-07 18:53:25 +0100

    change tag order in py example

--- py/examples/info_tag.py
+++ py/examples/info_tag.py
@@ -13,13 +13,13 @@ page['example'] = ui.form_card(
     box='1 1 2 2',
     items=[
         ui.inline(justify='center', items=[
-            ui.info_tag(name='tag-1', color=error, label='2', size='large'),
-            ui.info_tag(name='tag-2', color=warning, label='1', size='large'),
+            ui.info_tag(name='tag-1', color=error, label='1', size='large'),
+            ui.info_tag(name='tag-2', color=warning, label='2', size='large'),
             ui.info_tag(name='tag-3', color=success, label='3', size='large'),
         ]),
         ui.inline(justify='center', items=[
-            ui.info_tag(name='tag-4', color=error, label='2', size='small'),
-            ui.info_tag(name='tag-5', color=warning, label='1', size='small'),
+            ui.info_tag(name='tag-4', color=error, label='1', size='small'),
+            ui.info_tag(name='tag-5', color=warning, label='2', size='small'),
             ui.info_tag(name='tag-6', color=success, label='3', size='small'),
         ])
     ])
