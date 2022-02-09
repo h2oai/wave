9bf24b3cd9dc2b1e35d4053f88594f1b48662e4a
Martin Turoci <martin.turoci@h2o.ai>
2022-02-09 11:28:43 +0100

    chore: Clean up jest config #1189

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -19,8 +19,6 @@ module.exports = {
   testEnvironment: "jest-environment-jsdom-fourteen",
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": '<rootDir>/node_modules/babel-jest',
-    "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
-    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "<rootDir>/config/jest/fileTransform.js"
   },
   transformIgnorePatterns: [
     "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$",
