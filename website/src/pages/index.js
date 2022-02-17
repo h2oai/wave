a44113f0819129ea0849cbc1cdda268428551812
mturoci <64769322+mturoci@users.noreply.github.com>
2022-02-17 11:14:15 +0100

    ci: Publish release to S3 and make it downloadable from our docs site. (#1241)

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from 'react'
+import clsx from 'clsx'
+import Layout from '@theme/Layout'
+import Link from '@docusaurus/Link'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import useBaseUrl from '@docusaurus/useBaseUrl'
+import styles from './styles.module.css'
 
 const features = [
   {
@@ -66,16 +66,16 @@ const features = [
 function Feature({ icon, title, description }) {
   return (
     <div className={clsx('col col--4', styles.feature)}>
-      <div><i class={icon}></i></div>
+      <div><i className={icon}></i></div>
       <h3>{title}</h3>
       <p>{description}</p>
     </div>
-  );
+  )
 }
 
 function Home() {
-  const context = useDocusaurusContext();
-  const { siteConfig = {} } = context;
+  const context = useDocusaurusContext()
+  const { siteConfig = {} } = context
   return (
     <Layout
       title="Make AI Apps"
@@ -122,7 +122,7 @@ function Home() {
         )}
       </main>
     </Layout>
-  );
+  )
 }
 
-export default Home;
+export default Home
