25815d0312030b355ec58c90276563f7f93d0ff0
Martin Turoci <martin.turoci@h2o.ai>
2022-03-29 13:18:37 +0200

    chore: Minor type refactor. #730

--- py/examples/table_pagination.py
+++ py/examples/table_pagination.py
@@ -4,7 +4,7 @@
 # ---
 
 import os
-from typing import Any, Dict, List
+from typing import Dict, List
 from h2o_wave import main, app, Q, ui
 from copy import deepcopy
 import csv
@@ -22,7 +22,7 @@ rows_per_page = 10
 total_rows = len(all_rows)
 
 
-def get_rows(base: List[Any], sort: Dict[str, bool] = None, search: Dict = None, filters: Dict[str, List[str]] = None) -> List[Any]:
+def get_rows(base: List, sort: Dict[str, bool] = None, search: Dict = None, filters: Dict[str, List[str]] = None) -> List:
     # Make a deep copy in order to not mutate the original `all_issues` which serves as our baseline.
     rows = deepcopy(base)
 
