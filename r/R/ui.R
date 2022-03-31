f1fe0cd1799b821383e2fe251b98cc46d3ede6dd
Marek Mihok <marek.mihok@h2o.ai>
2022-03-31 11:55:28 +0200

    chore: side_panel attributes rearranged - generate components

--- r/R/ui.R
+++ r/R/ui.R
@@ -3677,9 +3677,9 @@ ui_dialog <- function(
 #' @param title The side panel's title.
 #' @param items The components displayed in this side panel.
 #' @param width The width of the dialog, e.g. '400px'. Defaults to '600px'.
-#' @param blocking True to prevent closing when clicking or tapping outside the side panel. Prevents interacting with the page behind the side panel. Defaults to False.
 #' @param name An identifying name for this component.
 #' @param events The events to capture on this side panel. One of 'dismissed'.
+#' @param blocking True to prevent closing when clicking or tapping outside the side panel. Prevents interacting with the page behind the side panel. Defaults to False.
 #' @param closable True if the side panel should have a closing 'X' button at the top right corner.
 #' @return A SidePanel instance.
 #' @export
@@ -3687,24 +3687,24 @@ ui_side_panel <- function(
   title,
   items,
   width = NULL,
-  blocking = NULL,
   name = NULL,
   events = NULL,
+  blocking = NULL,
   closable = NULL) {
   .guard_scalar("title", "character", title)
   .guard_vector("items", "WaveComponent", items)
   .guard_scalar("width", "character", width)
-  .guard_scalar("blocking", "logical", blocking)
   .guard_scalar("name", "character", name)
   .guard_vector("events", "character", events)
+  .guard_scalar("blocking", "logical", blocking)
   .guard_scalar("closable", "logical", closable)
   .o <- list(
     title=title,
     items=items,
     width=width,
-    blocking=blocking,
     name=name,
     events=events,
+    blocking=blocking,
     closable=closable)
   class(.o) <- append(class(.o), c(.wave_obj, "WaveSidePanel"))
   return(.o)
