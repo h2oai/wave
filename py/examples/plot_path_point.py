4551b3a0ca771efe47cf7476681ab8bf892aa5a2
Marek Mihok <marek.mihok@h2o.ai>
2022-04-08 12:47:03 +0200

    feat: normalize plot_path examples

--- py/examples/plot_path_point.py
+++ py/examples/plot_path_point.py
@@ -1,22 +1,29 @@
 # Plot / Path / Point
 # Make a path #plot with an additional layer of points.
 # ---
-from synth import FakeScatter
 from h2o_wave import site, data, ui
 
 page = site['/demo']
 
-n = 50
-f = FakeScatter()
-v = page.add('example', ui.plot_card(
+page.add('example', ui.plot_card(
     box='1 1 4 5',
     title='Path + Point',
-    data=data('profit sales', n),
+    data=data('price performance', 10, rows=[
+        (0.1, 0.6),
+        (0.2, 0.5),
+        (0.3, 0.3),
+        (0.4, 0.2),
+        (0.4, 0.5),
+        (0.2, 0.2),
+        (0.8, 0.5),
+        (0.3, 0.3),
+        (0.2, 0.4),
+        (0.1, 0.0),
+    ]),
     plot=ui.plot([
-        ui.mark(type='path', x='=profit', y='=sales'),
-        ui.mark(type='point', x='=profit', y='=sales'),
+        ui.mark(type='path', x='=price', y='=performance'),
+        ui.mark(type='point', x='=price', y='=performance'),
     ])
 ))
-v.data = [(x, y) for x, y in [f.next() for _ in range(n)]]
 
 page.save()
--- py/examples/plot_path_smooth.py
+++ py/examples/plot_path_smooth.py
@@ -1,19 +1,26 @@
 # Plot / Path / Smooth
 # Make a path #plot with a smooth curve.
 # ---
-from synth import FakeScatter
 from h2o_wave import site, data, ui
 
 page = site['/demo']
 
-n = 50
-f = FakeScatter()
-v = page.add('example', ui.plot_card(
+page.add('example', ui.plot_card(
     box='1 1 4 5',
     title='Path, smooth',
-    data=data('profit sales', n),
-    plot=ui.plot([ui.mark(type='path', x='=profit', y='=sales', curve='smooth')])
+    data=data('price performance', 10, rows=[
+        (0.1, 0.6),
+        (0.2, 0.5),
+        (0.3, 0.3),
+        (0.4, 0.2),
+        (0.4, 0.5),
+        (0.2, 0.2),
+        (0.8, 0.5),
+        (0.3, 0.3),
+        (0.2, 0.4),
+        (0.1, 0.0),
+    ]),
+    plot=ui.plot([ui.mark(type='path', x='=price', y='=performance', curve='smooth')])
 ))
-v.data = [(x, y) for x, y in [f.next() for _ in range(n)]]
 
 page.save()
