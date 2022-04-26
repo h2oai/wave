80c7cd00d077c5602231d1b93ac37b6d296e8b91
Martin Turoci <martin.turoci@h2o.ai>
2022-04-26 14:36:23 +0200

    fix: Use relative paths for static assets to make base urls work.

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -19,6 +19,7 @@ import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
   plugins: [
     reactRefresh(),
     eslintPlugin({ cache: false, exclude: ['node_modules', 'package-lock.json'] }),
