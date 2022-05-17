9195e3fd9c7b155ee4f4aef79020a846c273bab2
Martin Turoci <martin.turoci@h2o.ai>
2022-05-17 11:20:59 +0200

    chore: Refactor #1191

--- py/examples/tour.py
+++ py/examples/tour.py
@@ -361,74 +361,10 @@ def get_wave_completions(line, character, file_content):
 get_wave_completions(${position.lineNumber - 1}, ${position.column - 1}, \'\'\'${model.getValue()}\'\'\')
     '''
 
-    js_content = '''
-require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.33.0/min/vs' } })
-window.MonacoEnvironment = {
-  getWorkerUrl: function (workerId, label) {
-    return `data:text/javascript;charset=utf-8,$${encodeURIComponent(`
-      self.MonacoEnvironment = {
-        baseUrl: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.33.0/min/'
-      };
-      importScripts('https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.33.0/min/vs/base/worker/workerMain.js');`
-    )}`
-  }
-};
-
-(async () => {
-  window.pyodide = await loadPyodide()
-  window.pyodide.loadPackage('parso')
-})()
-
-require(['vs/editor/editor.main'], function () {
-    const snippetToCompletionItem = item => ({
-      label: item.prefix,
-      kind: monaco.languages.CompletionItemKind.Snippet,
-      documentation: item.description,
-      insertText: item.body.join('\\n'),
-      insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-    })
-    const completionToCompletionItem = item => ({
-      label: item.get('label'),
-      kind: item.get('kind'),
-      insertText: item.get('label'),
-      sortText: item.get('sort_text'),
-    })
-    monaco.languages.registerCompletionItemProvider('python', {
-      triggerCharacters: ['.', "'", '"'],
-      provideCompletionItems: async (model, position) => {
-        if (!window.code_snippets) {
-          const [snippets1, snippets2] = await Promise.all([
-            fetch('$snippets1').then(r => r.json()),
-            fetch('$snippets2').then(r => r.json()),
-          ])
-          window.code_snippets = [
-            ...Object.values(snippets1).map(snippetToCompletionItem),
-            ...Object.values(snippets2).map(snippetToCompletionItem)
-          ]
-        }
-        const pyRes = window.pyodide.runPython(`$py_content`)
-        const completions = pyRes ? pyRes.toJs().map(completionToCompletionItem) : []
-        return { suggestions: [ ...completions,...window.code_snippets] }
-      }
-  })
-  const editor = monaco.editor.create(document.getElementById('monaco-editor'), {
-    value: '',
-    language: 'python',
-    minimap: { enabled: false },
-    overviewRulerLanes: 0,
-    hideCursorInOverviewRuler: true,
-    scrollbar: { vertical: 'hidden' },
-    overviewRulerBorder: false,
-  })
-  window.editor = editor
-  window.emit_debounced = window.wave.debounce(2000, window.wave.emit)
-  editor.onDidChangeModelContent(e => {
-    if (e.isFlush) return
-    emit_debounced('editor', 'change', editor.getValue())
-  })
-})
-    '''
-    template = Template(js_content).substitute(snippets1=q.app.snippets1, snippets2=q.app.snippets2, py_content=py_content)
+    js_code = ''
+    with open('tour.js', 'r') as f:
+        js_code = f.read()
+    template = Template(js_code).substitute(snippets1=q.app.snippets1, snippets2=q.app.snippets2, py_content=py_content)
     q.page['meta'] = ui.meta_card(
         box='',
         title=app_title,
