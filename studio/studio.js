9ba90d3d97e245ec755e5eccbe4ee790c342ed67
Marek Mihok <marek.mihok@h2o.ai>
2022-06-16 12:07:02 +0200

    feat: file icons support
    feat: highlight whole row on hover, make tree compact
    
    fix: remove menu padding, input layout shift
    
    feat: add colors to icons
    
    feat: file explorer resizer
    
    feat: input focused by default, ability to cancel renaming
    
    feat: highlight active file, input styling
    
    feat: cancel creating by esc
    
    feat: open context menu by clicking on item
    
    feat: double click to rename
    
    feat: cancel create/rename when input lost focus
    
    feat: do not show create file/folder options in menu when right clicking on file
    
    feat: prevent console error on unsupported file extension assets
    
    feat: open folder when creating new file/folder inside
    
    fix: handle create file/folder input indentiation
    
    fix: refactor
    
    fix: prevent label from wrapping
    
    fix: file label preselect on rename
    
    fix: label hidden after rename cancel
    
    feat: refactor after PR review

--- studio/studio.js
+++ studio/studio.js
@@ -79,11 +79,45 @@ const initPyodide = setInterval(async () => {
   await window.pyodide.runPythonAsync(`$py_content`)
 }, 100)
 
+const iconLabels = {
+  'css': 'language-css3',
+  'gif': 'file-gif-box',
+  'html': 'language-html5',
+  'jpeg': 'file-jpg-box',
+  'jpg': 'file-jpg-box',
+  'js': 'language-javascript',
+  'json': 'code-json',
+  'md': 'book-open',
+  'png': 'file-png-box',
+  'py': 'language-python',
+  'svg': 'svg',
+  'txt': 'file-document',
+  'xml': 'file-xml-box',
+  'zip': 'folder-zip',
+}
+
 const baseUrl = '$base_url'
+
+const store = Vue.reactive({
+  activeFile: 'project/app.py'
+})
+
+const
+  getIconSrc = (folder, isSubtreeOpen) => {
+    if (folder.isFolder) return isSubtreeOpen ? baseUrl + 'assets/img/folder-open.svg' : baseUrl + 'assets/img/folder.svg'
+    if (folder.path) return baseUrl + `assets/img/file-icons/$${iconLabels[folder.path.split('.').pop()] || 'file'}.svg`
+  },
+  getItemLeftPadding = (folder) => {
+    if (folder.path) return (folder.path.split('/').length - (folder.action === 'new' ? 0 : 1)) * 0.7 + 'rem'
+  }
+
 // Tree file viewer.
 const Tree = {
+  mounted() {
+    this.bus.on('openFolder', () => this.isSubtreeOpen = true)
+  },
   props: { folder: { type: Object, required: false }, },
-  data() { return { isSubtreeOpen: true } },
+  data() { return { isSubtreeOpen: true, store, timer: null, clickCount: 0 } },
   methods: {
     onContextMenu(e) {
       eventBus.emit('menu', { e, folder: this.folder })
@@ -92,23 +126,61 @@ const Tree = {
       this.folder.action = null
       window.parent.wave.emit('file_viewer', this.folder.isFolder ? 'new_folder' : 'new_file', { path: this.folder.path, name: this.folder.label })
     },
+    rename() {
+      this.folder.action = 'rename'
+      this.folder._labelCache = this.folder.label
+    },
     onRenamed() {
       this.folder.action = null
       window.parent.wave.emit('file_viewer', this.folder.isFolder ? 'rename_folder' : 'rename_file', { path: this.folder.path, name: this.folder.label })
     },
+    onCreateCanceled() {
+      this.folder.label = ''
+      this.folder.action = null
+    },
+    onRenameCanceled() {
+      this.folder.action = null
+      // HACK: blur is fired after keyup resulting in calling onRenameCanceled twice 
+      // condition prevents from assigning 'null' to 'folder.label' on second call
+      // TODO: prevent blur event after keyup
+      if (this.folder._labelCache) this.folder.label = this.folder._labelCache
+      this.folder._labelCache = null
+    },
+    handleSelect(e) {
+      const
+        labelLength = this.folder.label.length,
+        extensionLength = this.folder.label.split('.').pop().length
+      this.$$nextTick(() => {
+        e.target.setSelectionRange(0, labelLength - (extensionLength ? extensionLength + 1 : 0))
+      })
+    },
     onClick(e) {
-      if (!document.querySelector('.menu')) this.isSubtreeOpen = !this.isSubtreeOpen
-      eventBus.emit('documentClick', e)
-      if (!this.folder.isFolder) window.parent.wave.emit('file_viewer', 'open', this.folder.path)
-    }
+      this.clickCount++
+      if (this.folder.action === 'rename') return
+      else if (this.clickCount === 1) {
+        // open on single click
+        if (!document.querySelector('.menu')) this.isSubtreeOpen = !this.isSubtreeOpen
+        eventBus.emit('documentClick', e)
+        if (!this.folder.isFolder) {
+          store.activeFile = this.folder.path
+          window.parent.wave.emit('file_viewer', 'open', this.folder.path)
+        }
+        this.timer = setTimeout(() => { this.clickCount = 0 }, 300)
+      } else {
+        // rename on double click
+        this.rename()
+        clearTimeout(this.timer)
+        this.clickCount = 0
+      }
+    },
   },
   template: `
-<li>
-  <span class="tree-item">
-    <img v-if="folder.isFolder" class="tree-item-img" src="$${baseUrl}assets/img/folder-solid.svg" alt="Folder" />
-    <input v-if="folder.action === 'new'" type="text" v-model="folder.label" @keyup.enter="onCreated">
-    <input v-else-if="folder.action === 'rename'" type="text" v-model="folder.label" @keyup.enter="onRenamed">
-    <span v-else @contextmenu.prevent="onContextMenu" @click.stop="onClick">{{folder.label}}</span>
+<li v-if="folder.label !== '' || folder.action === 'new'">
+  <span @contextmenu.prevent="onContextMenu" @click.stop="onClick" :class="{ 'tree-item-active': store.activeFile == folder.path, 'tree-item': true }" ref="sample" :style="{paddingLeft: getItemLeftPadding(folder)}">
+    <img class="tree-item-img" :src="getIconSrc(folder, isSubtreeOpen)" :alt="folder.isFolder ? 'Folder' : 'File'" />
+    <input v-if="folder.action === 'new'" v-focus type="text" spellcheck="false" v-model="folder.label" @keyup.enter="onCreated" @keyup.esc="onCreateCanceled" @blur="onCreateCanceled">
+    <input v-else-if="folder.action === 'rename'" v-focus type="text" spellcheck="false" v-model="folder.label" @keyup.enter="onRenamed" @keyup.esc="onRenameCanceled" @blur="onRenameCanceled" @focus="handleSelect">
+    <span v-else class="tree-item-label">{{folder.label}}</span>
   </span>
     <ul v-if="isSubtreeOpen && folder.isFolder && folder.children">
       <li v-for="folder in folder.children">
@@ -133,10 +205,12 @@ const Menu = {
   },
   methods: {
     newFile() {
+      eventBus.emit('openFolder')
       this.folder.children.push({ label: '', action: 'new', isFolder: false, path: this.folder.path })
       this.menuPosition = null
     },
     newFolder() {
+      eventBus.emit('openFolder')
       this.folder.children.push({ label: '', action: 'new', isFolder: true, path: this.folder.path })
       this.menuPosition = null
     },
@@ -148,15 +222,16 @@ const Menu = {
     },
     rename() {
       this.folder.action = 'rename'
+      this.folder._labelCache = this.folder.label
       this.menuPosition = null
     },
   },
   template: `
       <ul v-if="menuPosition && folder" class='menu' :style="{top: menuPosition.y + 'px', left: menuPosition.x + 'px'}">
-        <li @click="newFile"><img class="menu-item-img" src="$${baseUrl}assets/img/file-solid.svg" alt="Folder" />New file</li>
-        <li @click="newFolder"><img class="menu-item-img" src="$${baseUrl}assets/img/folder-solid.svg" alt="Folder" />New folder</li>
-        <li @click="rename"><img class="menu-item-img" src="$${baseUrl}assets/img/pen-solid.svg" alt="Folder" />Rename</li>
-        <li @click="remove" class='delete'><img class="menu-item-img" src="$${baseUrl}assets/img/trash-can-solid.svg" alt="Folder" />Delete</li>
+        <li v-if="folder.isFolder" @click="newFile"><img class="menu-item-img" src="$${baseUrl}assets/img/file-plus.svg" alt="Folder" />New file</li>
+        <li v-if="folder.isFolder" @click="newFolder"><img class="menu-item-img" src="$${baseUrl}assets/img/folder-plus.svg" alt="Folder" />New folder</li>
+        <li @click="rename"><img class="menu-item-img" src="$${baseUrl}assets/img/pencil.svg" alt="Folder" />Rename</li>
+        <li @click="remove" class='delete'><img class="menu-item-img" src="$${baseUrl}assets/img/trash-can.svg" alt="Folder" />Delete</li>
       </ul>
     `}
 class EventBus {
@@ -182,9 +257,39 @@ const app = Vue.createApp({
       <Teleport to="#file-tree-menu"><Menu/></Teleport>
       `
 })
-app.config.globalProperties.bus = eventBus
+app.config.globalProperties = { bus: eventBus, getIconSrc, getItemLeftPadding }
 app.component('Tree', Tree)
 app.component('Menu', Menu)
 app.mount('#file-tree')
+app.directive('focus', {
+  mounted: (el) => el.focus()
+})
 document.onclick = e => eventBus.emit('documentClick', e)
 eventBus.emit("folder", $folder)
+
+const handleHorizontalResize = () => {
+  let mouseX, elementWidth = 0
+  const
+    element = document.getElementById('explorer'),
+    resizer = document.getElementById('width-resizer'),
+    mouseMoveHandler = (e) => {
+      const dx = e.clientX - mouseX
+      if (elementWidth + dx > 60) element.style.width = elementWidth + dx + 'px'
+    },
+    mouseUpHandler = () => {
+      // handlers registered on document - events not fired when mouse is moved too quickly while registered on resizer itself
+      document.removeEventListener('mousemove', mouseMoveHandler)
+      document.removeEventListener('mouseup', mouseUpHandler)
+    },
+    mouseDownHandler = (e) => {
+      mouseX = e.clientX
+      const styles = window.getComputedStyle(element)
+      elementWidth = parseInt(styles.width, 10)
+      document.addEventListener('mousemove', mouseMoveHandler)
+      document.addEventListener('mouseup', mouseUpHandler)
+    }
+
+  resizer.addEventListener('mousedown', mouseDownHandler)
+}
+
+handleHorizontalResize()
