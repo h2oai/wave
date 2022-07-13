e2de46cbb46f75d59506a37d2c4581ef6c5f761c
Alexandre Alencar <alencardev@gmail.com>
2022-07-13 14:34:28 +0200

    fix: Let wave app select tab programmatically #349
    closes #349

--- ui/src/tabs.tsx
+++ ui/src/tabs.tsx
@@ -66,6 +66,7 @@ export const
       onLinkClick = (item?: Fluent.PivotItem) => {
         const name = item?.props.itemKey
         if (!name) return
+        setSelected(name)
         if (name.startsWith('#')) {
           window.location.hash = name.substr(1)
           return
@@ -80,13 +81,17 @@ export const
           wave.push()
         }
       },
-      tabs = m.items?.map(t => <Fluent.PivotItem key={t.name} itemIcon={t.icon} itemKey={t.name} headerText={t.label} />)
+      tabs = m.items?.map(t => <Fluent.PivotItem key={t.name} itemIcon={t.icon} itemKey={t.name} headerText={t.label} />),
+      [selected, setSelected] = React.useState(m.value)
+    
+    React.useEffect(() => setSelected(m.value), [m.value])
+
     return (
       <div className={css.pivot}>
         <Fluent.Pivot
           data-test={m.name}
           className={m.link ? 'w-tabs-link' : 'w-tabs'} //HACK: Marker classes.
-          defaultSelectedKey={m.value}
+          selectedKey={selected}
           linkFormat={m.link ? 'links' : 'tabs'}
           onLinkClick={onLinkClick}>{tabs}</Fluent.Pivot>
       </div>
