9291b3e1439e945bfc34f2302c9e6715be42ab5a
Alexandre Alencar <alencardev@gmail.com>
2022-07-22 11:35:55 +0200

    fix: Change combobox examples initial value "Blue" -> "Black"

--- py/examples/combobox.py
+++ py/examples/combobox.py
@@ -20,13 +20,13 @@ async def serve(q: Q):
         ]
     else:
         q.page['example'] = ui.form_card(box='1 1 4 10', items=[
-            ui.combobox(name='combobox', label='Enter or choose a color', placeholder='Color...', value='Blue',
+            ui.combobox(name='combobox', label='Enter or choose a color', placeholder='Color...', value='Black',
                         choices=combobox_choices),
-            ui.combobox(name='combobox_required', label='Enter or choose a color', placeholder='Color...', value='Blue',
+            ui.combobox(name='combobox_required', label='Enter or choose a color', placeholder='Color...', value='Black',
                         choices=combobox_choices, required=True),
-            ui.combobox(name='combobox_disabled', label='Enter or choose a color', placeholder='Color...', value='Blue',
+            ui.combobox(name='combobox_disabled', label='Enter or choose a color', placeholder='Color...', value='Black',
                         choices=combobox_choices, disabled=True),
-            ui.combobox(name='combobox_error', label='Enter or choose a color', placeholder='Color...', value='Blue',
+            ui.combobox(name='combobox_error', label='Enter or choose a color', placeholder='Color...', value='Black',
                         choices=combobox_choices, error='This combobox has an error!'),
             ui.combobox(name='combobox_multivalued', label='Enter or choose a color (multi select)', placeholder='Color...', values=['Black', 'Magenta'],
                         choices=combobox_choices),
