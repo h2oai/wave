1421be3a23ef24a46a1b2198a9f2ff173fb85e88
Alexandre Alencar <alencardev@gmail.com>
2022-08-24 14:21:11 +0200

    fix: allow setting Textbox "value" dynamically #1578
    * allow Textbox "value" to be set from wave app
    * add tests for TextField and MaskedTextField
    * fix warning about act() in tests
    
    closes #1578

--- ui/src/textbox.test.tsx
+++ ui/src/textbox.test.tsx
@@ -12,19 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { fireEvent, render } from '@testing-library/react'
+import { fireEvent, render, act } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import React from 'react'
 import { Textbox, XTextbox } from './textbox'
 import { wave } from './ui'
 
 const name = 'textbox'
-const textboxProps: Textbox = { name }
+let textboxProps: Textbox = { name }
 
 describe('Textbox.tsx', () => {
   beforeEach(() => {
     jest.clearAllMocks()
     jest.useFakeTimers()
     wave.args[name] = null
+    textboxProps = { name }
   })
 
   it('Renders data-test attr', () => {
@@ -51,7 +53,7 @@ describe('Textbox.tsx', () => {
   it('Sets args on input', () => {
     const { getByTestId } = render(<XTextbox model={textboxProps} />)
     fireEvent.change(getByTestId(name), { target: { value: 'text' } })
-    jest.runOnlyPendingTimers()
+    act(() => { jest.runOnlyPendingTimers() })
 
     expect(wave.args[name]).toBe('text')
   })
@@ -79,8 +81,24 @@ describe('Textbox.tsx', () => {
     fireEvent.change(getByTestId(name), { target: { value: 'aaa' } })
 
     expect(pushMock).not.toBeCalled() // Not called immediately, but after specified timeout.
-    jest.runOnlyPendingTimers()
+    act(() => { jest.advanceTimersByTime(250) })
+    expect(pushMock).not.toBeCalled()
+    act(() => { jest.advanceTimersByTime(250) })
     expect(pushMock).toBeCalled()
+  })
+
+  it('Debounces wave push', () => {
+    const { getByTestId } = render(<XTextbox model={{...textboxProps, trigger: true}} />)
+    const pushMock = jest.fn()
+    wave.push = pushMock
+
+    userEvent.type(getByTestId(name), 'a')
+    userEvent.type(getByTestId(name), 'a')
+    userEvent.type(getByTestId(name), 'a')
+    userEvent.type(getByTestId(name), 'a')
+    act(() => { jest.runOnlyPendingTimers() })
+
+    expect(pushMock).toBeCalledTimes(1)
   })
 
   it('Does not call sync on change - trigger not specified', () => {
@@ -92,5 +110,39 @@ describe('Textbox.tsx', () => {
     fireEvent.change(getByTestId(name), { target: { value: 'aaa' } })
 
     expect(pushMock).not.toBeCalled()
+  })
+
+  it('Display new value when "value" prop changes', () => {
+    const { getByTestId, rerender } = render(<XTextbox model={{...textboxProps, value: 'A'}} />)
+    expect(getByTestId(name)).toHaveValue('A')
+
+    rerender(<XTextbox model={{...textboxProps, value: 'B'}} />)
+    expect(getByTestId(name)).toHaveValue('B')
+  })
+
+  it('Types value and then display new value when "value" prop changes', () => {
+    const { getByTestId, rerender } = render(<XTextbox model={textboxProps} />)
+    userEvent.type(getByTestId(name), '{backspace}A{Enter}')
+    expect(getByTestId(name)).toHaveValue('A')
+
+    rerender(<XTextbox model={{...textboxProps, value: 'B'}} />)
+    expect(getByTestId(name)).toHaveValue('B')
+  })
+
+  it('Display new value when "value" prop changes (masked)', () => {
+    const { getByTestId, rerender } = render(<XTextbox model={{...textboxProps, value: '123', mask: '(999)'}} />)
+    expect(getByTestId(name)).toHaveValue('(123)')
+
+    rerender(<XTextbox model={{...textboxProps, value: '456', mask: '(999)'}} />)
+    expect(getByTestId(name)).toHaveValue('(456)')
+  })
+
+  it('Types value and then display new value when "value" prop changes (masked)', () => {
+    const { getByTestId, rerender } = render(<XTextbox model={{...textboxProps, mask: '(999)'}} />)
+    userEvent.type(getByTestId(name), '{backspace}123{Enter}')
+    expect(getByTestId(name)).toHaveValue('(123)')
+
+    rerender(<XTextbox model={{...textboxProps, value: '456', mask: '(999)'}} />)
+    expect(getByTestId(name)).toHaveValue('(456)')
   })
 })
