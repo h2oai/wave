4fdd4dba6265d03361a89828e777792f51eeca97
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-13 14:00:21 +0000

    Adding tarfile member sanitization to extractall()

--- py/h2o_wave/cli.py
+++ py/h2o_wave/cli.py
@@ -146,7 +146,26 @@ def fetch():
 
     print(f'Extracting...')
     with tarfile.open(tar_file) as tar:
-        tar.extractall()
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar)
 
     tar_dir = Path(tar_name)
     if not tar_dir.is_dir():
