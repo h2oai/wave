8454fe85703d19e528c7247b8d49a0b77e8e85d4
Marek Mihok <marek.mihok@h2o.ai>
2022-10-21 10:28:46 +0200

    chore: refactor and code cleaning #1542

--- ui/src/parts/lightbox.tsx
+++ ui/src/parts/lightbox.tsx
@@ -45,7 +45,6 @@ const
     content: {
       display: 'flex',
       position: 'relative',
-      overflow: 'auto',
       justifyContent: 'center',
     },
     footer: {
@@ -65,23 +64,42 @@ const
       margin: '0px 2px',
       cursor: 'pointer',
       filter: 'brightness(30%)',
-      border: '2px solid black'
+      border: '2px solid #000'
     },
-    imgCaptions: { whiteSpace: 'nowrap', padding: '10px 40px', height: '40px' },
-    text: { textOverflow: 'ellipsis', overflow: 'hidden' },
-    title: { fontWeight: 500 },
-    description: { color: '#bbb' },
-    navImgContainer: { display: 'inline-block', width: '124px' }
+    imgCaptions: {
+      whiteSpace: 'nowrap',
+      padding: '10px 40px',
+      height: IMAGE_CAPTIONS_HEIGHT - 20
+    },
+    text: {
+      textOverflow: 'ellipsis',
+      overflow: 'hidden'
+    },
+    title: {
+      fontWeight: 500
+    },
+    description: {
+      color: '#bbb'
+    },
+    navImgContainer: {
+      display: 'inline-block',
+      width: '124px'
+    },
+    iconStylesRootArrow: {
+      position: "absolute",
+      top: "50%",
+      padding: "16px",
+      marginTop: "-50px"
+    }
   }),
   iconStyles: Fluent.IButtonStyles = {
     flexContainer: { justifyContent: 'center' },
+    root: { margin: '4px 4px', width: 38, height: 38, backgroundColor: 'rgba(0, 0, 0, 0.3)' },
     rootHovered: { backgroundColor: 'rgba(255, 255, 255, 0.3)' },
     icon: { color: '#fff', lineHeight: 22, height: 'unset', padding: 4 },
     iconHovered: { color: '#fff' },
     iconPressed: { color: 'rgba(255, 255, 255, 0.7)' },
-  },
-  iconStylesRoot: Fluent.IStyle = { margin: '4px 4px', width: 38, height: 38, backgroundColor: 'rgba(0, 0, 0, 0.3)' },
-  iconStylesRootArrow: Fluent.IStyle = { ...iconStylesRoot, position: "absolute", top: "50%", padding: "16px", marginTop: "-50px", }
+  }
 
 type Image = { title: S, description?: S, type?: S, image?: S, path?: S }
 
@@ -103,6 +121,7 @@ export const Lightbox = ({ images, defaultImageIdx }: LightboxProps) => {
     isGallery = images.length > 1,
     FOOTER_HEIGHT = isGallery ? IMAGE_CAPTIONS_HEIGHT + IMAGE_NAV_HEIGHT : IMAGE_CAPTIONS_HEIGHT,
     imageNavRef = React.useRef<HTMLDivElement | null>(null),
+    defaultScrollSetRef = React.useRef(false),
     activeColor = isDark(getColorFromString(cssVar('$neutralPrimary'))!) ? cssVar('$card') : cssVar('$neutralPrimary'),
     imageHighlightStyle = { filter: 'unset', border: '2px solid', borderColor: activeColor },
     lazyImageObserver = new window.IntersectionObserver(entries =>
@@ -115,32 +134,20 @@ export const Lightbox = ({ images, defaultImageIdx }: LightboxProps) => {
         }
       })
     ),
-    onClose = () => {
-      lightboxB(null)
-      setActiveImageIdx(defaultImageIdx || 0)
-      if (imageNavRef.current) imageNavRef.current.scrollLeft = 0
-    },
     handleKeyDown = (ev: KeyboardEvent) => {
       if (['Escape', 'ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'].indexOf(ev.key) >= 0) ev.preventDefault()
-      if (ev.key === 'Escape') onClose()
+      if (ev.key === 'Escape') lightboxB(null)
       if (ev.key === 'ArrowRight') setActiveImageIdx(prevIdx => prevIdx === images.length - 1 ? 0 : prevIdx + 1)
       else if (ev.key === 'ArrowLeft') setActiveImageIdx(prevIdx => prevIdx === 0 ? images.length - 1 : prevIdx - 1)
     }
 
   React.useEffect(() => {
-    // Set initial scroll position.
-    if (isGallery && imageNavRef.current) {
-      const half = (imageNavRef.current.clientWidth / 2) - 62
-      const imageScroll = activeImageIdx * 124
-      if (imageScroll > half) imageNavRef.current.scrollLeft = imageScroll - half
-    }
-
     // Initialize intersection observer for lazy images.
     const lazyImages = [].slice.call(document.querySelectorAll(".lazy"))
     lazyImages.forEach(lazyImage => lazyImageObserver.observe(lazyImage))
 
     // Add keyboard events listener.
-    window.addEventListener("keydown", handleKeyDown, { capture: true, passive: false })
+    window.addEventListener("keydown", handleKeyDown)
     return () => window.removeEventListener("keydown", handleKeyDown)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
@@ -151,20 +158,24 @@ export const Lightbox = ({ images, defaultImageIdx }: LightboxProps) => {
       const
         navRef = imageNavRef.current,
         scrollLeftMiddle = (navRef.clientWidth / 2) - 62,
-        scrollLeftActiveImage = activeImageIdx * 124
+        scrollLeftActiveImage = activeImageIdx * 124,
+        scrollBehavior = defaultScrollSetRef.current ? 'smooth' : 'auto'
       if (activeImageIdx === 0) navRef.scrollLeft = 0
       else if (activeImageIdx === images.length - 1) navRef.scrollLeft = navRef.scrollWidth - navRef?.clientWidth
-      else if (scrollLeftActiveImage > scrollLeftMiddle) navRef.scrollTo({ left: scrollLeftActiveImage - scrollLeftMiddle, behavior: 'smooth' })
+      else if (scrollLeftActiveImage > scrollLeftMiddle) navRef.scrollTo({ left: scrollLeftActiveImage - scrollLeftMiddle, behavior: scrollBehavior })
+      else navRef.scrollTo({ left: 0, behavior: scrollBehavior })
+
+      if (!defaultScrollSetRef.current) defaultScrollSetRef.current = true
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [activeImageIdx, images.length])
+  }, [activeImageIdx])
 
   return (
     <div className={css.body}>
       <div className={css.header}>
         <Fluent.ActionButton
-          styles={{ ...iconStyles, root: iconStylesRoot }}
-          onClick={onClose}
+          styles={iconStyles}
+          onClick={() => lightboxB(null)}
           iconProps={{ iconName: 'Cancel', style: { fontSize: '22px' } }}
         />
       </div>
@@ -173,13 +184,15 @@ export const Lightbox = ({ images, defaultImageIdx }: LightboxProps) => {
         {isGallery &&
           <>
             <Fluent.ActionButton
-              styles={{ ...iconStyles, root: iconStylesRootArrow }}
+              styles={iconStyles}
+              className={css.iconStylesRootArrow}
               style={{ left: 0 }}
               onClick={() => setActiveImageIdx(activeImageIdx === 0 ? images.length - 1 : activeImageIdx - 1)}
               iconProps={{ iconName: 'ChevronLeft', style: { fontSize: '22px' } }}
             />
             <Fluent.ActionButton
-              styles={{ ...iconStyles, root: iconStylesRootArrow }}
+              styles={iconStyles}
+              className={css.iconStylesRootArrow}
               style={{ right: 0 }}
               onClick={() => setActiveImageIdx(activeImageIdx === images.length - 1 ? 0 : activeImageIdx + 1)}
               iconProps={{ iconName: 'ChevronRight', style: { fontSize: '22px' } }}
@@ -192,18 +205,21 @@ export const Lightbox = ({ images, defaultImageIdx }: LightboxProps) => {
           <div title={title} className={clas(css.text, css.title)}>{title}</div>
           <div title={description} className={clas(css.text, css.description)}>{description || ''}</div>
         </div>
-        {isGallery && <div className={css.imageNav} ref={imageNavRef}>
-          {images.map((image, idx) =>
-            <div key={idx} className={css.navImgContainer}>
-              <img
-                className={clas(css.img, css.navImg, 'lazy', style({ $nest: { '&:hover': imageHighlightStyle } }))}
-                style={activeImageIdx === idx ? imageHighlightStyle : undefined}
-                alt={title}
-                data-src={getImageSrc(image)}
-                onClick={() => setActiveImageIdx(idx)}
-              />
-            </div>
-          )}</div>}
+        {isGallery &&
+          <div className={css.imageNav} ref={imageNavRef}>
+            {images.map((image, idx) =>
+              <div key={idx} className={css.navImgContainer}>
+                <img
+                  className={clas(css.img, css.navImg, 'lazy', style({ $nest: { '&:hover': imageHighlightStyle } }))}
+                  style={activeImageIdx === idx ? imageHighlightStyle : undefined}
+                  alt={title}
+                  data-src={getImageSrc(image)}
+                  onClick={() => setActiveImageIdx(idx)}
+                />
+              </div>
+            )}
+          </div>
+        }
       </div>
     </div>
   )
