e56068589baf563922999c15992d2d743ce56f7a
Jackson Isaac <jacksonisaac2008@gmail.com>
2022-10-26 14:27:55 +0200

    fix: Add support for unicode in table export #1626 (#1674)

--- ui/src/table.tsx
+++ ui/src/table.tsx
@@ -592,7 +592,9 @@ const
           dataRows = (m.groups ? m.groups.flatMap(({ rows }) => rows) : m.rows)?.map(({ cells }) => cells) || [],
           data = toCSV([m.columns.map(({ label, name }) => label || name), ...dataRows]),
           a = document.createElement('a'),
-          blob = new Blob([data], { type: "octet/stream" }),
+          // Add BOM prefix to data. This is required to export unicode characters in the table correctly
+          // Reference: https://stackoverflow.com/a/18251283/1970068
+          blob = new Blob(['\uFEFF', data], { type: "text/csv;charset=utf-8," }),
           url = window.URL.createObjectURL(blob)
 
         a.href = url
