eaf21d4ba2902e49b2f2912ffb23f33154fb1c8f
Marek Mihok <marek.mihok@h2o.ai>
2022-10-26 13:02:00 +0200

    feat: style fluent list to support image grid, add more real-case py example #1599

--- py/examples/image_grid.py
+++ py/examples/image_grid.py
@@ -7,8 +7,15 @@ path = "https://images.unsplash.com/photo-1664136535720-ce2cd0087987?ixlib=rb-1.
 
 @app("/demo")
 async def serve(q: Q) -> None:
-    q.page["grid"] = ui.image_grid_card(box="1 1 5 6", height="100px", images=[
-        ui.image(title="hello-{i}", path=f"https://source.unsplash.com/random/sig={i}") for i in range(40)
-    ])
+    q.page["grid"] = ui.image_grid_card(box="1 1 8 12", height="100px", images=(
+        [ui.image(title="hello-{i}", path=f"https://source.unsplash.com/random/sig={i}") for i in range(3)] +
+        [ui.image(title="hello-{i}", path=f"https://source.unsplash.com/random/sig=110", width='100px')] +
+        [ui.image(title="hello-{i}", path=f"https://source.unsplash.com/random/sig={i}") for i in range(4)] +
+        [ui.image(title="hello-{i}", path=f"https://source.unsplash.com/random/sig=111", width='80px')] +
+        [ui.image(title="hello-{i}", path=f"https://source.unsplash.com/random/sig=112", width='450px')] +
+        [ui.image(title="hello-{i}", path=f"https://source.unsplash.com/random/sig={i}") for i in range(8)] +
+        [ui.image(title="hello-{i}", path=f"https://source.unsplash.com/random/sig=113", width='100px')] +
+        [ui.image(title="hello-{i}", path=f"https://source.unsplash.com/random/sig={i}") for i in range(10)]
+    ))
 
     await q.page.save()
--- ui/src/image_grid.tsx
+++ ui/src/image_grid.tsx
@@ -1,4 +1,3 @@
-
 // Copyright 2020 H2O.ai, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,35 +14,47 @@
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import * as Fluent from '@fluentui/react'
-import { Model, S } from 'h2o-wave'
+import { B, Id, Model, S, U } from 'h2o-wave'
 import React from 'react'
 import { stylesheet } from 'typestyle'
 import { Component } from './form'
 // import { Image } from './image'
 import { cards } from './layout'
 import { lightboxB } from './parts/lightbox'
+import { clas } from './theme'
 import { bond } from './ui'
 
 const css = stylesheet({
-  container: {
-    display: 'flex',
-    gap: 20,
-    padding: 20,
-    flexWrap: 'wrap',
-    alignItems: 'flex-start',
-    alignContent: 'flex-start',
-  },
+  // container: {
+  //   display: 'grid', // inline-grid
+  //   gridAutoRows: '300px',
+  //   gridGap: '10px',
+  //   gridTemplateColumns: 'repeat(auto-fill, minmax(30%, 1fr))'
+  // }
 })
 
+/** Create an image grid. */
 interface ImageGrid {
-  images: Component[]
-  width?: S,
-  height?: S,
+  /** The card's title. */
+  title: S
+  /** An identifying name for this component. */
+  name: Id
+  /** A list of images to be displayed in the image grid. */
+  images: Component[] // TODO: Image[]
+  /** A width of the image grid component. */
+  width?: S
+  /** A height of the image grid component. */
+  height?: S
+  /** Specifies the image placement. With the `best-fit` option gaps between images are minimized, but they can be placed in different order. */
+  image_placement?: 'default' | 'best-fit'
+  /** Number of images in one row. This setting ignores specified image `width` attribute. */
+  images_per_row?: U
+  /** Limit the number of rows displayed in the image grid. */
+  rows?: U
 }
 export interface State {
-  /** Images */
   // images: Image[]
-  images: Component[],
+  images: Component[] // TODO: Image[]
   width?: S
   height?: S
 }
@@ -53,12 +64,26 @@ export const
     const
       { width, height, images } = m,
       onRenderCell = React.useCallback((image, index: number | undefined) => {
+        console.log('w, h', image.width, image.height)
         return (
-          <div key={index} style={{ height: height }}>
+          <div key={index} style={{
+            // height: image.height,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            fontSize: '1.3em',
+            fontWeight: 'bold',
+            color: 'white',
+            gridRow: 'span 1'
+          }}>
             <img
-              width={width}
-              height={height}
-              style={{ flex: `0 0 ${width}px`, objectFit: 'contain', cursor: 'pointer' }}
+              width={'250px' || image.width}
+              // height={image.height}
+              style={{
+                flex: `0 0 ${width}px`,
+                objectFit: 'contain',
+                cursor: 'pointer',
+              }}
               src={image.path}
               onClick={image.path ? () => lightboxB({ images: images, defaultImageIdx: index }) : undefined}
             />
@@ -67,13 +92,30 @@ export const
         // eslint-disable-next-line react-hooks/exhaustive-deps
       }, [])
 
-    console.log('images:', images)
+    // console.log('images:', images)
 
     return (
       <Fluent.FocusZone>
         <Fluent.List
-          // className={classNames.listGridExample}
-          // style={{ display: 'inline-block' }}
+          // onRenderSurface={(props: Fluent.IListOnRenderSurfaceProps<Component> | undefined) => {
+          //   // console.log(props)
+          //   // eslint-disable-next-line react/prop-types
+          //   return <div
+          //     {...props?.divProps}
+          //     className={clas(css.container)}>
+          //     {props?.pageElements}
+          //   </div>
+          // }}
+          getPageStyle={page => {
+            console.log(page)
+            // return css.container
+            return {
+              display: 'grid', // inline-grid
+              gridAutoRows: '300px',
+              gridGap: '10px',
+              gridTemplateColumns: 'repeat(auto-fill, minmax(30%, 1fr))'
+            }
+          }}
           items={images.length ? images : undefined}
           // getItemCountForPage={getItemCountForPage}
           // getPageHeight={getPageHeight}
@@ -89,6 +131,7 @@ export const
     // const rows = state.box.substring(6)
     // const viewportHeight = rowHeight * rows
     // const shouldRenderImage = () => true
+    // console.log(name)
 
     const render = () => <XImageGrid model={{ width, height, images: images.flatMap(({ image }) => image) }} />
 
