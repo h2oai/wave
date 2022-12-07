1f7e7c0c14a26d23fd2cf2b35025d0eb0b2c1cbb
Colton Van Voorhis <cvanvoorhis01@gmail.com>
2022-12-07 11:28:37 -0500

    Change copyable_text example to be customer friendly

--- py/examples/copyable_text.py
+++ py/examples/copyable_text.py
@@ -10,20 +10,18 @@ multiline_content = '''Wave is truly awesome.
 You should try all the features!'''
 big_multiline_content = '''Wave is truly awesome.
 You should try all the features!
-like having a big height textbox!
-*
-*
-*
-*
-woohoo!'''
+Like having a big height textbox!
+...
+...
+...
+...
+Woohoo!'''
 page = site['/demo']
 
 page['hello'] = ui.form_card(box='1 1 3 3', items=[
     ui.copyable_text(label='Copyable text', value='Hello world!'),
     ui.copyable_text(label='Multiline Copyable text', value=multiline_content, multiline=True),
-    ui.copyable_text(label='Multiline Copyable text with height', value=multiline_content, multiline=True, height='250px'),
-    ui.copyable_text(label='Copyable text big height', value=big_multiline_content, multiline=True, height='1000px'),
-    ui.copyable_text(label='Copyable text half height', value=big_multiline_content, multiline=True, height='500px'),
+    ui.copyable_text(label='Multiline Copyable text with height=200px', value=big_multiline_content, multiline=True, height='200px'),
 ])
 
 page.save()
