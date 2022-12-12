cbb4a1c4bb1652a0b29eb7141a9658299bcaf64b
Martin Turoci <martin.turoci@h2o.ai>
2022-12-12 11:43:40 +0100

    fix: Support image popup on older Safari versions as well.

--- ui/src/parts/lightbox.tsx
+++ ui/src/parts/lightbox.tsx
@@ -32,7 +32,10 @@ const
   css = stylesheet({
     body: {
       position: 'fixed',
-      inset: '0px',
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
       zIndex: 1,
       backgroundColor: 'rgba(0, 0, 0, 0.9)',
       cursor: 'pointer'
