15284fcda3496e891d4fa05357dda6017e53a170
Martin Turoci <martin.turoci@h2o.ai>
2023-03-02 09:57:11 +0100

    ci: Use correct path for py API docs.

--- py/h2o_wave/h2o_wave/__init__.py
+++ py/h2o_wave/h2o_wave/__init__.py
@@ -27,7 +27,7 @@ Wave's components work in conjunction with the Wave relay server that facilitate
 realtime state synchronization between Python and web browsers.
 
 
-.. include:: ../docs/index.md
+.. include:: ../../docs/index.md
 """
 from .core import Site, AsyncSite, site, Page, Ref, data, pack, Expando, expando_to_dict, clone_expando, copy_expando
 from .server import listen, Q, app, main
