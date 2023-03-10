2eacd1ef19d8eca1c85b4605bfa501d319800c98
Martin Turoci <martin.turoci@h2o.ai>
2023-03-10 11:13:51 +0100

    chore: Simplify counter examples. #148

--- py/examples/counter_broadcast.py
+++ py/examples/counter_broadcast.py
@@ -2,22 +2,22 @@
 # Launch the server in #broadcast #mode to synchronize browser state across users.
 # Open `/demo` in multiple browsers and watch them synchronize in realtime.
 # ---
-from h2o_wave import main, app, Q, ui, pack
+from h2o_wave import main, app, Q, ui
 
 
 @app('/demo', mode='broadcast')
 async def serve(q: Q):
-    count = q.app.count or 0
-    if 'increment' in q.args:
-        count += 1
-        q.app.count = count
+    if not q.client.initialized:
+        if q.app.count is None:
+            q.app.count = 0
 
-    items = pack([ui.button(name='increment', label=f'Count={count}')])
+        q.page['example'] = ui.form_card(box='1 1 2 1', items=[
+            ui.button(name='increment', label=f'Count={q.app.count}')
+        ])
+        q.client.initialized = True
 
-    if count > 0:
-        form = q.page['example']
-        form.items = items
-    else:
-        q.page['example'] = ui.form_card(box='1 1 2 1', items=items)
+    if q.args.increment:
+        q.app.count += 1
+        q.page['example'].increment.label = f'Count={q.app.count}'
 
     await q.page.save()
--- py/examples/counter_unicast.py
+++ py/examples/counter_unicast.py
@@ -6,16 +6,15 @@ from h2o_wave import main, app, Q, ui
 
 @app('/demo')
 async def serve(q: Q):
-    count = q.client.count or 0
-
     if not q.client.initialized:
+        q.client.count = 0
         q.page['example'] = ui.form_card(box='1 1 2 1', items=[
-            ui.button(name='increment', label=f'Count={count}')
+            ui.button(name='increment', label=f'Count={q.client.count}')
         ])
         q.client.initialized = True
 
     if q.args.increment:
-        q.client.count = count = count + 1
-        q.page['example'].items[0].button.label = f'Count={count}'
+        q.client.count += 1
+        q.page['example'].increment.label = f'Count={q.client.count}'
 
     await q.page.save()
