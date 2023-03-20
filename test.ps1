e11ca565ca326370c8b2a8276be1322dff76cf65
Martin Turoci <martin.turoci@h2o.ai>
2023-03-20 10:10:05 +0100

    fix: Use proper error handling for win pwsh. #1874

--- test.ps1
+++ test.ps1
@@ -3,24 +3,31 @@ $ErrorActionPreference = "Stop"
 # Test e2e
 Set-Location e2e
 .\venv\Scripts\pytest -s
+if ($LastExitCode -ne 0) { exit $LastExitCode }
 
 Set-Location ..
 
 # Test Py.
 Set-Location py
 .\venv\Scripts\python -m tests
-$env:H2O_WAVE_BASE_URL= '/foo/'; .\venv\bin\python -m tests
-$env:H2O_WAVE_WAVED_DIR='..'; .\venv\bin\python -m tests
-$env:H2O_WAVE_WAVED_DIR='..'; $env:H2O_WAVE_BASE_URL='/foo/'; .\venv\bin\python -m tests
+if ($LastExitCode -ne 0) { exit $LastExitCode }
+$env:H2O_WAVE_BASE_URL= '/foo/'; .\venv\Scripts\python -m tests
+if ($LastExitCode -ne 0) { exit $LastExitCode }
+$env:H2O_WAVE_WAVED_DIR='..'; .\venv\Scripts\python -m tests
+if ($LastExitCode -ne 0) { exit $LastExitCode }
+$env:H2O_WAVE_WAVED_DIR='..'; $env:H2O_WAVE_BASE_URL='/foo/'; .\venv\Scripts\python -m tests
+if ($LastExitCode -ne 0) { exit $LastExitCode }
 
 Set-Location ..
 
 # Test VSC.
 Set-Location tools\vscode-extension
 .\venv\Scripts\python -m server.tests
+if ($LastExitCode -ne 0) { exit $LastExitCode }
 
 Set-Location ..\..
 
 # Test IntelliJ.
 Set-Location tools\intellij-plugin
 .\gradlew test
+if ($LastExitCode -ne 0) { exit $LastExitCode }
