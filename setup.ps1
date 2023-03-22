0885dcde6aa9162b188d5740da5ae0c32aa9bd84
Martin Turoci <martin.turoci@h2o.ai>
2023-03-22 10:54:31 +0100

    chore: Refactor. #1874

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,3 @@
-$ErrorActionPreference = "Stop"
-
 # Setup UI.
 Set-Location ui
 npm ci
@@ -27,16 +25,6 @@ npm run compile
 if ($LastExitCode -ne 0) { exit $LastExitCode }
 
 Set-Location ..\..
-
-# Setup e2e.
-Set-Location e2e
-python -m venv venv
-.\venv\Scripts\python -m pip install -r requirements.txt
-.\venv\Scripts\python -m pip install --editable ..\py\h2o_wave
-.\venv\Scripts\playwright install
-if ($LastExitCode -ne 0) { exit $LastExitCode }
-
-Set-Location ..
 
 # Build waved.
 go build -ldflags '-X main.Version=DEV -X main.BuildDate=20230317152454' -o waved cmd/wave/main.go
