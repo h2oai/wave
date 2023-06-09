d03cd04ca4bbf41d39aabbdef9e0a4b7de6620db
Martin Turoci <martin.turoci@h2o.ai>
2023-06-09 14:20:13 +0200

    chore: Refactor external libs packaging. #3.11

--- tools/vscode-extension/server/__main__.py
+++ tools/vscode-extension/server/__main__.py
@@ -15,20 +15,20 @@
 # limitations under the License.                                           #
 ############################################################################
 import argparse
-import logging
 import os
 import sys
 
 # HACK: Needed for prod where we use user's installed python.
-sys.path.append(os.path.join(os.getcwd(), 'venv', 'lib', 'python3.8', 'site-packages'))
+sys.path.append(os.path.join(os.getcwd(), 'deps'))
 
 from .lsp_server import server
-logging.basicConfig(filename="pygls.log", level=logging.DEBUG, filemode="w")
+
 
 def add_arguments(parser):
     parser.add_argument("--tcp", action="store_true", help="Use TCP server instead of stdio")
     parser.add_argument("--host", default="127.0.0.1", help="Bind to this address")
     parser.add_argument("--port", type=int, default=2087, help="Bind to this port")
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -39,6 +39,7 @@ def main():
         server.start_tcp(args.host, args.port)
     else:
         server.start_io()
+
 
 if __name__ == '__main__':
     main()
