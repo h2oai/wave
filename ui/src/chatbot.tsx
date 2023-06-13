450ce0ffa704c30c08c6a90ce334a4db50137065
Marek Mihok <marek.mihok@h2o.ai>
2023-06-13 12:11:10 +0200

    feat: Make chatbot messages to fill the entire line #1998 (#2008)

--- ui/src/chatbot.tsx
+++ ui/src/chatbot.tsx
@@ -17,7 +17,7 @@ import { B, Id, Model, Rec, S, unpack } from 'h2o-wave'
 import React from 'react'
 import { cards } from './layout'
 import { Markdown } from './markdown'
-import { clas, cssVar, getContrast, important, padding, px } from './theme'
+import { clas, cssVar, getContrast, important, px, rem } from './theme'
 import { bond, wave } from './ui'
 
 const
@@ -37,39 +37,27 @@ const
       right: 0,
       bottom: 62, // Height of input box + padding.
       overflowY: 'auto',
-      padding: padding(0, 15),
-    },
-    msg: {
-      display: 'inline-block',
-      backgroundColor: cssVar('$text'),
-      padding: 6,
-      borderRadius: 4,
-      maxWidth: '65ch',
-      borderTopLeftRadius: 0,
-      textAlign: 'left',
-    },
-    userMsg: {
-      backgroundColor: cssVar('$themePrimary'),
-      borderTopLeftRadius: 4,
-      borderTopRightRadius: 0,
     },
     msgWrapper: {
       '&:first-child': { marginTop: important(px(0)) },
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    msg: {
+      maxWidth: '65ch',
+      flexGrow: 1,
+    },
+    botMsg: {
+      backgroundColor: cssVar('$neutralLighter'),
+    },
+    textInput: {
+      padding: 15,
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      right: 0
     }
-  }),
-  getCornerStyle = (prev?: B, curr?: B, next?: B): React.CSSProperties | undefined => {
-    // First.
-    if (curr && curr !== prev && curr === next) return { borderBottomRightRadius: 0, borderTopRightRadius: 4 }
-    if (!curr && curr !== prev && curr === next) return { borderBottomLeftRadius: 0, borderTopLeftRadius: 4 }
-
-    // Middle.
-    if (curr && prev === curr && curr === next) return { borderTopRightRadius: 0, borderBottomRightRadius: 0 }
-    if (!curr && prev === curr && curr === next) return { borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }
-
-    // Last.
-    if (curr && prev === curr && curr !== next) return { borderTopRightRadius: 0, borderBottomRightRadius: 4 }
-    if (!curr && prev === curr && curr !== next) return { borderTopLeftRadius: 0, borderBottomLeftRadius: 4 }
-  }
+  })
 
 type ChatMessage = { msg: S, fromUser: B }
 
@@ -88,6 +76,8 @@ export const XChatbot = ({ model }: { model: Chatbot }) => {
     [msgs, setMsgs] = React.useState<ChatMessage[]>([]),
     [userInput, setUserInput] = React.useState(''),
     msgContainerRef = React.useRef<HTMLDivElement>(null),
+    theme = Fluent.useTheme(),
+    botTextColor = React.useMemo(() => getContrast(theme.palette.neutralLighter), [theme.palette.neutralLighter]),
     onChange = (e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newVal = '') => {
       e.preventDefault()
       wave.args[model.name] = newVal
@@ -101,7 +91,12 @@ export const XChatbot = ({ model }: { model: Chatbot }) => {
       setUserInput('')
     }
 
-  React.useEffect(() => { if (msgContainerRef.current) msgContainerRef.current.scrollTop = msgContainerRef.current.scrollHeight }, [msgs])
+  React.useEffect(() => {
+    if (!msgContainerRef.current) return
+    const offsetTop = msgContainerRef.current.scrollHeight
+    if (msgContainerRef.current?.scrollTo) msgContainerRef.current.scrollTo({ top: offsetTop, behavior: 'smooth' })
+    else msgContainerRef.current.scrollTop = offsetTop
+  }, [msgs])
   React.useEffect(() => { if (model.data) setMsgs(model.data as ChatMessage[]) }, [model.data])
 
   return (
@@ -110,29 +105,27 @@ export const XChatbot = ({ model }: { model: Chatbot }) => {
         {msgs.map(({ msg, fromUser }, idx) => (
           <div
             key={idx}
-            className={css.msgWrapper}
+            className={clas(css.msgWrapper, fromUser ? '' : css.botMsg)}
             style={{
-              marginTop: msgs[idx - 1]?.fromUser !== fromUser ? 3 : 10,
-              textAlign: fromUser ? 'right' : 'left',
-              color: getContrast(fromUser ? '$themePrimary' : '$text')
+              paddingTop: msgs[idx - 1]?.fromUser !== fromUser ? rem(0.8) : 0,
+              paddingBottom: msgs?.[idx + 1]?.fromUser !== fromUser ? rem(0.8) : 0,
+              color: fromUser ? '$text' : botTextColor
             }} >
             <span
-              className={clas(css.msg, fromUser ? css.userMsg : '', 'wave-s14')}
-              style={{
-                ...getCornerStyle(msgs[idx - 1]?.fromUser, fromUser, msgs[idx + 1]?.fromUser),
-                padding: msg?.includes('\n') ? 12 : 6,
-              }}>
+              className={clas(css.msg, 'wave-s14')}
+              style={{ padding: msg?.includes('\n') ? 12 : 6 }}>
               <Markdown source={msg || ''} />
             </span>
           </div>
         ))}
       </div>
-      <div style={{ padding: 15, position: 'absolute', bottom: 0, left: 0, right: 0 }}>
+      <div className={css.textInput}>
         <Fluent.TextField
           data-test={model.name}
           value={userInput}
           onChange={onChange}
           onKeyDown={onKeyDown}
+          autoComplete='off'
           placeholder={model.placeholder || 'Type your message'}
           styles={{ root: { flexGrow: 1 }, field: { width: `calc(100% - ${SUBMIT_BTN_SIZE}px)`, paddingRight: 0 } }}
         />
