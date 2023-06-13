137359f8c6070baeb624ed9e7c88ef299f0a8001
Martin Turoci <martin.turoci@h2o.ai>
2023-06-13 14:23:32 +0200

    fix: Initialize Go list buffer with correct idx.

--- listbuf.go
+++ listbuf.go
@@ -78,7 +78,7 @@ func (b *ListBuf) dump() BufD {
 func loadListBuf(ns *Namespace, b *ListBufD) *ListBuf {
 	t := ns.make(b.F)
 	if len(b.D) == 0 {
-		return &ListBuf{newFixBuf(t, 10), 0}
+		return &ListBuf{newFixBuf(t, 10), 9}
 	}
 	return &ListBuf{&FixBuf{t, b.D}, len(b.D)}
 }
