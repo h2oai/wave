81b21ba6c8d0e0edad52b9394a82e251e6539c64
Marek Mihok <marek.mihok@h2o.ai>
2023-06-21 00:09:57 +0200

    chore: restore the commit overriden by forcepush of out-of-date local branch #2010

--- py/examples/image_annotator_events_click.py
+++ py/examples/image_annotator_events_click.py
@@ -5,11 +5,6 @@
 from h2o_wave import main, app, Q, ui
 
 vehicle = {'x1': 657, 'y1': 273, 'x2': 848, 'y2': 440}
-vehicle_annotation_rect = ui.image_annotator_item(shape=ui.image_annotator_rect(x1=657, y1=273, x2=848, y2=440), tag='v')
- 
-
-def is_inside_object(x, y, rect):
-    return rect['x1'] <= x <= rect['x2'] and rect['y1'] <= y <= rect['y2']
 
 
 @app('/demo')
@@ -18,7 +13,7 @@ async def serve(q: Q):
         q.page['example'] = ui.form_card(box='1 1 5 8', items=[
             ui.image_annotator(
                 name='annotator',
-                title='Try annotating the car and see what happens!',
+                title='Click on the car and see what happens!',
                 image='https://images.unsplash.com/photo-1535082623926-b39352a03fb7?auto=compress&cs=tinysrgb&w=1260&h=750&q=80',
                 image_height='450px',
                 allowed_shapes=['rect'],
@@ -36,10 +31,13 @@ async def serve(q: Q):
         ]
     if q.events.annotator and q.events.annotator.click:
         x, y = q.events.annotator.click.values()
-        if is_inside_object(x, y, vehicle):
-            q.page['example'].annotator.items = [vehicle_annotation_rect]
+        # If the click is within the vehicle's bounding box, show the vehicle.
+        if vehicle['x1'] <= x <= vehicle['x2'] and vehicle['y1'] <= y <= vehicle['y2']:
+            q.page['example'].annotator.items = [
+                ui.image_annotator_item(shape=ui.image_annotator_rect(x1=657, y1=273, x2=848, y2=440), tag='v')
+            ]
+            q.page['example'].annotator.title = 'You got it!'
         else:
-            # Make a fake UI update
-            q.page['non-existent'].items = []
+            q.page['example'].annotator.title = 'You missed the car!'
 
     await q.page.save()
