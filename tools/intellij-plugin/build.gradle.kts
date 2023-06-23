f85ee1dc1ebe49e0121ac733117ad0d9baea469f
Martin Turoci <martin.turoci@h2o.ai>
2023-06-23 12:43:53 +0200

    Revert "chore: Update intellij gradle plugin to resolve missing dependency."
    This reverts commit 0e7910c59359764df4487f5ce8ee46219a2ccaa3.

--- tools/intellij-plugin/build.gradle.kts
+++ tools/intellij-plugin/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     // Kotlin support
     id("org.jetbrains.kotlin.jvm") version "1.6.0"
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "1.3.1"
+    id("org.jetbrains.intellij") version "1.3.0"
     // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
     id("io.gitlab.arturbosch.detekt") version "1.19.0"
     // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
