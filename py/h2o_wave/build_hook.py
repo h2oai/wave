c1daf81c6918bfba649dcb9931b1b3ff76763fa9
Martin Turoci <martin.turoci@h2o.ai>
2023-07-12 11:16:03 +0200

    feat: Migrate h2o-wave Pypi build to hatch. #2071

---
+++ py/h2o_wave/build_hook.py
@@ -0,0 +1,61 @@
+import os
+import shutil
+from typing import Any, Dict
+
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+from hatchling.metadata.plugin.interface import MetadataHookInterface
+
+
+class CustomMetadataHook(MetadataHookInterface):
+    def update(self, metadata: dict) -> None:
+        metadata['version'] = os.environ.get('VERSION', metadata['version'])
+
+class CustomBuildHook(BuildHookInterface):
+    def initialize(self, _version, build_data):
+        platform = os.environ.get('H2O_WAVE_PLATFORM')
+        if not platform:
+            return
+        
+        build_data['tag'] = f'py3-none-{platform}'
+        build_data['pure_python'] = False
+
+        version = os.environ.get('VERSION')
+        if not version:
+            raise Exception('VERSION environment variable must be set.')
+
+        arch = 'amd64'
+        if platform.endswith('arm64'):
+            arch = 'arm64'
+        operating_system = 'darwin'
+        if platform == 'win_amd64':
+            operating_system = 'windows'
+        elif platform == 'manylinux1_x86_64':
+            operating_system = 'linux'
+
+        binaries_path = os.path.join('..', '..', 'build', f'wave-{version}-{operating_system}-{arch}')
+        if not os.path.exists(binaries_path):
+            raise Exception(f'{binaries_path} does not exist. Run make release first to generate server binaries.')
+
+        self.copy_files(binaries_path, 'tmp', ['demo', 'examples', 'test'])
+
+        # Create a metadata file to get easy access to platform/OS arch when needed.
+        with open(os.path.join('h2o_wave', 'metadata.py'), 'w') as f:
+            f.write(f'''
+# Generated in build_hook.py.
+__platform__ = "{platform}"
+__arch__ = "{arch}"
+        ''')
+
+    def copy_files(self, src, dst, ignore):
+        for file_name in os.listdir(src):
+            src_file = os.path.join(src, file_name)
+            dst_file = os.path.join(dst, file_name)
+            if os.path.isfile(src_file):
+                os.makedirs(os.path.dirname(dst_file), exist_ok=True)
+                shutil.copy(src_file, dst_file)
+            elif os.path.isdir(src_file) and file_name not in ignore:
+                self.copy_files(src_file, dst_file, ignore)
+
+
+    def finalize(self, version: str, build_data: Dict[str, Any], artifact_path: str) -> None:
+        shutil.rmtree('tmp', ignore_errors=True) 
