e8c47615d72ba455b5ed94cb4049d8a769e56795
Martin Turoci <martin.turoci@h2o.ai>
2023-07-17 13:16:55 +0200

    fix: Try editable install without dependencies.

--- setup.ps1
+++ setup.ps1
@@ -9,9 +9,9 @@ Set-Location ..
 # Setup Py tests.
 Set-Location py
 python -m venv venv
-.\venv\Scripts\python -m pip install --editable h2o_wave
 .\venv\Scripts\python -m pip install --upgrade pip
-.\venv\Scripts\pip install httpx typing_extensions
+.\venv\Scripts\python -m pip install --no-deps --editable h2o_wave
+.\venv\Scripts\pip install httpx
 echo "# Generated in hatch_build.py\n__platform__ = 'linux'\n__arch__ = 'amd64'" > h2o_wave/h2o_wave/metadata.py
 if ($LastExitCode -ne 0) { exit $LastExitCode }
 
