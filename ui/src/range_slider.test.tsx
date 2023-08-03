f815d3a30eb6ec838af33494acaf24a3bfb050cc
Marek Mihok <marek.mihok@h2o.ai>
2023-08-03 11:37:45 +0200

    chore: add unit test for keeping values in bounds when min/max is updated

--- ui/src/range_slider.test.tsx
+++ ui/src/range_slider.test.tsx
@@ -79,6 +79,14 @@ describe('rangeSlider.tsx', () => {
     expect(wave.args[name]).toMatchObject([40, 60])
   })
 
+  it('Changes our of bounds values when min/max is updated', () => {
+    const { rerender } = render(<XRangeSlider model={{ ...rangeSliderProps, min: 0, max: 100, min_value: 30, max_value: 60 }} />)
+    expect(wave.args[name]).toMatchObject([30, 60])
+
+    rerender(<XRangeSlider model={{ ...rangeSliderProps, min: 40, max: 50, min_value: 30, max_value: 60 }} />)
+    expect(wave.args[name]).toMatchObject([40, 50])
+  })
+
   it('Calls sync on slide', () => {
     const pushMock = jest.fn()
     wave.push = pushMock
