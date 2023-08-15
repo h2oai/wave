bcf7ef6656492e37d3f1e438001a5c36d08dadf3
Marek Mihok <marek.mihok@h2o.ai>
2023-08-15 22:51:50 +0200

    chore: update unit tests to check if the infinite loop is prevented #2069

--- ui/src/table.test.tsx
+++ ui/src/table.test.tsx
@@ -340,6 +340,26 @@ describe('Table.tsx', () => {
       expect(emitMock).toHaveBeenCalledTimes(1)
     })
 
+    it('Fires event - single selection - pre-selected value', async () => {
+      const
+        props = { ...tableProps, single: true, events: ['select'], value: 'rowname2' },
+        { getAllByRole } = render(<XTable model={{ ...props }} />),
+        radioButtons = getAllByRole('radio')
+      fireEvent.click(radioButtons[0])
+      expect(emitMock).toHaveBeenCalledWith(tableProps.name, 'select', ['rowname1'])
+      expect(emitMock).toHaveBeenCalledTimes(1)
+    })
+
+    it('Fires event - multiple selection - select single - pre-selected values', async () => {
+      const
+        props = { ...tableProps, multiple: true, events: ['select'], values: ['rowname2', 'rowname3'] },
+        { getAllByRole } = render(<XTable model={{ ...props }} />),
+        checkboxes = getAllByRole('checkbox')
+      fireEvent.click(checkboxes[1])
+      expect(emitMock).toHaveBeenCalledWith(tableProps.name, 'select', ['rowname1'])
+      expect(emitMock).toHaveBeenCalledTimes(1)
+    })
+
     it('Clicks a column - link set on second col', () => {
       tableProps = {
         ...tableProps,
