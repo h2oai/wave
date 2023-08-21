ef3adf24e86ab93dd4758afa7f3873ea7d19591f
Marek Mihok <marek.mihok@h2o.ai>
2023-08-21 23:27:26 +0200

    chore: update tests for ui.tabs

--- ui/src/tabs.test.tsx
+++ ui/src/tabs.test.tsx
@@ -18,10 +18,14 @@ import { Tabs, XTabs } from './tabs'
 import { wave } from './ui'
 
 const name = 'tabs'
+const hashName = `#${name}`
 const tabsProps: Tabs = { name, items: [{ name }] }
 
 describe('Tabs.tsx', () => {
-  beforeEach(() => { wave.args[name] = null })
+  beforeEach(() => {
+    window.location.hash = ''
+    wave.args[name] = null
+  })
 
   it('Renders data-test attr', () => {
     const { queryByTestId } = render(<XTabs model={tabsProps} />)
@@ -38,15 +42,137 @@ describe('Tabs.tsx', () => {
     expect(wave.args[name]).toBe(name)
     expect(pushMock).toHaveBeenCalled()
   })
+
   it('Does not call sync on click - args not changed', () => {
     const pushMock = jest.fn()
     wave.push = pushMock
-    wave.args[name] = name
 
     const { getByRole } = render(<XTabs model={tabsProps} />)
+    wave.args[name] = name
     fireEvent.click(getByRole('tab'))
 
     expect(pushMock).toHaveBeenCalledTimes(0)
+  })
+
+  it('Set args when value is updated', () => {
+    const items = [{ name: 'tab1' }, { name: 'tab2' }]
+    const props = { ...tabsProps, items, value: 'tab1' }
+    const { rerender, getAllByRole } = render(<XTabs model={props} />)
+    expect(getAllByRole('tab')[0]).toHaveClass('is-selected')
+    expect(wave.args['tab2']).toBeUndefined()
+
+    props.value = 'tab2'
+    rerender(<XTabs model={props} />)
+    // TODO: Not consistent with ui.tab_card - wave.args['tab2'].toBe(true)
+    expect(wave.args[name]).toBe('tab2')
+  })
+
+  it('Does not set args when value is updated - hash name', () => {
+    const items = [{ name: '#tab1' }, { name: '#tab2' }]
+    const props = { ...tabsProps, items, value: '#tab1' }
+    const { rerender } = render(<XTabs model={props} />)
+    expect(wave.args[name]).toBeNull()
+
+    props.value = '#tab2'
+    rerender(<XTabs model={props} />)
+
+    expect(wave.args[name]).toBeNull()
+  })
+
+  it('Selects tab when value is updated', () => {
+    const items = [{ name: 'tab1' }, { name: 'tab2' }]
+    const props = { ...tabsProps, items, value: 'tab1' }
+    const { rerender, getAllByRole } = render(<XTabs model={props} />)
+    expect(getAllByRole('tab')[0]).toHaveClass('is-selected')
+    expect(getAllByRole('tab')[1]).not.toHaveClass('is-selected')
+
+    props.value = 'tab2'
+    rerender(<XTabs model={props} />)
+    expect(getAllByRole('tab')[0]).not.toHaveClass('is-selected')
+    expect(getAllByRole('tab')[1]).toHaveClass('is-selected')
+  })
+
+  it('Selects tab when value is updated twice to the same value', () => {
+    const items = [{ name: 'tab1' }, { name: 'tab2' }]
+    const props = { ...tabsProps, items, value: 'tab1' }
+    const { rerender, getAllByRole } = render(<XTabs model={props} />)
+    expect(getAllByRole('tab')[0]).toHaveClass('is-selected')
+    expect(getAllByRole('tab')[1]).not.toHaveClass('is-selected')
+
+    props.value = 'tab2'
+    rerender(<XTabs model={props} />)
+    expect(getAllByRole('tab')[0]).not.toHaveClass('is-selected')
+    expect(getAllByRole('tab')[1]).toHaveClass('is-selected')
+
+    fireEvent.click(getAllByRole('tab')[0])
+    expect(getAllByRole('tab')[0]).toHaveClass('is-selected')
+    expect(getAllByRole('tab')[1]).not.toHaveClass('is-selected')
+
+    props.value = 'tab2'
+    rerender(<XTabs model={props} />)
+    expect(getAllByRole('tab')[0]).not.toHaveClass('is-selected')
+    expect(getAllByRole('tab')[1]).toHaveClass('is-selected')
+  })
+
+  it('Selects tab when value is updated - hash name', () => {
+    const items = [{ name: '#tab1' }, { name: '#tab2' }]
+    const props = { ...tabsProps, items, value: '#tab1' }
+    const { rerender, getAllByRole } = render(<XTabs model={props} />)
+    expect(getAllByRole('tab')[0]).toHaveClass('is-selected')
+    expect(getAllByRole('tab')[1]).not.toHaveClass('is-selected')
+
+    props.value = '#tab2'
+    rerender(<XTabs model={props} />)
+    expect(getAllByRole('tab')[0]).not.toHaveClass('is-selected')
+    expect(getAllByRole('tab')[1]).toHaveClass('is-selected')
+  })
+
+  it('Selects tab when value is updated twice to the same value - hash name', () => {
+    const items = [{ name: '#tab1' }, { name: '#tab2' }]
+    const props = { ...tabsProps, items, value: '#tab1' }
+    const { rerender, getAllByRole } = render(<XTabs model={props} />)
+    expect(getAllByRole('tab')[0]).toHaveClass('is-selected')
+    expect(getAllByRole('tab')[1]).not.toHaveClass('is-selected')
+
+    props.value = '#tab2'
+    rerender(<XTabs model={props} />)
+    expect(getAllByRole('tab')[0]).not.toHaveClass('is-selected')
+    expect(getAllByRole('tab')[1]).toHaveClass('is-selected')
+
+    fireEvent.click(getAllByRole('tab')[0])
+    expect(getAllByRole('tab')[0]).toHaveClass('is-selected')
+    expect(getAllByRole('tab')[1]).not.toHaveClass('is-selected')
+
+    props.value = '#tab2'
+    rerender(<XTabs model={props} />)
+    expect(getAllByRole('tab')[0]).not.toHaveClass('is-selected')
+    expect(getAllByRole('tab')[1]).toHaveClass('is-selected')
+  })
+
+  it('Sets url hash - hash name', () => {
+    const { getByRole } = render(<XTabs model={{ ...tabsProps, items: [{ name: hashName }] }} />)
+    fireEvent.click(getByRole('tab'))
+
+    expect(window.location.hash).toBe(hashName)
+  })
+
+  // TODO: Fix.
+  it('Sets url hash when value is updated - hash name', () => {
+    const items = [{ name: '#tab1' }, { name: '#tab2' }]
+    const props = { ...{ ...tabsProps, items, value: '#tab1' } }
+    const { rerender } = render(<XTabs model={props} />)
+    expect(window.location.hash).toBe('')
+
+    props.value = '#tab2'
+    rerender(<XTabs model={props} />)
+    expect(window.location.hash).toBe('#tab2')
+  })
+
+  it('Sets default tab', () => {
+    const items = [{ name: 'tab1' }, { name: 'tab2' }]
+    const { getAllByRole } = render(<XTabs model={{ ...tabsProps, items, value: 'tab2' }} />)
+
+    expect(getAllByRole('tab')[1]).toHaveClass('is-selected')
   })
 
 })
