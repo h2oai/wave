83ca3f08d38a89b259c8f2ba29f780af839c50ae
Martin Turoci <martin.turoci@h2o.ai>
2023-09-04 08:29:29 +0200

    chore: Remove dead custom eslint rule.

--- ui/eslint/linter.js
+++ ui/eslint/linter.js
@@ -47,49 +47,5 @@ module.exports = {
         }
       })
     },
-    "data-test-on-component": {
-      meta: {
-        type: 'suggestion',
-      },
-      create: context => ({
-        ReturnStatement: node => {
-          // TODO: Remove when https://github.com/h2oai/qd/pull/70 will be merged.
-          return
-
-          if (!node.argument) return
-
-          const openingEl = node.argument.openingElement
-          if (node.argument.type !== 'JSXElement' && openingEl !== 'JSXOpeningElement') return
-
-          const isFormOrCard = (node) => {
-            if (!node) return false
-
-            const isCard = node.id && node.id.name === 'View'
-            const isFormComponent = node.id && node.id.name.startsWith('X')
-
-            return isCard || isFormComponent || isFormOrCard(node.parent)
-          }
-
-          const isWithinRender = (node) => {
-            if (!node) return false
-            return node.id && node.id.name === 'render' || isWithinRender(node.parent)
-          }
-
-          const isWithinMapFunc = (node) => {
-            if (!node) return false
-            return node.init && node.init.callee && node.init.callee.property && node.init.callee.property.name === 'map' || isWithinMapFunc(node.parent)
-          }
-
-          if (
-            !isWithinMapFunc(node.parent) &&
-            isWithinRender(node.parent) &&
-            isFormOrCard(node.parent) &&
-            !openingEl.attributes.find(({ name }) => name && name.name === 'data-test')
-          ) {
-            context.report(openingEl, 'React component wrapper must have data-test attribute specified.')
-          }
-        }
-      })
-    },
   }
 }
