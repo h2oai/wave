67799842bcdf0164453e3fdb100e1b57d4c42c46
Martin Turóci <64769322+mturoci@users.noreply.github.com>
2023-09-06 10:11:17 +0200

    refactor: Change wave share client protocol. (#2125)

--- py/h2o_wave/h2o_wave/share.py
+++ py/h2o_wave/h2o_wave/share.py
@@ -10,17 +10,26 @@ async def pipe(r: asyncio.StreamReader, w: asyncio.StreamWriter) -> None:
         await w.drain()
 
 
-async def listen_on_socket(local_host: str, local_port: int, remote_host: str, remote_port: int) -> None:
+async def listen_on_socket(local_host: str, local_port: int, remote_host: str, remote_port: int, id: str) -> None:
+    local_reader, local_writer = None, None
+    remote_reader, remote_writer = None, None
+    retries = 0
     while True:
+        if retries > 5:
+            break
         try:
             local_reader, local_writer = await asyncio.open_connection(local_host, local_port)
             remote_reader, remote_writer = await asyncio.open_connection(remote_host, remote_port, ssl=True)
+            remote_writer.write(f'__h2o_leap__ {id}\n'.encode())
 
+            retries = 0
             await asyncio.gather(pipe(local_reader, remote_writer), pipe(remote_reader, local_writer))
 
         # Swallow exceptions and reconnect.
         except Exception:
-            pass
+            retries += 1
         finally:
-            local_writer.close()
-            remote_writer.close()
+            if local_writer:
+                local_writer.close()
+            if remote_writer:
+                remote_writer.close()
