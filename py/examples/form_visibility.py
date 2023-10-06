45afd23710c84112e0b30a72afddfe6b7fb3e434
Martin Turóci <64769322+mturoci@users.noreply.github.com>
2023-10-06 13:23:44 +0200

    chore: Use maximum available space in ui.inline instead of hardcoded constant. #1974 (#2152)

--- py/examples/form_visibility.py
+++ py/examples/form_visibility.py
@@ -12,7 +12,7 @@ async def serve(q: Q):
             ui.text_l(name='text_l', content='Second text'),
             ui.text_m(name='text_m', content='Third text'),
             ui.text_s(name='text_s', content='Fourth text'),
-            ui.inline([
+            ui.buttons([
                 ui.button(name='left1', label='Left1'),
                 ui.button(name='left2', label='Left2'),
                 ui.button(name='left3', label='Left3'),
