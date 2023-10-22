4814d4bb0769bb9ca0832572e7ecd093b38a7806
Michael Liu <u7460999@anu.edu.au>
2023-10-22 16:48:23 +1100

    Fixed table_pagination.py

--- py/examples/table_pagination.py
+++ py/examples/table_pagination.py
@@ -17,7 +17,7 @@ class Issue:
         self.status = status
 
 
-all_rows = [Issue(text=i + 1, status=('Closed' if i % 2 == 0 else 'Open')) for i in range(100)]
+all_rows = [Issue(text=str(i + 1), status=('Closed' if i % 2 == 0 else 'Open')) for i in range(100)]
 rows_per_page = 10
 total_rows = len(all_rows)
 
@@ -37,8 +37,8 @@ def get_rows(base: List, sort: Optional[Dict[str, bool]] = None, search: Optiona
         rows = [row for row in rows if any(search_val in str(getattr(row, col)).lower() for col in cols)]
     # Filter out rows that do not contain filtered column value.
     if filters:
-        for col, filters in filters.items():
-            rows = [row for row in rows if not filters or any(f in getattr(row, col) for f in filters)]
+        for col, filter_values in filters.items():
+            rows = [row for row in rows if not filter_values or any(f in getattr(row, col) for f in filter_values)]
 
     return rows
 
