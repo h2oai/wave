a48d60d6a33dfcd8750252c44e480de2b17a9fcc
Marek Mihok <marek.mihok@h2o.ai>
2023-11-30 17:21:28 +0100

    chore: update py example #2196

--- py/examples/table_events_select.py
+++ py/examples/table_events_select.py
@@ -7,20 +7,21 @@ from h2o_wave import main, app, Q, ui
 
 @app('/demo')
 async def serve(q: Q):
-    if q.events.table and q.events.table.select is not None:
+    if q.events.table and q.events.table.select:
         q.page['description'].content = f'{q.events.table.select}'
     else:
         q.page['table'] = ui.form_card(box='1 1 3 4', items=[
             ui.table(
                 name='table',
-                columns=[ui.table_column(name='text', label='Table select event')],
+                columns=[ui.table_column(name='text', label='Table select event', searchable=True, filterable=True)],
                 rows=[
                     ui.table_row(name='row1', cells=['Row 1']),
                     ui.table_row(name='row2', cells=['Row 2']),
                     ui.table_row(name='row3', cells=['Row 3'])
                 ],
                 multiple=True,
-                events=['select']
+                events=['select'],
+                groupable=True
             )
         ])
         q.page['description'] = ui.markdown_card(box='4 1 3 4', title='Selected rows', content='Nothing selected yet.')
