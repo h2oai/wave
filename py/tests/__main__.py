9ed9aac7ac205cf1bd5bf07d31434f01b176649b
Marek Mihok <marek.mihok@h2o.ai>
2024-03-13 14:06:19 +0100

    ci: Resolve resource warning in py tests #2261 (#2286)

--- py/tests/__main__.py
+++ py/tests/__main__.py
@@ -6,6 +6,9 @@ import httpx
 import os
 import platform
 
+# HACK: Use urllib3 import side-effect to resolve "ResourceWarning: unclosed transport" in python>=3.9 versions.
+import urllib3
+
 from .test_expando import *
 from .test_python_server import *
 from .test_python_server_async import *
