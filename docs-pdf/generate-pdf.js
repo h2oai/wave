03e19b7d620ddf82e65842931b28253c7ef75e7f
Shaun <shaun.yogeshwaran@h2o.ai>
2025-05-21 10:59:46 +0530

    Fix images

--- docs-pdf/generate-pdf.js
+++ docs-pdf/generate-pdf.js
@@ -121,6 +121,23 @@ async function cleanupDom(page) {
   });
 }
 
+async function autoScroll(page) {
+  await page.evaluate(async () => {
+    await new Promise((resolve) => {
+      let totalHeight = 0;
+      const distance = 100;
+      const timer = setInterval(() => {
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+        if (totalHeight >= document.body.scrollHeight) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 100);
+    });
+  });
+}
+
 
 async function generatePdfBuffers(page, urls) {
   const pdfBuffers = [];
