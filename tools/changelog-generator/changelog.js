fcc21c8ac0cf24e8f4c69cd390f2dfe3a1bb2379
Martin Turoci <martin.turoci@h2o.ai>
2026-02-11 10:23:31 +0100

    ci: Auto-generate changelog against proper prev version.

--- tools/changelog-generator/changelog.js
+++ tools/changelog-generator/changelog.js
@@ -1,43 +1,66 @@
 #!/usr/bin/env node
 // WARNING: Works only on Linux and MacOS.
-if (process.platform === 'win32') {
-  console.error('Windows is not supported.')
-  process.exit(1)
+if (process.platform === "win32") {
+  console.error("Windows is not supported.");
+  process.exit(1);
 }
+
+const child_process = require("child_process");
 
 const typesToChangelog = {
-  'feat': 'Added',
-  'fix': 'Fixed',
-  'perf': 'Performance Improvements',
-  'docs': 'Documentation',
-  '!': 'Breaking Changes',
-}
+  feat: "Added",
+  fix: "Fixed",
+  perf: "Performance Improvements",
+  docs: "Documentation",
+  security: "Security",
+  "!": "Breaking Changes",
+};
 
-const version = process.env.VERSION || require('../../ui/package.json').version
-const categorizedCommits = require('child_process')
-  .execSync(`git log v${version}..HEAD --oneline`)
+// git show-ref --tags -d
+const tags = child_process
+  .execSync("git show-ref --tags -d")
   .toString()
-  .split('\n')
-  .reduce((categorizedCommits, commit) => {
-    // Remove commit hash.
-    commit = commit.substring(commit.indexOf(' ') + 1)
-    let type = commit.split(':')[0].toLocaleLowerCase()
+  .split("\n");
 
-    if (type.endsWith('!')) type = '!'
-    if (type === 'feat') commit = commit.replace(/feat/i, 'New')
-    if (type === 'docs') commit = commit.replace(/docs/i, 'Docs')
-    if (type === 'perf' || type === 'fix' || type === '!') commit = commit.split(':')[1].trim()
-    if (categorizedCommits[type]) categorizedCommits[type].push(commit)
+const prevVersion = tags[tags.length - 4]
+  .split(" ")[1]
+  .split("/")
+  .pop()
+  .replace(/-g$/, "");
 
-    return categorizedCommits
-  }, { 'feat': [], 'fix': [], 'perf': [], 'docs': [], '!': [] })
+const categorizedCommits = child_process
+  .execSync(`git log ${prevVersion}..HEAD --oneline`)
+  .toString()
+  .split("\n")
+  .reduce(
+    (categorizedCommits, commit) => {
+      // Remove commit hash.
+      commit = commit.substring(commit.indexOf(" ") + 1);
+      let type = commit.split(":")[0].toLocaleLowerCase();
 
-categorizedCommits['feat'] = categorizedCommits['feat'].concat(categorizedCommits['docs'])
-delete categorizedCommits['docs']
+      if (type.endsWith("!")) type = "!";
+      if (type === "feat") commit = commit.replace(/feat/i, "New");
+      if (type === "docs") commit = commit.replace(/docs/i, "Docs");
+      if (type === "perf" || type === "fix" || type === "!")
+        commit = commit.split(":")[1].trim();
+      if (categorizedCommits[type]) categorizedCommits[type].push(commit);
+
+      return categorizedCommits;
+    },
+    { feat: [], fix: [], perf: [], docs: [], "!": [], security: [] },
+  );
+
+categorizedCommits["feat"] = categorizedCommits["feat"].concat(
+  categorizedCommits["docs"],
+);
+delete categorizedCommits["docs"];
 
 const changelog = Object.keys(categorizedCommits)
-  .filter(type => categorizedCommits[type].length)
-  .map(type => `- ${typesToChangelog[type]}\n${categorizedCommits[type].map(commit => `  - ${commit}`).join('\n')}`)
+  .filter((type) => categorizedCommits[type].length)
+  .map(
+    (type) =>
+      `- ${typesToChangelog[type]}\n${categorizedCommits[type].map((commit) => `  - ${commit}`).join("\n")}`,
+  );
 
-require('fs').writeFileSync('CHANGELOG.md', changelog.join('\n'))
-console.log('CHANGELOG.md updated.')
+require("fs").writeFileSync("CHANGELOG.md", changelog.join("\n"));
+console.log("CHANGELOG:\n" + changelog.join("\n"));
